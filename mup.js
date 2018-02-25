3f2fa56c1ea64573abb8af0a45772e4af847682a
Aurélien Ooms <aurelien.ooms@gmail.com>
2018-02-25 23:04:12 +0100

    add mup config

---
+++ mup.js
@@ -0,0 +1,37 @@
+module.exports = {
+  servers: {
+    one: {
+      host: 'localhost',
+      username: 'meteorapp'
+    }
+  },
+
+  meteor: {
+    name: 'patients',
+    path: '.',
+    servers: {
+      one: {}
+    },
+    docker: {
+      image: 'abernix/meteord:base'
+    },
+    buildOptions: {
+      serverOnly: true
+    },
+    env: {
+      ROOT_URL: 'http://localhost',
+      PORT: 3001
+    },
+    deployCheckWaitTime: 30,
+    deployCheckPort: 80,
+    enableUploadProgressBar: true
+  },
+
+  mongo: {
+    oplog: true,
+    port: 27017,
+    servers: {
+      one: {},
+    },
+  },
+};
