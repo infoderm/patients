bbd196e932b87655a5a7eb4d8f2273c120c5a614
Aurélien Ooms <aurelien.ooms@gmail.com>
2018-09-21 21:40:12 +0200

    :bug: fix(ui): reset SearchBox after click

--- imports/ui/Header.js
+++ imports/ui/Header.js
@@ -11,18 +11,20 @@ import Typography from '@material-ui/core/Typography';
 import SearchBox from './patients/SearchBox.js';
 import AccountsUI from './users/AccountsUI.js';
 
+import Downshift from 'downshift';
+
 const drawerWidth = 240;
 
 const styles = theme => ({
-	appBar: {
-		position: 'fixed',
-		width: `calc(100% - ${drawerWidth}px)`,
-		marginLeft: drawerWidth,
-	},
-	title: {
-		flex: '0 1 auto',
-		marginLeft: theme.spacing.unit * 3,
-	},
+  appBar: {
+    position: 'fixed',
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  title: {
+    flex: '0 1 auto',
+    marginLeft: theme.spacing.unit * 3,
+  },
 });
 
 function getSuggestions(suggestions, inputValue) {
@@ -39,45 +41,63 @@ function getSuggestions(suggestions, inputValue) {
 
 class Header extends React.Component {
 
-	constructor(props){
-		super(props);
-	}
+  constructor(props){
+    super(props);
+  }
 
-	handleChange = (selectedItem, downshiftState) => {
-		if ( selectedItem ) {
-			const { history } = this.props;
-			history.push(`/patient/${selectedItem._id}`);
-		}
+  handleChange = (selectedItem, downshiftState) => {
+    if ( selectedItem ) {
+      const { history } = this.props;
+      history.push(`/patient/${selectedItem._id}`);
+    }
+  };
+
+  reduceState = (state, changes) => {
+    switch (changes.type) {
+      case Downshift.stateChangeTypes.clickItem:
+	return {
+	  ...changes,
+	  inputValue: '',
 	};
+      default:
+	return changes;
+    }
+  };
 
-	render(){
+  render(){
 
-		const { classes, patients, currentUser } = this.props;
+    const { classes, patients, currentUser } = this.props;
 
-		const suggestions = patients.map(
-			patient => ({
-				label : `${patient.lastname} ${patient.firstname}` ,
-				_id : patient._id ,
-			})
-		) ;
+    const suggestions = patients.map(
+      patient => ({
+	label : `${patient.lastname} ${patient.firstname}` ,
+	_id : patient._id ,
+      })
+    ) ;
 
-		return (
-			<AppBar className={classes.appBar}>
-				<Toolbar>
-					<Typography variant="title" color="inherit" className={classes.title}>{location.pathname}</Typography>
-					<div style={{flex:'1 1 auto'}}></div>
-					<SearchBox filter={getSuggestions} suggestions={suggestions} itemToString={item => item ? item.label : ''} onChange={this.handleChange}/>
-					<AccountsUI currentUser={currentUser}/>
-				</Toolbar>
-			</AppBar>
-			);
-	}
+    return (
+      <AppBar className={classes.appBar}>
+	<Toolbar>
+	  <Typography variant="title" color="inherit" className={classes.title}>{location.pathname}</Typography>
+	  <div style={{flex:'1 1 auto'}}></div>
+	  <SearchBox
+	    filter={getSuggestions}
+	    suggestions={suggestions}
+	    itemToString={item => item ? item.label : ''}
+	    onChange={this.handleChange}
+	    stateReducer={this.reduceState}
+	  />
+	  <AccountsUI currentUser={currentUser}/>
+	</Toolbar>
+      </AppBar>
+      );
+}
 }
 
 Header.propTypes = {
-	classes: PropTypes.object.isRequired,
-	theme: PropTypes.object.isRequired,
-	history: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
 };
 
 export default withRouter( withStyles(styles, { withTheme: true }) (Header) ) ;
