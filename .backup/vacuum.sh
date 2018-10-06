135ba5e4c529a833f4308e6a107133cf6eeadfb1
Aurélien Ooms <aurelien.ooms@gmail.com>
2018-10-07 00:39:07 +0200

    :bug: fix(backup): uncomment vacuum code

--- .backup/vacuum.sh
+++ .backup/vacuum.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 
+set -o xtrace
+
 CLOUD='db' # dropbox
 
 cd "$(dirname "$0")"
 LIST="$(sh list.sh)"
 KEEP="$(python3 keep.py <<< "$LIST")"
 
-echo "Backup list:"
-echo "$LIST"
-echo "Keeping:"
-echo "$KEEP"
-
 DELETE="$(comm -23 <(echo "$LIST") <(echo "$KEEP"))"
 
 for dir in $DELETE ; do
-  echo "Deleting $dir"
-  echo rc purge "$CLOUD":patients-backup/"$dir"
+  rc purge "$CLOUD":patients-backup/"$dir"
 done
