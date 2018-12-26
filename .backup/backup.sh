30517f126bddb4cecf5b8b6a45b7ea9b6414dbea
Aurélien Ooms <aurelien.ooms@gmail.com>
2018-12-26 11:26:51 +0100

    :bug: fix(backup): upgrade encryption algo

--- .backup/backup.sh
+++ .backup/backup.sh
@@ -5,12 +5,13 @@ set -o xtrace
 SERVER='meteorapp@localhost'
 CLOUD='db' # dropbox
 IDENTITY="$HOME/.ssh/meteorapp"
+CRYPTO="-pbkdf2"
 
 cd "$(dirname "$0")"
 ssh -i "$IDENTITY" "$SERVER" rm -rf dump/patients patients.gz patients.gz.enc || exit 1
 ssh "$SERVER" mongodump --db patients || exit 1
 ssh "$SERVER" tar czf patients.gz dump/patients || exit 1
-ssh "$SERVER" openssl enc -aes-256-cbc -in patients.gz -out patients.gz.enc -pass file:key/patients || exit 1
+ssh "$SERVER" openssl enc $CRYPTO -in patients.gz -out patients.gz.enc -pass file:key/patients || exit 1
 rsync -a "$SERVER":patients.gz.enc patients-backup.gz.enc || exit 1
 rc copy patients-backup.gz.enc "$CLOUD":patients-backup/"$(date '+%Y-%m-%d_%H:%M:%S')"
 
