44beaa1d7ad61c029875925d39dd407916fae635
Aurélien Ooms <aurelien.ooms@gmail.com>
2019-01-23 14:54:22 +0100

    refactor: simplify backup

--- .backup/backup.sh
+++ .backup/backup.sh
@@ -11,12 +11,16 @@ function onserver {
   ssh -i "$IDENTITY" "$SERVER" "$@"
 }
 
+function load {
+  rsync -e "ssh -i $IDENTITY" -a "$@"
+}
+
 cd "$(dirname "$0")"
 onserver rm -rf dump/patients patients.gz patients.gz.enc || exit 1
 onserver mongodump --db patients || exit 1
 onserver tar czf patients.gz dump/patients || exit 1
 onserver openssl enc $CRYPTO -in patients.gz -out patients.gz.enc -pass file:key/patients || exit 1
-rsync -e "ssh -i $IDENTITY" -a "$SERVER":patients.gz.enc patients-backup.gz.enc || exit 1
+load "$SERVER":patients.gz.enc patients-backup.gz.enc || exit 1
 rc copy patients-backup.gz.enc "$CLOUD":patients-backup/"$(date '+%Y-%m-%d_%H:%M:%S')"
 
 if [ "$?" -eq 0 ] ; then
--- .backup/restore.sh
+++ .backup/restore.sh
@@ -10,9 +10,13 @@ function onserver {
   ssh -i "$IDENTITY" "$SERVER" "$@"
 }
 
+function load {
+  rsync -e "ssh -i $IDENTITY" -a "$@"
+}
+
 cd "$(dirname "$0")"
 onserver rm -rf dump/patients patients.gz patients.gz.enc || exit 1
-rsync -e "ssh -i $IDENTITY" -a patients-backup.gz.enc "$SERVER":patients.gz.enc || exit 1
+load patients-backup.gz.enc "$SERVER":patients.gz.enc || exit 1
 onserver openssl enc -d $CRYPTO -in patients.gz.enc -out patients.gz -pass file:key/patients || exit 1
 onserver tar xzf patients.gz || exit 1
 onserver mongorestore -d patients dump/patients/
