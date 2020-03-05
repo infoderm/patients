1dd902c010840f71cc58deb1565f9043c95e9212
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-03-05 16:43:52 +0100

    Replace typeface-roboto by webfontloader.

--- client/main.js
+++ client/main.js
@@ -5,7 +5,16 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 
-import 'typeface-roboto';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@material-ui/pickers/adapter/date-fns';
+
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Roboto:300,400,500']
+  }
+});
 
 import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App.js';
