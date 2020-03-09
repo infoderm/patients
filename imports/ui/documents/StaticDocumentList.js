b6c16776b0b73badcd65c03cb0bae6dadb4633b4
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-03-09 22:18:43 +0100

    Refactor StaticDocumentList using Paginator.

--- imports/ui/documents/StaticDocumentList.js
+++ imports/ui/documents/StaticDocumentList.js
@@ -1,46 +1,19 @@
-import React from 'react' ;
+import React, {Fragment} from 'react' ;
 import PropTypes from 'prop-types';
 
-import { Link } from 'react-router-dom';
-
-import { makeStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
-import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
-import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import Paginator from '../navigation/Paginator.js' ;
 
 import DocumentsPage from './DocumentsPage.js';
 
-const useStyles = makeStyles(
-  theme => ({
-    fabprev: {
-        position: 'fixed',
-        bottom: theme.spacing(3),
-        right: theme.spacing(12),
-    },
-    fabnext: {
-        position: 'fixed',
-        bottom: theme.spacing(3),
-        right: theme.spacing(3),
-    },
-  })
-);
-
 export default function StaticDocumentList ( { page , perpage , documents } ) {
 
-  const classes = useStyles();
-
   return (
-    <div>
-      <DocumentsPage documents={documents}/>
-      { page === 1 ? null :
-      <Fab className={classes.fabprev} color="primary" component={Link} to={`/documents/page/${page-1}`}>
-          <NavigateBeforeIcon/>
-      </Fab> }
-      { documents.length < perpage ? null :
-      <Fab className={classes.fabnext} color="primary" component={Link} to={`/documents/page/${page+1}`}>
-          <NavigateNextIcon/>
-      </Fab> }
-    </div>
+    <Fragment>
+      <div>
+        <DocumentsPage documents={documents}/>
+      </div>
+      <Paginator page={page} end={documents.length < perpage} root="/documents"/>
+    </Fragment>
   ) ;
 }
 
