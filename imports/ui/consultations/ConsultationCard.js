5abae4e5ad57bb6da36d5fc8ab359c8f3150ef94
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-03-09 11:30:16 +0100

    :recycle: refactor(ConsultationCard): Use hooks.

--- imports/ui/consultations/ConsultationCard.js
+++ imports/ui/consultations/ConsultationCard.js
@@ -1,12 +1,12 @@
 import { Meteor } from 'meteor/meteor' ;
 import { withTracker } from 'meteor/react-meteor-data' ;
 
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom'
 
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
@@ -54,219 +54,211 @@ import AttachmentLink from '../attachments/AttachmentLink.js';
 
 import { Patients } from '../../api/patients.js';
 
-const styles = theme => ({
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    fontWeight: theme.typography.fontWeightRegular,
-  },
-  chips: {
-    display: 'flex',
-    justifyContent: 'center',
-    flexWrap: 'wrap',
-  } ,
-  chip: {
-    marginRight: theme.spacing(1),
-  },
-  linksep: {
-    marginRight: theme.spacing(1),
-  },
-  link: {
-    fontWeight: 'bold',
-  },
-  patientchip: {
-    marginRight: theme.spacing(1),
-    backgroundColor: '#88f',
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-  pricechip: {
-    marginRight: theme.spacing(1),
-    backgroundColor: '#228d57',
-    color: '#e8e9c9',
-    fontWeight: 'bold',
-  },
-  debtchip: {
-    marginRight: theme.spacing(1),
-    backgroundColor: '#f88',
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-});
+const useStyles = makeStyles(
+  theme => ({
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+    chips: {
+      display: 'flex',
+      justifyContent: 'center',
+      flexWrap: 'wrap',
+    } ,
+    chip: {
+      marginRight: theme.spacing(1),
+    },
+    linksep: {
+      marginRight: theme.spacing(1),
+    },
+    link: {
+      fontWeight: 'bold',
+    },
+    patientchip: {
+      marginRight: theme.spacing(1),
+      backgroundColor: '#88f',
+      color: '#fff',
+      fontWeight: 'bold',
+    },
+    pricechip: {
+      marginRight: theme.spacing(1),
+      backgroundColor: '#228d57',
+      color: '#e8e9c9',
+      fontWeight: 'bold',
+    },
+    debtchip: {
+      marginRight: theme.spacing(1),
+      backgroundColor: '#f88',
+      color: '#fff',
+      fontWeight: 'bold',
+    },
+  })
+);
 
-class ConsultationCard extends React.Component {
+function ConsultationCard ( props ) {
 
-  constructor (props) {
-    super(props)
-    this.state = {
-      paying: false,
-      settling: false,
-      deleting: false,
-    };
-  }
+  const classes = useStyles();
 
-  render () {
+  const [paying, setPaying] = useState(false);
+  const [settling, setSettling] = useState(false);
+  const [deleting, setDeleting] = useState(false);
 
-    const {
-      patientChip,
-      showPrice,
-      defaultExpanded,
-      classes ,
-      loadingPatient ,
-      patient ,
-      consultation : {
-	_id,
-	patientId,
-	datetime,
-	reason,
-	done,
-	todo,
-	treatment,
-	next,
-	more,
-	currency,
-	payment_method,
-	price,
-	paid,
-	book,
-	attachments,
-      } ,
-    } = this.props;
+  const {
+    patientChip,
+    showPrice,
+    defaultExpanded,
+    loadingPatient ,
+    patient ,
+    consultation : {
+      _id,
+      patientId,
+      datetime,
+      reason,
+      done,
+      todo,
+      treatment,
+      next,
+      more,
+      currency,
+      payment_method,
+      price,
+      paid,
+      book,
+      attachments,
+    } ,
+  } = props;
 
-    const { paying, settling , deleting } = this.state;
+  const missingPaymentData = currency === undefined || price === undefined || paid === undefined;
+  const owes = ! (missingPaymentData || paid === price) ;
+  const owed = owes ? price - paid : 0 ;
 
-    const missingPaymentData = currency === undefined || price === undefined || paid === undefined;
-    const owes = ! (missingPaymentData || paid === price) ;
-    const owed = owes ? price - paid : 0 ;
-
-    return (
-      <ExpansionPanel defaultExpanded={defaultExpanded}>
-	<ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
-	  <div className={classes.chips}>
-	    <Chip label={dateFormat(datetime,'iii do MMMM yyyy')} className={classes.chip} component={Link} to={`/calendar/${dateFormat(datetime,'yyyy-MM-dd')}`}/>
-	    <Chip label={dateFormat(datetime,'hh:mma')} className={classes.chip}/>
-	    { !patientChip ? null :
-	      <Chip
-	      avatar={(!loadingPatient && patient && patient.photo) ? <Avatar src={`data:image/png;base64,${patient.photo}`}/> : null}
-	      label={loadingPatient ? patientId : !patient ? 'Not found' : `${patient.lastname} ${patient.firstname}`}
-	      className={classes.patientchip}
-	      component={Link}
-	      to={`/patient/${patientId}`}
-	      />
-	    }
-	    { attachments === undefined || attachments.length === 0 ? '' :
-	      <Chip
-		avatar={<Avatar><AttachmentIcon/></Avatar>}
-		label={attachments.length}
-		className={classes.chip}
-	      />
-	    }
-	    { !missingPaymentData && showPrice &&
-	      <Chip
-		className={classes.pricechip}
-		avatar={<Avatar>{ payment_method === 'wire' ? <PaymentIcon/> : <MoneyIcon/> }</Avatar>}
-		label={Currency.format(price, {code: currency})}
-	      />
-	    }
-	    { owes && <Chip label={`Doit ${Currency.format(owed, {code: currency})}`} className={classes.debtchip}/> }
-	  </div>
-	</ExpansionPanelSummary>
-	<ExpansionPanelDetails>
-	  <List>
-	    <ListItem>
-	      <ListItemAvatar><Avatar><InfoIcon/></Avatar></ListItemAvatar>
-	      <ListItemText primary="Motif de la consultation" secondary={reason}/>
-	    </ListItem>
-	    <ListItem>
-	      <ListItemAvatar><Avatar><DoneIcon/></Avatar></ListItemAvatar>
-	      <ListItemText primary="Examens déjà réalisés" secondary={done}/>
-	    </ListItem>
-	    <ListItem>
-	      <ListItemAvatar><Avatar><HourglassFullIcon/></Avatar></ListItemAvatar>
-	      <ListItemText primary="Examens à réaliser" secondary={todo}/>
-	    </ListItem>
-	    <ListItem>
-	      <ListItemAvatar><Avatar><EditIcon/></Avatar></ListItemAvatar>
-	      <ListItemText primary="Traitement" secondary={treatment}/>
-	    </ListItem>
-	    <ListItem>
-	      <ListItemAvatar><Avatar><AlarmIcon/></Avatar></ListItemAvatar>
-	      <ListItemText primary="À revoir" secondary={next}/>
-	    </ListItem>
-	    <ListItem>
-	      <ListItemAvatar><Avatar><WarningIcon/></Avatar></ListItemAvatar>
-	      <ListItemText primary="Autres remarques" secondary={more}/>
-	    </ListItem>
-	    { missingPaymentData ? '' :
-	    <ListItem>
-	      <ListItemAvatar><Avatar><EuroSymbolIcon/></Avatar></ListItemAvatar>
-	      <ListItemText
-		primary="Paiement"
-		secondary={`À payé ${Currency.format(paid, {code: currency})} de ${Currency.format(price, {code: currency})}.`}
-	      />
-	    </ListItem> }
-	    { missingPaymentData ? '' :
-	    <ListItem>
-	      <ListItemAvatar><Avatar><AccountBalanceWalletIcon/></Avatar></ListItemAvatar>
-	      <ListItemText
-		primary="Méthode de Paiement"
-		secondary={payment_method === 'wire' ? 'Virement' : 'Cash'}
-	      />
-	    </ListItem> }
-	    { book === '' ? '' :
-	    <ListItem>
-	      <ListItemAvatar><Avatar><BookIcon/></Avatar></ListItemAvatar>
-	      <ListItemText primary="Carnet" secondary={book}/>
-	    </ListItem> }
-	    { attachments === undefined || attachments.length === 0 ? '' :
-	    <ListItem>
-	      <ListItemAvatar><Avatar><AttachmentIcon/></Avatar></ListItemAvatar>
-	      <ListItemText
-		disableTypography={true}
-		primary={<Typography variant="subtitle1">{attachments.length} attachments</Typography>}
-		secondary={
-		(<ul>{
-		  attachments.map(
-		    (attachmentId, i) => (
-		      <li key={attachmentId}>
-			<AttachmentLink className={classes.link} attachmentId={attachmentId}/>
-		      </li>
-		    )
+  return (
+    <ExpansionPanel defaultExpanded={defaultExpanded}>
+      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
+	<div className={classes.chips}>
+	  <Chip label={dateFormat(datetime,'iii do MMMM yyyy')} className={classes.chip} component={Link} to={`/calendar/${dateFormat(datetime,'yyyy-MM-dd')}`}/>
+	  <Chip label={dateFormat(datetime,'hh:mma')} className={classes.chip}/>
+	  { !patientChip ? null :
+	    <Chip
+	    avatar={(!loadingPatient && patient && patient.photo) ? <Avatar src={`data:image/png;base64,${patient.photo}`}/> : null}
+	    label={loadingPatient ? patientId : !patient ? 'Not found' : `${patient.lastname} ${patient.firstname}`}
+	    className={classes.patientchip}
+	    component={Link}
+	    to={`/patient/${patientId}`}
+	    />
+	  }
+	  { attachments === undefined || attachments.length === 0 ? '' :
+	    <Chip
+	      avatar={<Avatar><AttachmentIcon/></Avatar>}
+	      label={attachments.length}
+	      className={classes.chip}
+	    />
+	  }
+	  { !missingPaymentData && showPrice &&
+	    <Chip
+	      className={classes.pricechip}
+	      avatar={<Avatar>{ payment_method === 'wire' ? <PaymentIcon/> : <MoneyIcon/> }</Avatar>}
+	      label={Currency.format(price, {code: currency})}
+	    />
+	  }
+	  { owes && <Chip label={`Doit ${Currency.format(owed, {code: currency})}`} className={classes.debtchip}/> }
+	</div>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+	<List>
+	  <ListItem>
+	    <ListItemAvatar><Avatar><InfoIcon/></Avatar></ListItemAvatar>
+	    <ListItemText primary="Motif de la consultation" secondary={reason}/>
+	  </ListItem>
+	  <ListItem>
+	    <ListItemAvatar><Avatar><DoneIcon/></Avatar></ListItemAvatar>
+	    <ListItemText primary="Examens déjà réalisés" secondary={done}/>
+	  </ListItem>
+	  <ListItem>
+	    <ListItemAvatar><Avatar><HourglassFullIcon/></Avatar></ListItemAvatar>
+	    <ListItemText primary="Examens à réaliser" secondary={todo}/>
+	  </ListItem>
+	  <ListItem>
+	    <ListItemAvatar><Avatar><EditIcon/></Avatar></ListItemAvatar>
+	    <ListItemText primary="Traitement" secondary={treatment}/>
+	  </ListItem>
+	  <ListItem>
+	    <ListItemAvatar><Avatar><AlarmIcon/></Avatar></ListItemAvatar>
+	    <ListItemText primary="À revoir" secondary={next}/>
+	  </ListItem>
+	  <ListItem>
+	    <ListItemAvatar><Avatar><WarningIcon/></Avatar></ListItemAvatar>
+	    <ListItemText primary="Autres remarques" secondary={more}/>
+	  </ListItem>
+	  { missingPaymentData ? '' :
+	  <ListItem>
+	    <ListItemAvatar><Avatar><EuroSymbolIcon/></Avatar></ListItemAvatar>
+	    <ListItemText
+	      primary="Paiement"
+	      secondary={`À payé ${Currency.format(paid, {code: currency})} de ${Currency.format(price, {code: currency})}.`}
+	    />
+	  </ListItem> }
+	  { missingPaymentData ? '' :
+	  <ListItem>
+	    <ListItemAvatar><Avatar><AccountBalanceWalletIcon/></Avatar></ListItemAvatar>
+	    <ListItemText
+	      primary="Méthode de Paiement"
+	      secondary={payment_method === 'wire' ? 'Virement' : 'Cash'}
+	    />
+	  </ListItem> }
+	  { book === '' ? '' :
+	  <ListItem>
+	    <ListItemAvatar><Avatar><BookIcon/></Avatar></ListItemAvatar>
+	    <ListItemText primary="Carnet" secondary={book}/>
+	  </ListItem> }
+	  { attachments === undefined || attachments.length === 0 ? '' :
+	  <ListItem>
+	    <ListItemAvatar><Avatar><AttachmentIcon/></Avatar></ListItemAvatar>
+	    <ListItemText
+	      disableTypography={true}
+	      primary={<Typography variant="subtitle1">{attachments.length} attachments</Typography>}
+	      secondary={
+	      (<ul>{
+		attachments.map(
+		  (attachmentId, i) => (
+		    <li key={attachmentId}>
+		      <AttachmentLink className={classes.link} attachmentId={attachmentId}/>
+		    </li>
 		  )
-		}</ul>)
-		}
-	      />
-	    </ListItem> }
-	  </List>
-	</ExpansionPanelDetails>
-	    <Divider/>
-	<ExpansionPanelActions>
-	  <Button color="primary" component={Link} to={`/edit/consultation/${_id}`}>
-	    Edit<EditIcon/>
-	  </Button>
-	  { owes && (payment_method === 'wire') && <Button color="primary" onClick={e => this.setState({ paying: true})}>
-	    Pay by Phone<SmartphoneIcon/>
-	  </Button> }
-	  { owes && <Button color="primary" onClick={e => this.setState({ settling: true})}>
-	    Settle debt<EuroSymbolIcon/>
-	  </Button> }
-	  <AttachFileButton color="primary" method="consultations.attach" item={_id}/>
-	  <Button color="secondary" onClick={e => this.setState({ deleting: true})}>
-	    Delete<DeleteIcon/>
-	  </Button>
-	  {(!owes  || payment_method !== 'wire'|| loadingPatient || !patient) ? null :
-	  <ConsultationPaymentDialog open={paying} onClose={e => this.setState({ paying: false})} consultation={this.props.consultation} patient={patient}/>
-	  }
-	  {(!owes || loadingPatient || !patient) ? null :
-	  <ConsultationDebtSettlementDialog open={settling} onClose={e => this.setState({ settling: false})} consultation={this.props.consultation} patient={patient}/>
-	  }
-	  {(loadingPatient || !patient) ? null :
-	  <ConsultationDeletionDialog open={deleting} onClose={e => this.setState({ deleting: false})} consultation={this.props.consultation} patient={patient}/>
-	  }
-	</ExpansionPanelActions>
-      </ExpansionPanel>
-    );
-  }
-
+		)
+	      }</ul>)
+	      }
+	    />
+	  </ListItem> }
+	</List>
+      </ExpansionPanelDetails>
+	  <Divider/>
+      <ExpansionPanelActions>
+	<Button color="primary" component={Link} to={`/edit/consultation/${_id}`}>
+	  Edit<EditIcon/>
+	</Button>
+	{ owes && (payment_method === 'wire') && <Button color="primary" onClick={e => setPaying(true)}>
+	  Pay by Phone<SmartphoneIcon/>
+	</Button> }
+	{ owes && <Button color="primary" onClick={e => setSettling(true)}>
+	  Settle debt<EuroSymbolIcon/>
+	</Button> }
+	<AttachFileButton color="primary" method="consultations.attach" item={_id}/>
+	<Button color="secondary" onClick={e => setDeleting(true)}>
+	  Delete<DeleteIcon/>
+	</Button>
+	{(!owes  || payment_method !== 'wire'|| loadingPatient || !patient) ? null :
+	<ConsultationPaymentDialog open={paying} onClose={e => setPaying(false)} consultation={props.consultation} patient={patient}/>
+	}
+	{(!owes || loadingPatient || !patient) ? null :
+	<ConsultationDebtSettlementDialog open={settling} onClose={e => setSettling(false)} consultation={props.consultation} patient={patient}/>
+	}
+	{(loadingPatient || !patient) ? null :
+	<ConsultationDeletionDialog open={deleting} onClose={e => setDeleting(false)} consultation={props.consultation} patient={patient}/>
+	}
+      </ExpansionPanelActions>
+    </ExpansionPanel>
+  );
 }
 
 ConsultationCard.defaultProps = {
@@ -276,7 +268,6 @@ ConsultationCard.defaultProps = {
 } ;
 
 ConsultationCard.propTypes = {
-  classes: PropTypes.object.isRequired,
   consultation: PropTypes.object.isRequired,
   patientChip: PropTypes.bool.isRequired,
   showPrice: PropTypes.bool.isRequired,
@@ -291,4 +282,4 @@ export default withTracker(({consultation}) => {
 		return { loadingPatient: false, patient } ;
 	}
 	else return { loadingPatient: true } ;
-}) ( withStyles(styles, { withTheme: true})(ConsultationCard) ) ;
+}) ( ConsultationCard ) ;
