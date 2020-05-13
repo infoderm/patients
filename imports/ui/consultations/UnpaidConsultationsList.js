4f69c26ab011baa487c2ad081d54eb7ec02acb0c
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-05-13 02:05:42 +0200

    :sparkles: feat(consultations): Add filters in UnpaidConsultationsList.

--- imports/ui/consultations/UnpaidConsultationsList.js
+++ imports/ui/consultations/UnpaidConsultationsList.js
@@ -1,10 +1,16 @@
 import { Meteor } from 'meteor/meteor' ;
 import { withTracker } from 'meteor/react-meteor-data' ;
 
-import React from 'react' ;
-import PropTypes from 'prop-types';
+import React, {Fragment, useState} from 'react' ;
 
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+
+import MoneyIcon from '@material-ui/icons/Money';
+import PaymentIcon from '@material-ui/icons/Payment';
+import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
 
 import ConsultationCard from './ConsultationCard.js';
 
@@ -13,31 +19,50 @@ import NoContent from '../navigation/NoContent.js' ;
 
 import { Consultations } from '../../api/consultations.js';
 
-const styles = theme => ({
+const useStyles = makeStyles(
+  theme => ({
 	container: {
 		padding: theme.spacing(3),
 	},
-});
+    thirdPartyToggle: {
+        position: 'fixed',
+        bottom: theme.spacing(3),
+        right: theme.spacing(3),
+    },
+    wireToggle: {
+        position: 'fixed',
+        bottom: theme.spacing(3),
+        right: theme.spacing(12),
+    },
+    cashToggle: {
+        position: 'fixed',
+        bottom: theme.spacing(3),
+        right: theme.spacing(21),
+    },
+  })
+);
 
-class UnpaidConsultationsList extends React.Component {
+function UnpaidConsultationsList ( { loading , consultations } ) {
 
-	constructor ( props ) {
-		super(props);
-	}
+	const classes = useStyles();
 
-	render ( ) {
+	const [showCash, setShowCash] = useState(true) ;
+	const [showWire, setShowWire] = useState(true) ;
+	const [showThirdParty, setShowThirdParty] = useState(false) ;
 
-		const { classes, loading } = this.props ;
+	if (loading) return <Loading/>;
 
-		if (loading) return <Loading/>;
+	const unpaidConsultations = consultations
+		.filter(consultation => consultation.paid !== consultation.price)
+		.filter(consultation => showCash || consultation.payment_method !== 'cash')
+		.filter(consultation => showWire || consultation.payment_method !== 'wire')
+		.filter(consultation => showThirdParty || consultation.payment_method !== 'third-party');
 
-		const { consultations } = this.props ;
-
-		const unpaidConsultations = consultations.filter(consultation => consultation.paid !== consultation.price);
-
-		if (unpaidConsultations.length === 0) return <NoContent>All consultations have been paid for :)</NoContent>;
-
-		return (
+	return (
+		<Fragment>
+			{ unpaidConsultations.length === 0 ?
+			<NoContent>All consultations have been paid for :)</NoContent>
+			:
 			<div className={classes.container}>
 				{
 					unpaidConsultations.map(
@@ -51,21 +76,27 @@ class UnpaidConsultationsList extends React.Component {
 					)
 				}
 			</div>
-		);
-	}
-
+			}
+			<Fab className={classes.cashToggle} color={showCash ? "primary" : "default"} onClick={() => setShowCash(!showCash)}>
+				<MoneyIcon/>
+			</Fab>
+			<Fab className={classes.wireToggle} color={showWire ? "primary" : "default"} onClick={() => setShowWire(!showWire)}>
+				<PaymentIcon/>
+			</Fab>
+			<Fab className={classes.thirdPartyToggle} color={showThirdParty ? "primary" : "default"} onClick={() => setShowThirdParty(!showThirdParty)}>
+				<AccountBalanceWalletIcon/>
+			</Fab>
+		</Fragment>
+	);
 }
-
-UnpaidConsultationsList.propTypes = {
-	classes: PropTypes.object.isRequired,
-	theme: PropTypes.object.isRequired,
-};
 
 export default withTracker(() => {
 	const handle = Meteor.subscribe('consultations.unpaid');
 	if ( !handle.ready() ) return { loading: true } ;
 	return {
 		loading: false,
-		consultations: Consultations.find({}, {sort: {datetime: 1}}).fetch() ,
+		consultations: Consultations.find({
+			isDone: true,
+		}, {sort: {datetime: 1}}).fetch() ,
 	} ;
-}) ( withStyles(styles, { withTheme: true })(UnpaidConsultationsList) );
+}) ( UnpaidConsultationsList );
