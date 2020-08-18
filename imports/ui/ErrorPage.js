50156b5a550b45652cbd326f1440bcbbc0d540ec
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-08-18 23:16:11 +0200

    :bug: fix(ErrorPage): react-router -> react-router-dom.

--- imports/ui/ErrorPage.js
+++ imports/ui/ErrorPage.js
@@ -1,5 +1,5 @@
 import React from 'react' ;
-import { useHistory } from 'react-router' ;
+import { useHistory } from 'react-router-dom' ;
 
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
