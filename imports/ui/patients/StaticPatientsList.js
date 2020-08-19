493a8e6828085504fe444a4272782d658a267d95
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-08-19 17:40:32 +0200

    :sparkles: feat(StaticPatientsList): Display NewPatientCard when page 1 is empty.

--- imports/ui/patients/StaticPatientsList.js
+++ imports/ui/patients/StaticPatientsList.js
@@ -1,11 +1,14 @@
 import React, {Fragment} from 'react' ;
 import PropTypes from 'prop-types';
 
+import Grid from '@material-ui/core/Grid' ;
+
 import Loading from '../navigation/Loading.js';
 import NoContent from '../navigation/NoContent.js';
 import Paginator from '../navigation/Paginator.js' ;
 
 import PatientsPage from './PatientsPage.js';
+import NewPatientCard from './NewPatientCard.js' ;
 
 export default function StaticPatientsList ( { page , perpage , loading , patients , root } ) {
 
@@ -16,6 +19,15 @@ export default function StaticPatientsList ( { page , perpage , loading , patien
           <Loading/>
           : patients.length ?
           <PatientsPage patients={patients}/>
+          : page === 1 ?
+          <div>
+            <NoContent>No patients match the current query.</NoContent>
+            <Grid container spacing={3}>
+              <Grid item sm="auto" md="auto" lg={3} xl={4}/>
+              <NewPatientCard/>
+              <Grid item sm="auto" md="auto" lg={3} xl={4}/>
+            </Grid>
+          </div>
           :
           <NoContent>{`Nothing to see on page ${page}.`}</NoContent>
         }
