e8415999edfe27a9a287683b3529c771d5baba52
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-08-19 17:39:36 +0200

    :recycle: refactor(PatientsPage): Extract NewPatientCard.

--- imports/ui/patients/PatientsPage.js
+++ imports/ui/patients/PatientsPage.js
@@ -1,41 +1,18 @@
 import React from 'react' ;
-import { Link } from 'react-router-dom'
 
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types' ;
 
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import FaceIcon from '@material-ui/icons/Face';
+import Grid from '@material-ui/core/Grid' ;
 
-import PatientCard from './PatientCard.js';
-
-const useStyles = makeStyles(
-  theme => ({
-    buttonTile: {
-      minHeight: 200,
-      width: '100%',
-      fontSize: '1rem',
-    },
-    rightIcon: {
-      marginLeft: theme.spacing(1),
-    },
-  })
-);
+import PatientCard from './PatientCard.js' ;
+import NewPatientCard from './NewPatientCard.js' ;
 
 export default function PatientsPage ( { patients } ) {
-
-  const classes = useStyles() ;
 
   return (
       <div>
         <Grid container spacing={3}>
-          <Grid item sm={12} md={12} lg={6} xl={4}>
-            <Button variant="contained" className={classes.buttonTile} component={Link} to="/new/patient">
-              Add a new patient
-              <FaceIcon className={classes.rightIcon}/>
-            </Button>
-          </Grid>
+          <NewPatientCard/>
           { patients.map(patient => ( <PatientCard key={patient._id} patient={patient}/> )) }
         </Grid>
       </div>
