5eb381b9e3f3c1b603c7cd4a4b3f181499ecc687
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-08-20 11:31:18 +0200

    :recycle: refactor(AccountsUI): Rewrite as functional.

--- imports/ui/users/AccountsUI.js
+++ imports/ui/users/AccountsUI.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -11,78 +11,62 @@ import CloseIcon from '@material-ui/icons/Close';
 import Dashboard from './Dashboard.js';
 import SignInForm from './SignInForm.js';
 
-class AccountsUI extends React.Component {
+const useStyles = makeStyles(
+	theme => ({
+		close: {
+			width: theme.spacing(4),
+			height: theme.spacing(4),
+		},
+		snackbar: {
+		},
+	})
+);
 
-	constructor ( props ) {
-		super(props);
-		this.state = {
-			open: false,
-			message: '',
-		};
-	}
+export default function AccountsUI ( { currentUser , ...rest } ) {
 
-	render ( ) {
+	const classes = useStyles();
+	const [open, setOpen] = useState(false) ;
+	const [message, setMessage] = useState('') ;
 
-		const { currentUser , classes , ...rest } = this.props ;
+	const feedback = message => {
+		setMessage(message);
+		setOpen(true);
+	};
 
-		const { open , message } = this.state ;
+	const handleClose = (event, reason) => {
+		if (reason === 'clickaway') return;
+		setOpen(false);
+	};
 
-		const feedback = message => {
-			this.setState({ open: true, message });
-		};
-
-		const handleClose = (event, reason) => {
-			if (reason === 'clickaway') return;
-			this.setState({ open: false });
-		};
-
-		return (
-			<div {...rest}>
-				{ currentUser ? <Dashboard currentUser={currentUser} feedback={feedback}/> : <SignInForm feedback={feedback}/> }
-				<Snackbar
-					className={classes.snackbar}
-					anchorOrigin={{
-						vertical: 'bottom',
-						horizontal: 'center',
-					}}
-					open={open}
-					autoHideDuration={6000}
-					onClose={handleClose}
-					SnackbarContentProps={{
-						'aria-describedby': 'account-ui-snackbar-message',
-					}}
-					message={<span id="account-ui-snackbar-message">{message}</span>}
-					action={[
-					<IconButton
-						key="close"
-						aria-label="Close"
-						color="inherit"
-						className={classes.close}
-						onClick={handleClose}
-					>
-						<CloseIcon/>
+	return (
+		<div {...rest}>
+			{ currentUser ? <Dashboard currentUser={currentUser} feedback={feedback}/> : <SignInForm feedback={feedback}/> }
+			<Snackbar
+				className={classes.snackbar}
+				anchorOrigin={{
+					vertical: 'bottom',
+					horizontal: 'center',
+				}}
+				open={open}
+				autoHideDuration={6000}
+				onClose={handleClose}
+				SnackbarContentProps={{
+				'aria-describedby': 'account-ui-snackbar-message',
+				}}
+				message={<span id="account-ui-snackbar-message">{message}</span>}
+				action={[
+				<IconButton
+					key="close"
+					aria-label="Close"
+					color="inherit"
+					className={classes.close}
+					onClick={handleClose}
+				>
+					<CloseIcon/>
 					</IconButton>,
-					]}
-				/>
-			</div>
-		) ;
-
-	}
+				]}
+			/>
+		</div>
+	) ;
 
 }
-
-
-const styles = theme => ({
-  close: {
-    width: theme.spacing(4),
-    height: theme.spacing(4),
-  },
-  snackbar: {
-  },
-});
-
-AccountsUI.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(AccountsUI);
