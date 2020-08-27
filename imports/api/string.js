48f88d60d4af48db154a82bee45a22eca340f17e
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-08-27 10:14:14 +0200

    :sparkles: feat(shatter): Implement custom text indexing method.

--- imports/api/string.js
+++ imports/api/string.js
@@ -1,6 +1,7 @@
 import deburr from 'lodash.deburr';
 
-import {all, map} from '@aureooms/js-itertools';
+import {all, map, sorted} from '@aureooms/js-itertools';
+import {len, decreasing} from '@aureooms/js-compare';
 
 const normalized = (string) => {
 	string = string.toLowerCase();
@@ -29,4 +30,110 @@ const makeIndex = (data) => {
 	};
 };
 
-export {normalized, onlyASCII, onlyLowerCaseASCII, makeIndex};
+const PARTICLES_FR = ['du', 'de', 'des', "d'", 'le', 'la'];
+const PARTICLES_NL = [
+	'de',
+	'den',
+	'op',
+	"t'",
+	"'t",
+	'ten',
+	'ter',
+	'te',
+	'van',
+	'der'
+];
+const PARTICLES_DE = [
+	'am',
+	'an',
+	'af',
+	'auf',
+	'aus',
+	'der',
+	'im',
+	'von',
+	'und',
+	'zu',
+	'zum',
+	'zur'
+];
+
+const PARTICLES = new Set([...PARTICLES_FR, ...PARTICLES_NL, ...PARTICLES_DE]);
+const PARTICLES_ORDERED = sorted(len(decreasing), PARTICLES);
+
+const words = (string) => string.trim().split(/\s+/);
+
+function* splitParticles(data) {
+	const queue = words(data).reverse();
+	outer: while (queue.length !== 0) {
+		const word = queue.pop();
+		// greedy match
+		// TODO use prefix tree
+		for (const particle of PARTICLES_ORDERED) {
+			if (word.startsWith(particle)) {
+				yield particle;
+				const rest = word.slice(particle.length);
+				if (rest) queue.push(rest);
+				continue outer;
+			}
+		}
+
+		yield word;
+	}
+}
+
+const normalizeSearch = (data) =>
+	[...splitParticles(onlyLowerCaseASCII(data))].join(' ');
+
+function* nonEmptySubstrings(string) {
+	const n = string.length;
+	for (let i = 0; i < n; ++i) {
+		for (let j = i + 1; j <= n; ++j) {
+			yield string.slice(i, j);
+		}
+	}
+}
+
+const SHATTER_SHORT = 2;
+const SHATTER_MEDIUM = 4;
+const SHATTER_LONG = 6;
+
+const shatter = (data) => {
+	const parts = splitParticles(onlyLowerCaseASCII(data));
+	const index = {
+		whole: [],
+		particles: [],
+		substring_long: [],
+		substring_medium: [],
+		substring_short: []
+	};
+	for (const part of parts) {
+		if (PARTICLES.has(part)) {
+			index.particles.push(part);
+		} else {
+			for (const _part of part.split(/[^a-z]+/)) {
+				index.whole.push(_part);
+				for (const substring of nonEmptySubstrings(_part)) {
+					if (substring.length < SHATTER_SHORT) continue;
+					else if (substring.length === _part.length) continue;
+					else if (substring.length < SHATTER_MEDIUM)
+						index.substring_short.push(substring);
+					else if (substring.length < SHATTER_LONG)
+						index.substring_medium.push(substring);
+					else index.substring_long.push(substring);
+				}
+			}
+		}
+	}
+
+	return index;
+};
+
+export {
+	normalized,
+	onlyASCII,
+	onlyLowerCaseASCII,
+	makeIndex,
+	shatter,
+	normalizeSearch
+};
