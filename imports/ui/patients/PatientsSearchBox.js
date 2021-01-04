ddaa5658c2a9bb53679508dd99b7f5318d6539da
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-01-04 16:09:24 +0100

    :bug: fix(PatientsSearchBox): Correctly clear selection after selection.

--- imports/ui/patients/PatientsSearchBox.js
+++ imports/ui/patients/PatientsSearchBox.js
@@ -2,32 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import {useHistory} from 'react-router-dom';
-import Downshift from 'downshift';
 
 import {patients} from '../../api/patients.js';
 const patientFilter = patients.filter;
 
 import SearchBoxWithSuggestions from '../input/SearchBoxWithSuggestions.js';
 
-const reduceState = (state, changes) => {
-	switch (changes.type) {
-		case Downshift.stateChangeTypes.keyDownEnter:
-		case Downshift.stateChangeTypes.clickItem:
-			return {
-				...changes,
-				inputValue: ''
-			};
-		default:
-			return changes;
-	}
-};
-
 export default function PatientsSearchBox({patients, className}) {
 	const history = useHistory();
 
-	const handleChange = (selectedItem, _downshiftState) => {
+	const handleChange = (selectedItem, {clearSelection}) => {
 		if (selectedItem) {
 			history.push(`/patient/${selectedItem._id}`);
+			clearSelection();
 		}
 	};
 
@@ -43,7 +30,6 @@ export default function PatientsSearchBox({patients, className}) {
 				suggestions={suggestions}
 				itemToString={(item) => (item ? item.label : '')}
 				itemToKey={(item) => item._id}
-				stateReducer={reduceState}
 				placeholder="Search a patient…"
 				onChange={handleChange}
 			/>
