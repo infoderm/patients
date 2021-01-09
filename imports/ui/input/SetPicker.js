ec7c41adfc077a039df38bd8f09306fb2a619b63
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-01-09 08:31:05 +0100

    :bug: fix(SetPicker): Avoid potential bug...
    ... where `inputTransform(inputValue).trim()`
    !== `itemToString(createNewItem(inputTransform(inputValue).trim()))`
    
    Needs more patching in the future.

--- imports/ui/input/SetPicker.js
+++ imports/ui/input/SetPicker.js
@@ -156,13 +156,16 @@ class SetPicker extends React.Component {
 					this.highlightedIndex === null &&
 					createNewItem
 				) {
-					const item = inputValue.trim();
+					// TODO avoid creating new item before multiset check
+					// and/or handle item creation failure
+					const item = createNewItem(inputValue.trim());
+					const itemString = itemToString(item);
 					const newValue = value.slice();
 					if (
 						multiset ||
-						all(map((x) => x !== item, map(itemToString, value)))
+						all(map((x) => x !== itemString, map(itemToString, value)))
 					) {
-						newValue.push(createNewItem(item));
+						newValue.push(item);
 						if (sort) {
 							sort(newValue);
 						}
