577ccb172debb69ae2c806558487dd834d818d58
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-01-20 12:03:55 +0100

    :recycle: refactor(makeQuery): Reduce complexity.
    Side-effect: reduces flicker in /consultations (fixes #138).

--- imports/api/makeQuery.js
+++ imports/api/makeQuery.js
@@ -1,28 +1,16 @@
 import {Meteor} from 'meteor/meteor';
 import {useTracker} from 'meteor/react-meteor-data';
-import {useState, useEffect} from 'react';
-
-// TODO do not use useState and useEffect, simply exploit handle.ready();
 
 const makeQuery = (Collection, subscription) => (query, options, deps) => {
-	const [loading, setLoading] = useState(true);
-	const [results, setResults] = useState([]);
-
-	useEffect(() => {
-		setLoading(true);
-	}, deps);
-
-	useTracker(() => {
+	return useTracker(() => {
 		const handle = Meteor.subscribe(subscription, query, options);
 
-		if (handle.ready()) {
-			const results = Collection.find(query, options).fetch();
-			setLoading(false);
-			setResults(results);
-		}
-	}, deps);
+		return {
+			loading: !handle.ready(),
+			results: Collection.find(query, options).fetch()
+		};
 
-	return {loading, results};
+	}, deps);
 };
 
 export default makeQuery;
