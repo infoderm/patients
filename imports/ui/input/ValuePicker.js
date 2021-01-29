619eb1dcaf032db689365a6ae1d7e8c6e2dcff15
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-01-29 14:43:43 +0100

    :recycle: refactor(ValuePicker): Rewrite as function component.

--- imports/ui/input/ValuePicker.js
+++ imports/ui/input/ValuePicker.js
@@ -10,52 +10,50 @@ import MenuItem from '@material-ui/core/MenuItem';
 const MAGIC = '8204932840';
 let nextId = 0;
 
-class ValuePicker extends React.Component {
-	render() {
-		const {
-			label,
-			name,
-			value,
-			onChange,
-			options,
-			optionToString,
-			pairToKey,
-			...rest
-		} = this.props;
+const ValuePicker = (props) => {
+	const {
+		label,
+		name,
+		value,
+		onChange,
+		options,
+		optionToString,
+		pairToKey,
+		...rest
+	} = props;
 
-		let {id} = this.props;
+	let {id} = props;
 
-		if (id === undefined && label) {
-			id = `value-picker-${MAGIC}-${nextId++}`;
-		}
-
-		const input = <Input id={id} />;
-
-		const inputProps = {
-			name,
-			id
-		};
-
-		return (
-			<FormControl>
-				{label && <InputLabel htmlFor={id}>{label}</InputLabel>}
-				<Select
-					value={value}
-					input={input}
-					inputProps={inputProps}
-					onChange={onChange}
-					{...rest}
-				>
-					{options.map((option, index) => (
-						<MenuItem key={pairToKey(option, index)} value={option}>
-							{optionToString(option)}
-						</MenuItem>
-					))}
-				</Select>
-			</FormControl>
-		);
+	if (id === undefined && label) {
+		id = `value-picker-${MAGIC}-${nextId++}`;
 	}
-}
+
+	const input = <Input id={id} />;
+
+	const inputProps = {
+		name,
+		id
+	};
+
+	return (
+		<FormControl>
+			{label && <InputLabel htmlFor={id}>{label}</InputLabel>}
+			<Select
+				value={value}
+				input={input}
+				inputProps={inputProps}
+				onChange={onChange}
+				{...rest}
+			>
+				{options.map((option, index) => (
+					<MenuItem key={pairToKey(option, index)} value={option}>
+						{optionToString(option)}
+					</MenuItem>
+				))}
+			</Select>
+		</FormControl>
+	);
+};
 
 ValuePicker.defaultProps = {
 	optionToString: (x) => x.toString(),
