6fcf4cae4616ff99f71c72914351681959b46b03
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-02-01 17:22:52 +0100

    :recycle: refactor(ConsultationForm): Rewrite using hooks.
    Fixes #99.

--- imports/ui/consultations/ConsultationForm.js
+++ imports/ui/consultations/ConsultationForm.js
@@ -1,13 +1,12 @@
 import {Meteor} from 'meteor/meteor';
-import {withTracker} from 'meteor/react-meteor-data';
 
-import React from 'react';
+import React, {useReducer} from 'react';
 import PropTypes from 'prop-types';
 
-import {withRouter} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 import {Prompt} from 'react-router';
 
-import {withStyles} from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
@@ -20,7 +19,7 @@ import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
 
 import {format} from 'date-fns';
 
-import {Patients} from '../../api/patients.js';
+import usePatient from '../patients/usePatient.js';
 import {books, useBooksFind} from '../../api/books.js';
 import {computeFixedFabStyle} from '../button/FixedFab.js';
 import AutocompleteWithSuggestions from '../input/AutocompleteWithSuggestions.js';
@@ -57,65 +56,120 @@ const styles = (theme) => ({
 	doneButton: computeFixedFabStyle({theme, col: 1})
 });
 
-class ConsultationForm extends React.Component {
-	constructor(props) {
-		super(props);
+const useStyles = makeStyles(styles);
 
-		const {consultation} = props;
+const defaultState = {
+	date: '1970-01-01',
+	time: '00:00',
+	reason: '',
+	done: '',
+	todo: '',
+	treatment: '',
+	next: '',
+	more: '',
 
-		this.state = {
-			patientId: consultation.patientId,
-			date: format(consultation.datetime, 'yyyy-MM-dd'),
-			time: format(consultation.datetime, 'HH:mm'),
-			reason: consultation.reason ?? '',
-			done: consultation.done ?? '',
-			todo: consultation.todo ?? '',
-			treatment: consultation.treatment ?? '',
-			next: consultation.next ?? '',
-			more: consultation.more ?? '',
+	currency: 'EUR',
+	payment_method: 'cash',
+	price: '',
+	paid: '',
+	book: '',
 
-			currency: consultation.currency ?? 'EUR',
-			payment_method: consultation.payment_method ?? 'cash',
-			price: consultation.price ?? '',
-			paid: consultation.paid ?? '',
-			book: consultation.book ?? '',
+	syncPaid: true,
+	dirty: false
+};
 
-			syncPaid: true,
-			dirty: false
-		};
+const init = (consultation) => ({
+	...defaultState,
+	date: format(consultation.datetime, 'yyyy-MM-dd'),
+	time: format(consultation.datetime, 'HH:mm'),
+	reason: consultation.reason,
+	done: consultation.done,
+	todo: consultation.todo,
+	treatment: consultation.treatment,
+	next: consultation.next,
+	more: consultation.more,
+
+	currency: consultation.currency,
+	payment_method: consultation.payment_method,
+	price: consultation.price,
+	paid: consultation.paid,
+	book: consultation.book
+});
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'update':
+			return {...state, [action.key]: action.value, dirty: true};
+		case 'not-dirty':
+			return {...state, dirty: false};
+		case 'update-paid':
+			return {...state, paid: action.value, syncPaid: false, dirty: true};
+		case 'update-price':
+			return {
+				...state,
+				price: action.value,
+				paid:
+					state.syncPaid && state.payment_method === 'cash'
+						? action.value
+						: state.paid,
+				dirty: true
+			};
+		default:
+			throw new Error(`Unknown action type ${action.type}.`);
 	}
+};
 
-	handleSubmit = (setDoneDatetime) => (event) => {
+const ConsultationForm = ({consultation}) => {
+	const classes = useStyles();
+	const history = useHistory();
+
+	const {_id: consultationId, patientId, doneDatetime} = consultation;
+
+	const [state, dispatch] = useReducer(reducer, consultation, init);
+
+	const options = {
+		fields: {
+			niss: 1,
+			firstname: 1,
+			lastname: 1,
+			photo: 1
+		}
+	};
+
+	const {
+		date,
+		time,
+		reason,
+		done,
+		todo,
+		treatment,
+		next,
+		more,
+
+		currency,
+		payment_method,
+		price,
+		paid,
+		book,
+
+		dirty
+	} = state;
+
+	const deps = [patientId];
+
+	const {loading: loadingPatient, fields: patient} = usePatient(
+		{},
+		patientId,
+		options,
+		deps
+	);
+
+	const handleSubmit = (setDoneDatetime) => (event) => {
 		event.preventDefault();
-
-		const {
-			history,
-			consultation: {_id: consultationId}
-		} = this.props;
-
-		const {
-			patientId,
-			date,
-			time,
-			reason,
-			done,
-			todo,
-			treatment,
-			next,
-			more,
-
-			currency,
-			payment_method,
-			price,
-			paid,
-			book
-		} = this.state;
-
-		const datetime = new Date(`${date}T${time}`);
 
 		const consultation = {
 			patientId,
-			datetime,
+			datetime: new Date(`${date}T${time}`),
 			reason,
 			done,
 			todo,
@@ -139,9 +193,7 @@ class ConsultationForm extends React.Component {
 					if (err) {
 						console.error(err);
 					} else {
-						this.setState({
-							dirty: false
-						});
+						dispatch({type: 'not-dirty'});
 						history.push({pathname: `/consultation/${_id}`});
 					}
 				}
@@ -156,9 +208,7 @@ class ConsultationForm extends React.Component {
 					if (err) {
 						console.error(err);
 					} else {
-						this.setState({
-							dirty: false
-						});
+						dispatch({type: 'not-dirty'});
 						history.push({pathname: `/consultation/${consultationId}`});
 					}
 				}
@@ -166,300 +216,250 @@ class ConsultationForm extends React.Component {
 		}
 	};
 
-	render() {
-		const {
-			classes,
-			consultation: {doneDatetime},
-			loadingPatient,
-			patient
-		} = this.props;
+	const update = (key, transform = (x) => x) => (e) => {
+		dispatch({type: 'update', key, value: transform(e.target.value)});
+	};
 
-		const update = (key, transform = (x) => x) => (e) => {
-			this.setState({
-				[key]: transform(e.target.value),
-				dirty: true
-			});
-		};
-
-		return (
-			<div className={classes.container}>
-				<Prompt
-					when={this.state.dirty}
-					message="You are trying to leave the page without saving your changes. Are you sure you want to continue?"
-				/>
-				<Grid container className={classes.header} spacing={3}>
-					{loadingPatient || !patient || !patient.photo ? (
-						''
-					) : (
-						<Grid item xs={1}>
-							<Avatar
-								alt={`${patient.firstname} ${patient.lastname}`}
-								src={`data:image/png;base64,${patient.photo}`}
-								className={classes.avatar}
-							/>
-						</Grid>
-					)}
-					{loadingPatient || !patient ? (
-						''
-					) : (
-						<Grid item xs={2}>
-							<TextField
-								inputProps={{readOnly: true}}
-								label="Lastname"
-								value={patient.lastname}
-							/>
-						</Grid>
-					)}
-					{loadingPatient || !patient ? (
-						''
-					) : (
-						<Grid item xs={2}>
-							<TextField
-								inputProps={{readOnly: true}}
-								label="Firstname"
-								value={patient.firstname}
-							/>
-						</Grid>
-					)}
-					{loadingPatient || !patient ? (
-						''
-					) : (
-						<Grid item xs={2}>
-							<TextField
-								inputProps={{readOnly: true}}
-								label="NISS"
-								value={patient.niss}
-							/>
-						</Grid>
-					)}
-					{!loadingPatient && patient ? (
-						''
-					) : (
-						<Grid item xs={2}>
-							<TextField
-								disabled
-								label="Patient id"
-								value={this.state.patientId}
-							/>
-						</Grid>
-					)}
+	return (
+		<div className={classes.container}>
+			<Prompt
+				when={dirty}
+				message="You are trying to leave the page without saving your changes. Are you sure you want to continue?"
+			/>
+			<Grid container className={classes.header} spacing={3}>
+				{loadingPatient || !patient || !patient.photo ? null : (
+					<Grid item xs={1}>
+						<Avatar
+							alt={`${patient.firstname} ${patient.lastname}`}
+							src={`data:image/png;base64,${patient.photo}`}
+							className={classes.avatar}
+						/>
+					</Grid>
+				)}
+				{loadingPatient || !patient ? null : (
 					<Grid item xs={2}>
 						<TextField
-							type="date"
-							label="Date"
-							InputLabelProps={{
-								shrink: true
-							}}
-							value={this.state.date}
-							onChange={update('date')}
+							inputProps={{readOnly: true}}
+							label="Lastname"
+							value={patient.lastname}
 						/>
 					</Grid>
-					<Grid item xs={1}>
+				)}
+				{loadingPatient || !patient ? null : (
+					<Grid item xs={2}>
 						<TextField
-							type="time"
-							label="Time"
-							InputLabelProps={{
-								shrink: true
-							}}
-							value={this.state.time}
-							onChange={update('time')}
+							inputProps={{readOnly: true}}
+							label="Firstname"
+							value={patient.firstname}
 						/>
 					</Grid>
+				)}
+				{loadingPatient || !patient ? null : (
+					<Grid item xs={2}>
+						<TextField
+							inputProps={{readOnly: true}}
+							label="NISS"
+							value={patient.niss}
+						/>
+					</Grid>
+				)}
+				{!loadingPatient && patient ? null : (
+					<Grid item xs={2}>
+						<TextField disabled label="Patient id" value={patientId} />
+					</Grid>
+				)}
+				<Grid item xs={2}>
+					<TextField
+						type="date"
+						label="Date"
+						InputLabelProps={{
+							shrink: true
+						}}
+						value={date}
+						onChange={update('date')}
+					/>
 				</Grid>
-				<Grid container className={classes.form} spacing={3}>
-					<Grid item xs={12}>
-						<TextField
-							autoFocus
-							multiline
-							label="Motif de la visite"
-							placeholder="Motif de la visite"
-							rows={4}
-							className={classes.multiline}
-							value={this.state.reason}
-							margin="normal"
-							onChange={update('reason')}
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						<TextField
-							multiline
-							label="Examens déjà réalisés"
-							placeholder="Examens déjà réalisés"
-							rows={4}
-							className={classes.multiline}
-							value={this.state.done}
-							margin="normal"
-							onChange={update('done')}
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						<TextField
-							multiline
-							label="Examens à réaliser"
-							placeholder="Examens à réaliser"
-							rows={4}
-							className={classes.multiline}
-							value={this.state.todo}
-							margin="normal"
-							onChange={update('todo')}
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						<TextField
-							multiline
-							label="Traitement"
-							placeholder="Traitement"
-							rows={4}
-							className={classes.multiline}
-							value={this.state.treatment}
-							margin="normal"
-							onChange={update('treatment')}
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						<TextField
-							multiline
-							label="À revoir"
-							placeholder="À revoir"
-							rows={4}
-							className={classes.multiline}
-							value={this.state.next}
-							margin="normal"
-							onChange={update('next')}
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						<TextField
-							multiline
-							label="Autres remarques"
-							placeholder="Write some additional information about the consultation here"
-							rows={4}
-							className={classes.multiline}
-							value={this.state.more}
-							margin="normal"
-							onChange={update('more')}
-						/>
-					</Grid>
+				<Grid item xs={1}>
+					<TextField
+						type="time"
+						label="Time"
+						InputLabelProps={{
+							shrink: true
+						}}
+						value={time}
+						onChange={update('time')}
+					/>
+				</Grid>
+			</Grid>
+			<Grid container className={classes.form} spacing={3}>
+				<Grid item xs={12}>
+					<TextField
+						autoFocus
+						multiline
+						label="Motif de la visite"
+						placeholder="Motif de la visite"
+						rows={4}
+						className={classes.multiline}
+						value={reason}
+						margin="normal"
+						onChange={update('reason')}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<TextField
+						multiline
+						label="Examens déjà réalisés"
+						placeholder="Examens déjà réalisés"
+						rows={4}
+						className={classes.multiline}
+						value={done}
+						margin="normal"
+						onChange={update('done')}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<TextField
+						multiline
+						label="Examens à réaliser"
+						placeholder="Examens à réaliser"
+						rows={4}
+						className={classes.multiline}
+						value={todo}
+						margin="normal"
+						onChange={update('todo')}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<TextField
+						multiline
+						label="Traitement"
+						placeholder="Traitement"
+						rows={4}
+						className={classes.multiline}
+						value={treatment}
+						margin="normal"
+						onChange={update('treatment')}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<TextField
+						multiline
+						label="À revoir"
+						placeholder="À revoir"
+						rows={4}
+						className={classes.multiline}
+						value={next}
+						margin="normal"
+						onChange={update('next')}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<TextField
+						multiline
+						label="Autres remarques"
+						placeholder="Write some additional information about the consultation here"
+						rows={4}
+						className={classes.multiline}
+						value={more}
+						margin="normal"
+						onChange={update('more')}
+					/>
+				</Grid>
 
-					<Grid item xs={1}>
-						<TextField
-							select
-							label="Currency"
-							value={this.state.currency}
-							margin="normal"
-							onChange={update('currency')}
-						>
-							<MenuItem value="EUR">€</MenuItem>
-						</TextField>
-					</Grid>
-					<Grid item xs={1}>
-						<TextField
-							select
-							label="Payment Method"
-							value={this.state.payment_method}
-							margin="normal"
-							onChange={update('payment_method')}
-						>
-							<MenuItem value="cash">cash</MenuItem>
-							<MenuItem value="wire">virement</MenuItem>
-							<MenuItem value="third-party">tiers payant</MenuItem>
-						</TextField>
-					</Grid>
-					<Grid item xs={3}>
-						<TextField
-							label="Prix"
-							value={this.state.price}
-							margin="normal"
-							error={!/^\d+$/.test(this.state.price)}
-							onChange={(e) => {
-								const price = e.target.value;
-								this.setState((state) => ({
-									price,
-									paid:
-										state.syncPaid && state.payment_method === 'cash'
-											? price
-											: state.paid,
-									dirty: true
-								}));
-							}}
-						/>
-					</Grid>
-					<Grid item xs={3}>
-						<TextField
-							label="Payé"
-							value={this.state.paid}
-							margin="normal"
-							error={!/^\d+$/.test(this.state.paid)}
-							onChange={(e) =>
-								this.setState({
-									paid: e.target.value,
-									syncPaid: false,
-									dirty: true
-								})
+				<Grid item xs={1}>
+					<TextField
+						select
+						label="Currency"
+						value={currency}
+						margin="normal"
+						onChange={update('currency')}
+					>
+						<MenuItem value="EUR">€</MenuItem>
+					</TextField>
+				</Grid>
+				<Grid item xs={1}>
+					<TextField
+						select
+						label="Payment Method"
+						value={payment_method}
+						margin="normal"
+						onChange={update('payment_method')}
+					>
+						<MenuItem value="cash">cash</MenuItem>
+						<MenuItem value="wire">virement</MenuItem>
+						<MenuItem value="third-party">tiers payant</MenuItem>
+					</TextField>
+				</Grid>
+				<Grid item xs={3}>
+					<TextField
+						label="Prix"
+						value={price}
+						margin="normal"
+						error={!/^\d+$/.test(price)}
+						onChange={({target: {value}}) =>
+							dispatch({type: 'update-price', value})
+						}
+					/>
+				</Grid>
+				<Grid item xs={3}>
+					<TextField
+						label="Payé"
+						value={paid}
+						margin="normal"
+						error={!/^\d+$/.test(paid)}
+						onChange={({target: {value}}) =>
+							dispatch({type: 'update-paid', value})
+						}
+					/>
+				</Grid>
+				<Grid item xs={3}>
+					<AutocompleteWithSuggestions
+						itemToString={(x) => (x ? x.bookNumber : '')}
+						useSuggestions={makeSubstringSuggestions(
+							useBooksFind,
+							[],
+							'bookNumber'
+						)}
+						TextFieldProps={{
+							label: 'Carnet',
+							margin: 'normal'
+						}}
+						inputValue={book}
+						onInputChange={(event, newInputValue) => {
+							if (event) {
+								return update(
+									'book',
+									books.sanitizeInput
+								)({target: {value: newInputValue}});
 							}
-						/>
-					</Grid>
-					<Grid item xs={3}>
-						<AutocompleteWithSuggestions
-							itemToString={(x) => (x ? x.bookNumber : '')}
-							useSuggestions={makeSubstringSuggestions(
-								useBooksFind,
-								[],
-								'bookNumber'
-							)}
-							TextFieldProps={{
-								label: 'Carnet',
-								margin: 'normal'
-							}}
-							inputValue={this.state.book}
-							onInputChange={(event, newInputValue) => {
-								if (event) {
-									return update(
-										'book',
-										books.sanitizeInput
-									)({target: {value: newInputValue}});
-								}
-							}}
-						/>
-					</Grid>
+						}}
+					/>
 				</Grid>
-				<Fab
-					className={classes.saveButton}
-					color={doneDatetime ? 'primary' : 'secondary'}
-					aria-label="save"
-					disabled={!this.state.dirty}
-					onClick={this.handleSubmit(false)}
-				>
-					<SaveIcon />
-				</Fab>
-				<Fab
-					className={classes.doneButton}
-					color={!doneDatetime ? 'primary' : 'secondary'}
-					aria-label="done"
-					disabled={!this.state.dirty}
-					onClick={this.handleSubmit(true)}
-				>
-					<AssignmentTurnedInIcon />
-				</Fab>
-			</div>
-		);
-	}
-}
-
-ConsultationForm.propTypes = {
-	classes: PropTypes.object.isRequired,
-	history: PropTypes.object.isRequired
+			</Grid>
+			<Fab
+				className={classes.saveButton}
+				color={doneDatetime ? 'primary' : 'secondary'}
+				aria-label="save"
+				disabled={!dirty}
+				onClick={handleSubmit(false)}
+			>
+				<SaveIcon />
+			</Fab>
+			<Fab
+				className={classes.doneButton}
+				color={!doneDatetime ? 'primary' : 'secondary'}
+				aria-label="done"
+				disabled={!dirty}
+				onClick={handleSubmit(true)}
+			>
+				<AssignmentTurnedInIcon />
+			</Fab>
+		</div>
+	);
 };
 
-export default withRouter(
-	withTracker(({consultation}) => {
-		const _id = consultation.patientId;
-		const handle = Meteor.subscribe('patient', _id);
-		if (handle.ready()) {
-			const patient = Patients.findOne(_id);
-			return {loadingPatient: false, patient};
-		}
+ConsultationForm.propTypes = {
+	consultation: PropTypes.object.isRequired
+};
 
-		return {loadingPatient: true};
-	})(withStyles(styles, {withTheme: true})(ConsultationForm))
-);
+export default ConsultationForm;
