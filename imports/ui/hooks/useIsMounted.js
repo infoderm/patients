f1da788acc5404b2a3f7209245d6731dea5a9bd9
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-02-01 13:58:33 +0100

    :sparkles: feat(useIsMounted): New hook.

---
+++ imports/ui/hooks/useIsMounted.js
@@ -0,0 +1,20 @@
+import {useRef, useEffect} from 'react';
+
+// See https://gist.github.com/jaydenseric/a67cfb1b809b1b789daa17dfe6f83daa
+const useIsMounted = () => {
+	// Component is certainly mounted from the beginning
+	const componentIsMounted = useRef(true);
+
+	useEffect(() => {
+		// When non-SSR + (ComponentDidMount or ComponentDidUpdate):
+		// do nothing.
+		// when non-SSR + ComponentWillUnmount:
+		return () => {
+			componentIsMounted.current = false;
+		};
+	}, []);
+
+	return () => componentIsMounted.current;
+};
+
+export default useIsMounted;
