a26179b964a1cd2465014f6ce028f136ecab6a81
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-02-03 14:11:53 +0100

    :rocket: perf(stats): Make freq sub reactive.

--- imports/api/stats.js
+++ imports/api/stats.js
@@ -170,52 +170,77 @@ if (Meteor.isServer) {
 		});
 
 		const inc = (patientId) => {
-			if (!pRefs.has(patientId)) {
-				const entry = {
-					...Patients.findOne(patientId, {fields: {sex: 1}}), // TODO make reactive
-					count: 0
-				};
-				pRefs.set(patientId, entry);
-				if (count[0][entry.sex] === undefined) count[0][entry.sex] = 0;
-				count[0][entry.sex] += 1;
-			}
-
+			if (!pRefs.has(patientId))
+				throw new Error(`inc: patientId ${patientId} does not exist`);
 			const patient = pRefs.get(patientId);
-			count[patient.count][patient.sex] -= 1;
-			patient.count += 1;
-			if (count[patient.count] === undefined) count[patient.count] = {};
-			if (count[patient.count][patient.sex] === undefined)
-				count[patient.count][patient.sex] = 0;
-			count[patient.count][patient.sex] += 1;
+			count[patient.freq][patient.sex] -= 1;
+			patient.freq += 1;
+			if (count[patient.freq] === undefined) count[patient.freq] = {};
+			if (count[patient.freq][patient.sex] === undefined)
+				count[patient.freq][patient.sex] = 0;
+			count[patient.freq][patient.sex] += 1;
 		};
 
 		const dec = (patientId) => {
+			if (!pRefs.has(patientId))
+				throw new Error(`dec: patientId ${patientId} does not exist`);
 			const patient = pRefs.get(patientId);
-			count[patient.count][patient.sex] -= 1;
-			patient.count -= 1;
-			if (count[patient.count] === undefined) count[patient.count] = {};
-			if (count[patient.count][patient.sex] === undefined)
-				count[patient.count][patient.sex] = 0;
-			count[patient.count][patient.sex] += 1;
+			count[patient.freq][patient.sex] -= 1;
+			patient.freq -= 1;
+			if (count[patient.freq] === undefined) count[patient.freq] = {};
+			if (count[patient.freq][patient.sex] === undefined)
+				count[patient.freq][patient.sex] = 0;
+			count[patient.freq][patient.sex] += 1;
 		};
 
 		let initializing = true;
-		const handle = Consultations.find(selector, options).observeChanges({
+		const commit = () => {
+			if (!initializing) {
+				this.changed(collection, key, state());
+			}
+		};
+
+		const pHandle = Patients.find(
+			{owner: this.userId},
+			{fields: {sex: 1}}
+		).observeChanges({
+			added: (_id, {sex}) => {
+				pRefs.set(_id, {freq: 0, sex});
+				if (count[0][sex] === undefined) count[0][sex] = 0;
+				count[0][sex] += 1;
+				commit();
+			},
+			changed: (_id, {sex}) => {
+				const {freq, sex: prev} = pRefs.get(_id);
+				count[freq][prev] -= 1;
+				if (count[freq][sex] === undefined) count[freq][sex] = 0;
+				count[freq][sex] += 1;
+				pRefs.set(_id, {freq, sex});
+				commit();
+			},
+			removed: (_id) => {
+				pRefs.delete(_id);
+				// everything should be commited by the consultations observer
+			}
+		});
+
+		const cHandle = Consultations.find(selector, options).observeChanges({
 			added: (_id, {patientId}) => {
 				total += 1;
 				inc(patientId);
 				refs.set(_id, patientId);
-				if (!initializing) {
-					this.changed(collection, key, state());
-				}
+				commit();
 			},
+
+			// changed: ... // TODO We assume a consultation does not change
+			// patientId. Handle that.
 
 			removed: (_id) => {
 				total -= 1;
 				const patientId = refs.get(_id);
 				dec(patientId);
 				refs.delete(_id);
-				this.changed(collection, key, state());
+				commit();
 			}
 		});
 
@@ -223,6 +248,9 @@ if (Meteor.isServer) {
 		this.added(collection, key, state());
 		this.ready();
 
-		this.onStop(() => handle.stop());
+		this.onStop(() => {
+			cHandle.stop();
+			pHandle.stop();
+		});
 	});
 }
