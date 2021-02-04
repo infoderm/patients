f4999d27dd7b9e7cbc6beb843e21976a41a687b5
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-02-04 16:23:18 +0100

    :rocket: perf: Do not rely on CDN for fonts.
    Fixes #174.

--- client/main.js
+++ client/main.js
@@ -9,14 +9,6 @@ import {BrowserRouter} from 'react-router-dom';
 import {LocalizationProvider} from '@material-ui/pickers';
 import DateFnsUtils from '@material-ui/pickers/adapter/date-fns';
 
-import WebFont from 'webfontloader';
-
-WebFont.load({
-	google: {
-		families: ['Roboto:300,400,500']
-	}
-});
-
 import App from '../imports/ui/App.js';
 
 Meteor.startup(() =>
