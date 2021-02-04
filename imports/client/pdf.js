7e2922bd51530b1b5ee00320643b963592ea763f
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-02-04 16:26:28 +0100

    :rocket: perf(pdf): Use WebWorker with local source.
    By serving `pdfs-dist/build` files via `/public` (using postinstall) and
    setting `pdfjs.GlobalWorkerOptions.workerSrc` appropriately.
    
    Fixes #173.

--- imports/client/pdf.js
+++ imports/client/pdf.js
@@ -2,22 +2,18 @@ import pdfjs from 'pdfjs-dist';
 
 // "pages": doc.pdfInfo.numPages
 
-export const WORKER_URL = 'pdfjs-dist/build/pdf.worker.js';
-export const CMAP_URL = 'pdfjs-dist/cmaps/';
+export const WORKER_URL = '/pdfjs-dist/build/pdf.worker.js';
+// export const WORKER_URL = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+export const CMAP_URL = '/pdfjs-dist/cmaps/';
 export const CMAP_PACKED = true;
 
 export function fetchPDF({url, cMapUrl, cMapPacked}) {
 	cMapUrl = cMapUrl ?? CMAP_URL;
 	cMapPacked = cMapPacked ?? CMAP_PACKED;
 
-	// NB: we have to use an explicit path here otherwise the worker file is
-	// not part of the build.
-	return import('pdfjs-dist/build/pdf.worker.js').then((module) => {
-		const globalObject = typeof window !== 'undefined' ? window : {};
-		if (globalObject.pdfjsWorker === undefined) {
-			globalObject.pdfjsWorker = module;
-		}
+	if (pdfjs.GlobalWorkerOptions.workerSrc === '') {
+		pdfjs.GlobalWorkerOptions.workerSrc = WORKER_URL;
+	}
 
-		return pdfjs.getDocument({url, cMapUrl, cMapPacked}).promise;
-	});
+	return pdfjs.getDocument({url, cMapUrl, cMapPacked}).promise;
 }
