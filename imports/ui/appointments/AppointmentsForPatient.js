c239354f387e9439e69cdba8215ad11de3de7836
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-02-19 17:46:01 +0100

    :recycle: refactor(AppointmentsForPatient): Using ConsultationsPager.
    And allow to filter cancelled/noshow appointments.

--- imports/ui/appointments/AppointmentsForPatient.js
+++ imports/ui/appointments/AppointmentsForPatient.js
@@ -1,61 +1,106 @@
-import {Meteor} from 'meteor/meteor';
-import {withTracker} from 'meteor/react-meteor-data';
+import React, {useState} from 'react';
 
-import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import PhoneDisabledIcon from '@material-ui/icons/PhoneDisabled';
+import AlarmOffIcon from '@material-ui/icons/AlarmOff';
 
-import {Patients} from '../../api/patients.js';
-import {Consultations} from '../../api/consultations.js';
+import startOfToday from 'date-fns/startOfToday';
+
+import usePatient from '../patients/usePatient.js';
 
 import Loading from '../navigation/Loading.js';
 import NoContent from '../navigation/NoContent.js';
+import {computeFixedFabStyle} from '../button/FixedFab.js';
 
-import AppointmentsForPatientStatic from './AppointmentsForPatientStatic.js';
+import ConsultationsPager from '../consultations/ConsultationsPager.js';
 
-const AppointmentsForPatient = (props) => {
-	const {loading, patient, ...rest} = props;
+const useStyles = makeStyles((theme) => ({
+	noShowToggle: computeFixedFabStyle({theme, col: 4}),
+	cancelledToggle: computeFixedFabStyle({theme, col: 5})
+}));
+
+const ConsultationsForPatient = (props) => {
+	const classes = useStyles();
+
+	const [showCancelled, setShowCancelled] = useState(false);
+	const [showNoShow, setShowNoShow] = useState(false);
+
+	const {patientId, page, perpage} = props;
+
+	const options = {fields: {_id: 1}};
+
+	const deps = [patientId];
+
+	const {loading, found} = usePatient({}, patientId, options, deps);
 
 	if (loading) {
 		return <Loading />;
 	}
 
-	if (!patient) {
+	if (!found) {
 		return <NoContent>Patient not found.</NoContent>;
 	}
 
-	return <AppointmentsForPatientStatic {...rest} />;
+	const $or = [
+		{
+			isCancelled: {
+				$in: [false, null]
+			},
+			scheduledDatetime: {
+				$gt: startOfToday() // TODO make reactive?
+			}
+		}
+	];
+
+	const query = {
+		patientId,
+		isDone: false,
+		$or
+	};
+
+	if (showCancelled) {
+		$or.push({
+			isCancelled: true
+		});
+	}
+
+	if (showNoShow) {
+		$or.push({
+			isCancelled: {
+				$in: [false, null]
+			}
+		});
+	}
+
+	const sort = {datetime: 1};
+
+	return (
+		<>
+			<ConsultationsPager
+				root={`/patient/${patientId}/appointments`}
+				page={page}
+				perpage={perpage}
+				query={query}
+				sort={sort}
+				defaultExpandedFirst={page === 1}
+			/>
+			<Fab
+				className={classes.noShowToggle}
+				color={showNoShow ? 'primary' : 'default'}
+				onClick={() => setShowNoShow(!showNoShow)}
+			>
+				<PhoneDisabledIcon />
+			</Fab>
+			<Fab
+				className={classes.cancelledToggle}
+				color={showCancelled ? 'primary' : 'default'}
+				onClick={() => setShowCancelled(!showCancelled)}
+			>
+				<AlarmOffIcon />
+			</Fab>
+		</>
+	);
 };
 
-export default withTracker(({patientId, page, perpage}) => {
-	const patientHandle = Meteor.subscribe('patient', patientId);
-	const appointmentsHandle = Meteor.subscribe(
-		'patient.appointments',
-		patientId
-	);
-
-	const loading = !patientHandle.ready() || !appointmentsHandle.ready();
-
-	const patient = loading ? null : Patients.findOne(patientId);
-
-	const appointments = loading
-		? []
-		: Consultations.find(
-				{
-					patientId,
-					isDone: false
-				},
-				{
-					sort: {datetime: 1},
-					skip: (page - 1) * perpage,
-					limit: perpage
-				}
-		  ).fetch();
-
-	return {
-		patientId,
-		page,
-		perpage,
-		loading,
-		patient,
-		appointments
-	};
-})(AppointmentsForPatient);
+export default ConsultationsForPatient;
