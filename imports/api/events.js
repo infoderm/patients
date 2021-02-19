b27fe0c67a0fa84f72ea89e129534687958ce2ac
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-02-19 21:18:57 +0100

    :sparkles: feat(events): Publish end datetime of event.

--- imports/api/events.js
+++ imports/api/events.js
@@ -2,20 +2,51 @@ import {Meteor} from 'meteor/meteor';
 import {Mongo} from 'meteor/mongo';
 import {check} from 'meteor/check';
 
+import addMilliseconds from 'date-fns/addMilliseconds';
+import addMinutes from 'date-fns/addMinutes';
+
 import {Consultations} from './consultations.js';
 import {Patients} from './patients.js';
+
+const DEFAULT_DURATION_IN_MINUTES = 15;
 
 const events = 'events';
 export const Events = new Mongo.Collection(events);
 
 if (Meteor.isServer) {
-	const event = (_id, {owner, patientId, datetime, isDone, isCancelled}) => {
+	const event = (
+		_id,
+		{
+			owner,
+			patientId,
+			datetime,
+			isDone,
+			isCancelled,
+			duration,
+			doneDatetime,
+			createdAt
+		}
+	) => {
 		const patient = Patients.findOne(patientId); // TODO Make reactive (maybe)?
+		const begin = datetime;
+		const end = isDone
+			? doneDatetime
+				? doneDatetime
+				: duration
+				? addMilliseconds(begin, duration)
+				: createdAt
+				? createdAt
+				: addMinutes(begin, DEFAULT_DURATION_IN_MINUTES)
+			: duration
+			? addMilliseconds(begin, duration)
+			: addMinutes(begin, DEFAULT_DURATION_IN_MINUTES);
+
 		return {
 			owner,
 			calendar: isDone ? 'consultations' : 'appointments',
 			title: `${patient.lastname} ${patient.firstname}`,
-			begin: datetime,
+			begin,
+			end,
 			isCancelled,
 			uri: `/consultation/${_id}`
 		};
@@ -34,7 +65,16 @@ if (Meteor.isServer) {
 		};
 
 		const options = {
-			fields: {_id: 1, patientId: 1, datetime: 1, isDone: 1, isCancelled: 1}
+			fields: {
+				_id: 1,
+				patientId: 1,
+				datetime: 1,
+				isDone: 1,
+				isCancelled: 1,
+				duration: 1,
+				doneDatetime: 1,
+				createdAt: 1
+			}
 		};
 
 		const handle = Consultations.find(query, options).observeChanges({
