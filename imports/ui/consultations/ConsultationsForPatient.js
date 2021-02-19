491c8efe9ea5372a295c1c101f816b90d49cbd12
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-02-19 18:11:12 +0100

    :sparkles: feat(ConsultationsForPatient): Add tooltip to new consultation button.

--- imports/ui/consultations/ConsultationsForPatient.js
+++ imports/ui/consultations/ConsultationsForPatient.js
@@ -2,25 +2,17 @@ import React from 'react';
 
 import {Link} from 'react-router-dom';
 
-import {makeStyles} from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
 import AddCommentIcon from '@material-ui/icons/AddComment';
 
 import usePatient from '../patients/usePatient.js';
 
 import Loading from '../navigation/Loading.js';
 import NoContent from '../navigation/NoContent.js';
-import {computeFixedFabStyle} from '../button/FixedFab.js';
+import FixedFab from '../button/FixedFab.js';
 
 import ConsultationsPager from './ConsultationsPager.js';
 
-const useStyles = makeStyles((theme) => ({
-	createButton: computeFixedFabStyle({theme, col: 4})
-}));
-
 const ConsultationsForPatient = (props) => {
-	const classes = useStyles();
-
 	const {patientId, page, perpage} = props;
 
 	const options = {fields: {_id: 1}};
@@ -54,14 +46,15 @@ const ConsultationsForPatient = (props) => {
 				sort={sort}
 				defaultExpandedFirst={page === 1}
 			/>
-			<Fab
-				className={classes.createButton}
+			<FixedFab
+				col={4}
 				color="primary"
 				component={Link}
 				to={`/new/consultation/for/${patientId}`}
+				tooltip="Start new consultation"
 			>
 				<AddCommentIcon />
-			</Fab>
+			</FixedFab>
 		</>
 	);
 };
