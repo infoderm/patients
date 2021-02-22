949831096703a30596f7406f74a48f982dccd3bf
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-02-22 10:59:47 +0100

    :sparkles: feat(addTooltip): Make more generic.

--- imports/ui/accessibility/addTooltip.js
+++ imports/ui/accessibility/addTooltip.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Tooltip from './Tooltip.js';
 
-const addTooltip = (Component) => ({tooltip, ...rest}) => {
+const addTooltip = (Component, transform = (_props, x) => x) => ({
+	tooltip,
+	...rest
+}) => {
+	tooltip = transform(rest, tooltip);
+
 	return tooltip ? (
 		<Tooltip title={tooltip} aria-label={tooltip}>
 			<Component {...rest} />
