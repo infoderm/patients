8861a9a7a8f972986a33db6e99af225458060de4
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-22 14:25:55 +0100

    :recycle: refactor(imports/client/uri): Remove dead code.

--- imports/client/uri.js
+++ imports/client/uri.js
@@ -1,18 +1,7 @@
-import base64 from '@aureooms/js-codec-base64';
-
-const options = {variant: 'RFC7515'};
-const decode = (x) => base64.decode(x, options);
-const encode = (x) => base64.encode(x, options);
-
 const myEncodeURIComponent = encodeURIComponent;
 const myDecodeURIComponent = decodeURIComponent;
-const myEncodeURIComponent2 = (component) => decode(Buffer.from(component));
-const myDecodeURIComponent2 = (component) =>
-	Buffer.from(encode(component)).toString();
 
 export {
 	myDecodeURIComponent,
 	myEncodeURIComponent,
-	myDecodeURIComponent2,
-	myEncodeURIComponent2
 };
