f0ddeb8b095089fa5ccf6adccae7b4b34ea73769
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-04-21 23:15:40 +0200

    :sparkles: feat(ConsultationForm): Allow to pass undefined update function.

--- imports/ui/consultations/ConsultationForm.js
+++ imports/ui/consultations/ConsultationForm.js
@@ -7,7 +7,7 @@ import {makeStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
-import TextField from '@material-ui/core/TextField';
+import TextField from '../input/TextField.js';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import IconButton from '@material-ui/core/IconButton';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -59,85 +59,92 @@ const ConsultationForm = ({consultation, update}) => {
 			<Grid container spacing={3}>
 				<Grid item xs={12}>
 					<TextField
-						autoFocus
 						multiline
+						readOnly={!update}
+						autoFocus={Boolean(update)}
 						label="Motif de la visite"
 						placeholder="Motif de la visite"
 						rows={4}
 						className={classes.multiline}
 						value={reason}
 						margin="normal"
-						onChange={update('reason')}
+						onChange={update && update('reason')}
 					/>
 				</Grid>
 				<Grid item xs={12}>
 					<TextField
 						multiline
+						readOnly={!update}
 						label="Examens déjà réalisés"
 						placeholder="Examens déjà réalisés"
 						rows={4}
 						className={classes.multiline}
 						value={done}
 						margin="normal"
-						onChange={update('done')}
+						onChange={update && update('done')}
 					/>
 				</Grid>
 				<Grid item xs={12}>
 					<TextField
 						multiline
+						readOnly={!update}
 						label="Examens à réaliser"
 						placeholder="Examens à réaliser"
 						rows={4}
 						className={classes.multiline}
 						value={todo}
 						margin="normal"
-						onChange={update('todo')}
+						onChange={update && update('todo')}
 					/>
 				</Grid>
 				<Grid item xs={12}>
 					<TextField
 						multiline
+						readOnly={!update}
 						label="Traitement"
 						placeholder="Traitement"
 						rows={4}
 						className={classes.multiline}
 						value={treatment}
 						margin="normal"
-						onChange={update('treatment')}
+						onChange={update && update('treatment')}
 					/>
 				</Grid>
 				<Grid item xs={12}>
 					<TextField
 						multiline
+						readOnly={!update}
 						label="À revoir"
 						placeholder="À revoir"
 						rows={4}
 						className={classes.multiline}
 						value={next}
 						margin="normal"
-						onChange={update('next')}
+						onChange={update && update('next')}
 					/>
 				</Grid>
 				<Grid item xs={12}>
 					<TextField
 						multiline
+						readOnly={!update}
 						label="Autres remarques"
 						placeholder="Write some additional information about the consultation here"
 						rows={4}
 						className={classes.multiline}
 						value={more}
 						margin="normal"
-						onChange={update('more')}
+						onChange={update && update('more')}
 					/>
 				</Grid>
 
 				<Grid item xs={1}>
 					<TextField
 						select
+						readOnly={!update}
 						label="Currency"
 						value={currency}
 						margin="normal"
-						onChange={update('currency')}
+						onChange={update && update('currency')}
 					>
 						<MenuItem value="EUR">€</MenuItem>
 					</TextField>
@@ -145,10 +152,11 @@ const ConsultationForm = ({consultation, update}) => {
 				<Grid item xs={1}>
 					<TextField
 						select
+						readOnly={!update}
 						label="Payment Method"
 						value={payment_method}
 						margin="normal"
-						onChange={update('payment_method')}
+						onChange={update && update('payment_method')}
 					>
 						<MenuItem value="cash">cash</MenuItem>
 						<MenuItem value="wire">virement</MenuItem>
@@ -158,6 +166,7 @@ const ConsultationForm = ({consultation, update}) => {
 				<Grid item xs={3}>
 					<TextField
 						fullWidth
+						readOnly={!update}
 						label="Prix"
 						value={price}
 						margin="normal"
@@ -175,40 +184,45 @@ const ConsultationForm = ({consultation, update}) => {
 								</InputAdornment>
 							)
 						}}
-						onChange={update('price')}
+						onChange={update && update('price')}
 					/>
 				</Grid>
 				<Grid item xs={3}>
 					<TextField
+						readOnly={!update}
 						label="Payé"
 						value={paid}
 						margin="normal"
 						error={!/^\d+$/.test(paid)}
-						onChange={update('paid')}
+						onChange={update && update('paid')}
 					/>
 				</Grid>
 				<Grid item xs={3}>
-					<AutocompleteWithSuggestions
-						itemToString={(x) => (x ? x.bookNumber : '')}
-						useSuggestions={makeSubstringSuggestions(
-							useBooksFind,
-							[],
-							'bookNumber'
-						)}
-						TextFieldProps={{
-							label: 'Carnet',
-							margin: 'normal'
-						}}
-						inputValue={book}
-						onInputChange={(event, newInputValue) => {
-							if (event) {
-								return update(
-									'book',
-									books.sanitizeInput
-								)({target: {value: newInputValue}});
-							}
-						}}
-					/>
+					{update ? (
+						<AutocompleteWithSuggestions
+							itemToString={(x) => (x ? x.bookNumber : '')}
+							useSuggestions={makeSubstringSuggestions(
+								useBooksFind,
+								[],
+								'bookNumber'
+							)}
+							TextFieldProps={{
+								label: 'Carnet',
+								margin: 'normal'
+							}}
+							inputValue={book}
+							onInputChange={(event, newInputValue) => {
+								if (event) {
+									return update(
+										'book',
+										books.sanitizeInput
+									)({target: {value: newInputValue}});
+								}
+							}}
+						/>
+					) : (
+						<TextField readOnly label="Carnet" value={book} margin="normal" />
+					)}
 				</Grid>
 			</Grid>
 		</Paper>
@@ -216,7 +230,8 @@ const ConsultationForm = ({consultation, update}) => {
 };
 
 ConsultationForm.propTypes = {
-	consultation: PropTypes.object.isRequired
+	consultation: PropTypes.object.isRequired,
+	update: PropTypes.func
 };
 
 export default ConsultationForm;
