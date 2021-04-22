f86dac544ef277e1250e8fa471a152669349cf45
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-04-22 11:10:32 +0200

    :recycle: refactor(ConsultationsList): Remove styling.

--- imports/ui/consultations/ConsultationsList.js
+++ imports/ui/consultations/ConsultationsList.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {makeStyles} from '@material-ui/core/styles';
 
 import ReactiveConsultationCard from './ReactiveConsultationCard.js';
-
-const useStyles = makeStyles((theme) => ({
-	container: {
-		padding: theme.spacing(3)
-	}
-}));
 
 export default function ConsultationsList({
 	items,
 	itemProps,
 	defaultExpandedFirst
 }) {
-	const classes = useStyles();
-
 	return (
-		<div className={classes.container}>
+		<div>
 			{items.map((consultation, i) => (
 				<ReactiveConsultationCard
 					key={consultation._id}
