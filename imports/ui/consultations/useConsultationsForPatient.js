6daf9d95aa7521f90771ec40b8ca109000521ac7
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-04-23 17:36:24 +0200

    :recycle: refactor(useConsultationsForPatient): Create ad-hoc hook.

---
+++ imports/ui/consultations/useConsultationsForPatient.js
@@ -0,0 +1,33 @@
+import {useConsultationsAndAppointments} from '../../api/consultations.js';
+
+/**
+ * useConsultationsForPatient.
+ *
+ * @param {string} patientId
+ * @param {{limit: number}} options
+ * @return {{loading: boolean, results: array}}
+ */
+const useConsultationsForPatient = (patientId, {limit}) => {
+	const query = {
+		patientId,
+		isDone: true
+	};
+
+	const options = {
+		sort: {
+			datetime: -1
+		},
+		limit,
+		fields: {
+			patientId: 1,
+			isDone: 1,
+			datetime: 1
+		}
+	};
+
+	const deps = [patientId, limit];
+
+	return useConsultationsAndAppointments(query, options, deps);
+};
+
+export default useConsultationsForPatient;
