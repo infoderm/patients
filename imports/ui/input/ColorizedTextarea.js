2b320ebbf6ae1dbff74727679099c60d9ea4db5d
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-05-07 18:06:53 +0200

    :bicyclist: perf(ColorizedTextarea): Memoize colorized output.

--- imports/ui/input/ColorizedTextarea.js
+++ imports/ui/input/ColorizedTextarea.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 
 import InputLabel from '@material-ui/core/InputLabel';
 import TextField from '@material-ui/core/TextField';
@@ -6,50 +6,50 @@ import Typography from '@material-ui/core/Typography';
 
 import colorizeText from '../../client/colorizeText';
 
+const ReadOnlyColorizedTextArea = (props) => {
+	const {className, label, rowsMax, dict, value} = props;
+
+	const children = useMemo(() => colorizeText(dict, value), [dict, value]);
+
+	return (
+		<div className={className}>
+			{label && <InputLabel shrink>{label}</InputLabel>}
+			<Typography
+				style={{
+					maxHeight: rowsMax ? `${1.5 * rowsMax}rem` : undefined,
+					whiteSpace: 'pre-wrap',
+					overflowWrap: 'break-word'
+				}}
+			>
+				{children}
+			</Typography>
+		</div>
+	);
+};
+
 const ColorizedTextarea = (props) => {
 	const {
 		readOnly,
-		label,
+		dict,
 		placeholder,
 		rows,
-		rowsMax,
-		className,
-		value,
 		margin,
 		onChange,
-		dict,
-		...rest
+		...common
 	} = props;
 
 	if (readOnly) {
-		return (
-			<div className={className}>
-				{label && <InputLabel shrink>{label}</InputLabel>}
-				<Typography
-					style={{
-						maxHeight: rowsMax ? `${1.5 * rowsMax}rem` : undefined,
-						whiteSpace: 'pre-wrap',
-						overflowWrap: 'break-word'
-					}}
-				>
-					{colorizeText(dict, value)}
-				</Typography>
-			</div>
-		);
+		return <ReadOnlyColorizedTextArea dict={dict} {...common} />;
 	}
 
 	return (
 		<TextField
 			multiline
-			className={className}
-			label={label}
-			value={value}
 			placeholder={placeholder}
 			rows={rows}
-			rowsMax={rowsMax}
 			margin={margin}
 			onChange={onChange}
-			{...rest}
+			{...common}
 		/>
 	);
 };
