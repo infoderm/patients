82491e9a2bcdbc25406d4ea1e9c3870fd13cad0c
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-05-18 10:38:38 +0200

    :recycle: refactor(TagDeletionDialog): Sort import statements.

--- imports/ui/tags/TagDeletionDialog.js
+++ imports/ui/tags/TagDeletionDialog.js
@@ -5,13 +5,13 @@ import PropTypes from 'prop-types';
 
 import {useSnackbar} from 'notistack';
 
-import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
+import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
 import CancelIcon from '@material-ui/icons/Cancel';
 
