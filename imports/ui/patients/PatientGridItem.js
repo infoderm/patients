047ea831ffcaea5e6c6ccc30f99e52a69ce1e997
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-05-19 22:08:46 +0200

    :recycle: refactor(PatientGridItem): Allow to pass CardProps down.

--- imports/ui/patients/PatientGridItem.js
+++ imports/ui/patients/PatientGridItem.js
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types';
 
 import Grid from '@material-ui/core/Grid';
 
-const PatientGridItem = ({Card, ...rest}) => (
+const PatientGridItem = ({Card, CardProps, ...rest}) => (
 	<Grid item xs={12} sm={12} md={12} lg={6} xl={4}>
-		<Card {...rest} />
+		<Card {...CardProps} {...rest} />
 	</Grid>
 );
 
 PatientGridItem.propTypes = {
-	Card: PropTypes.elementType.isRequired
+	Card: PropTypes.elementType.isRequired,
+	CardProps: PropTypes.object
 };
 
 export default PatientGridItem;
