35282353112bba99be49c3d742b28469a35e8096
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-05-19 22:01:32 +0200

    :recycle: refactor(ConfirmationDialog): Modernize and generalize.
    This is progress on #198.

--- imports/ui/modal/ConfirmationDialog.js
+++ imports/ui/modal/ConfirmationDialog.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {makeStyles} from '@material-ui/core/styles';
-
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -13,12 +11,6 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import DoneIcon from '@material-ui/icons/Done';
 import CancelIcon from '@material-ui/icons/Cancel';
 
-const useStyles = makeStyles((theme) => ({
-	rightIcon: {
-		marginLeft: theme.spacing(1)
-	}
-}));
-
 const ConfirmationDialog = ({
 	open,
 	onCancel,
@@ -26,32 +18,43 @@ const ConfirmationDialog = ({
 	title,
 	text,
 	cancel,
-	confirm
-}) => {
-	const classes = useStyles();
-
-	return (
-		<Dialog open={open} component="form" onClose={onCancel}>
-			<DialogTitle>{title}</DialogTitle>
-			<DialogContent>
-				<DialogContentText>{text}</DialogContentText>
-			</DialogContent>
-			<DialogActions>
-				<Button type="submit" color="default" onClick={onCancel}>
-					{cancel}
-					<CancelIcon className={classes.rightIcon} />
-				</Button>
-				<Button color="primary" onClick={onConfirm}>
-					{confirm}
-					<DoneIcon className={classes.rightIcon} />
-				</Button>
-			</DialogActions>
-		</Dialog>
-	);
-};
+	confirm,
+	CancelIcon,
+	ConfirmIcon,
+	cancelColor,
+	confirmColor
+}) => (
+	<Dialog open={open} component="form" onClose={onCancel}>
+		<DialogTitle>{title}</DialogTitle>
+		<DialogContent>
+			<DialogContentText>{text}</DialogContentText>
+		</DialogContent>
+		<DialogActions>
+			<Button
+				type="submit"
+				color={cancelColor}
+				endIcon={<CancelIcon />}
+				onClick={onCancel}
+			>
+				{cancel}
+			</Button>
+			<Button
+				color={confirmColor}
+				endIcon={<ConfirmIcon />}
+				onClick={onConfirm}
+			>
+				{confirm}
+			</Button>
+		</DialogActions>
+	</Dialog>
+);
 
 ConfirmationDialog.defaultProps = {
-	open: false
+	open: false,
+	CancelIcon,
+	ConfirmIcon: DoneIcon,
+	cancelColor: 'default',
+	confirmColor: 'primary'
 };
 
 ConfirmationDialog.propTypes = {
@@ -61,7 +64,11 @@ ConfirmationDialog.propTypes = {
 	title: PropTypes.string.isRequired,
 	text: PropTypes.string.isRequired,
 	cancel: PropTypes.string.isRequired,
-	confirm: PropTypes.string.isRequired
+	confirm: PropTypes.string.isRequired,
+	CancelIcon: PropTypes.elementType,
+	ConfirmIcon: PropTypes.elementType,
+	cancelColor: PropTypes.string,
+	confirmColor: PropTypes.string
 };
 
 export default ConfirmationDialog;
