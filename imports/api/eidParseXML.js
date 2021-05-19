5dbb858c9a2c4150ad24008090d8dfe511faf005
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-05-19 22:16:40 +0200

    :recycle: refactor(insertPatient): Extract eidParseXML.

---
+++ imports/api/eidParseXML.js
@@ -0,0 +1,33 @@
+import {xml2json} from 'xml-js';
+
+/**
+ * eidParseXML.
+ *
+ * @param {string} xmlString
+ * @return {Object}
+ */
+const eidParseXML = (xmlString) => {
+	// TODO validate using xsd
+	const jsonString = xml2json(xmlString, {compact: true, spaces: 4});
+	const json = JSON.parse(jsonString);
+	console.debug('eidXMLToJSON', {json});
+
+	const {address, identity} = json.eid;
+	const attributes = identity._attributes;
+	const d = attributes.dateofbirth;
+
+	return {
+		niss: attributes.nationalnumber,
+		firstname: identity.firstname._text,
+		lastname: identity.name._text,
+		photo: identity.photo._text,
+		birthdate: `${d.slice(0, 4)}-${d.slice(4, 6)}-${d.slice(6, 8)}`,
+		sex: attributes.gender,
+
+		municipality: address.municipality._text,
+		streetandnumber: address.streetandnumber._text,
+		zip: address.zip._text
+	};
+};
+
+export default eidParseXML;
--- imports/client/insertPatient.js
+++ imports/client/insertPatient.js
@@ -1,30 +1,11 @@
 import {Meteor} from 'meteor/meteor';
-import {xml2json} from 'xml-js';
 
 import {Patients} from '../api/patients.js';
 
+import eidParseXML from '../api/eidParseXML.js';
+
 export default function insertPatient(history, xmlString) {
-	// TODO validate using xsd
-	const jsonString = xml2json(xmlString, {compact: true, spaces: 4});
-	const json = JSON.parse(jsonString);
-	console.log(json);
-
-	const {address, identity} = json.eid;
-	const attributes = identity._attributes;
-	const d = attributes.dateofbirth;
-
-	const op = {
-		niss: attributes.nationalnumber,
-		firstname: identity.firstname._text,
-		lastname: identity.name._text,
-		photo: identity.photo._text,
-		birthdate: `${d.slice(0, 4)}-${d.slice(4, 6)}-${d.slice(6, 8)}`,
-		sex: attributes.gender,
-
-		municipality: address.municipality._text,
-		streetandnumber: address.streetandnumber._text,
-		zip: address.zip._text
-	};
+	const op = eidParseXML(xmlString);
 
 	const query = {niss: op.niss};
 	const options = {fields: {_id: 1, niss: 1}};
