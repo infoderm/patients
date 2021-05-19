69ffabfb6e59a565430b145f2e6ea6cae88ebdb7
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-05-19 21:56:13 +0200

    :recycle: refactor(CustomWholeWindowDropZone): Extract useOnDrop hook.

---
+++ imports/client/useOnDrop.js
@@ -0,0 +1,10 @@
+import {useMemo} from 'react';
+import {useHistory} from 'react-router-dom';
+import handleDrop from './handleDrop.js';
+
+const useOnDrop = () => {
+	const history = useHistory();
+	return useMemo(() => handleDrop(history), [history]);
+};
+
+export default useOnDrop;
--- imports/ui/input/CustomWholeWindowDropZone.js
+++ imports/ui/input/CustomWholeWindowDropZone.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import {useHistory} from 'react-router-dom';
 import {useSnackbar} from 'notistack';
-import handleDrop from '../../client/handleDrop.js';
+import useOnDrop from '../../client/useOnDrop.js';
 import WholeWindowDropZone from './WholeWindowDropZone.js';
 
 export default function CustomWholeWindowDropZone() {
-	const history = useHistory();
 	const {enqueueSnackbar, closeSnackbar} = useSnackbar();
-	const onDrop = handleDrop(history);
+	const onDrop = useOnDrop();
 	const callback = (data) => {
 		const key = enqueueSnackbar('Processing data...', {variant: 'info'});
 		try {
