075655bb7f845de29efcb0af2482420eb9d80c61
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-05-19 22:24:29 +0200

    :sparkles: feat(insertPatient): Suggest action to take when dropping eid info.
    Fixes #231.

--- imports/client/insertPatient.js
+++ imports/client/insertPatient.js
@@ -1,35 +1,20 @@
-import {Meteor} from 'meteor/meteor';
+import React from 'react';
 
-import {Patients} from '../api/patients.js';
+import dialog from '../ui/modal/dialog.js';
+import EidCardDialog from '../ui/eid/EidCardDialog.js';
 
 import eidParseXML from '../api/eidParseXML.js';
 
 export default function insertPatient(history, xmlString) {
-	const op = eidParseXML(xmlString);
+	const eidInfo = eidParseXML(xmlString);
 
-	const query = {niss: op.niss};
-	const options = {fields: {_id: 1, niss: 1}};
-	Meteor.subscribe('patients', query, options, {
-		onReady: () => {
-			const patient = Patients.findOne(query, options);
-			if (patient) {
-				Meteor.call('patients.update', patient._id, op, (err, _res) => {
-					if (err) {
-						console.error(err);
-					} else {
-						history.push({pathname: `/patient/${patient._id}`});
-					}
-				});
-			} else {
-				Meteor.call('patients.insert', op, (err, _id) => {
-					if (err) {
-						console.error(err);
-					} else {
-						history.push({pathname: `/patient/${_id}`});
-					}
-				});
-			}
-		},
-		onStop: (err) => console.error(err)
-	});
+	dialog((resolve) => (
+		<EidCardDialog
+			history={history}
+			eidInfo={eidInfo}
+			onClose={() => {
+				resolve();
+			}}
+		/>
+	));
 }
