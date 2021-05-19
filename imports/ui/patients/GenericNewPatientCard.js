abe14bffad599067f9e6820e7784451672237dba
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-05-19 10:32:01 +0200

    :recycle: refactor(NewPatientCard): Extract GenericNewPatientCard.
    Also, this is progress on #198.

---
+++ imports/ui/patients/GenericNewPatientCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {makeStyles} from '@material-ui/core/styles';
+
+import Button from '@material-ui/core/Button';
+import FaceIcon from '@material-ui/icons/Face';
+
+const useStyles = makeStyles(() => ({
+	buttonTile: {
+		minHeight: 200,
+		fontSize: '1rem'
+	}
+}));
+
+const GenericNewPatientCard = (props) => {
+	const classes = useStyles();
+
+	return (
+		<Button
+			fullWidth
+			variant="contained"
+			className={classes.buttonTile}
+			endIcon={<FaceIcon />}
+			{...props}
+		>
+			Add a new patient
+		</Button>
+	);
+};
+
+export default GenericNewPatientCard;
--- imports/ui/patients/NewPatientCard.js
+++ imports/ui/patients/NewPatientCard.js
@@ -1,34 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-import {makeStyles} from '@material-ui/core/styles';
+import GenericNewPatientCard from './GenericNewPatientCard.js';
 
-import Button from '@material-ui/core/Button';
-import FaceIcon from '@material-ui/icons/Face';
+const NewPatientCard = () => (
+	<GenericNewPatientCard component={Link} to="/new/patient" />
+);
 
-const useStyles = makeStyles((theme) => ({
-	buttonTile: {
-		minHeight: 200,
-		width: '100%',
-		fontSize: '1rem'
-	},
-	rightIcon: {
-		marginLeft: theme.spacing(1)
-	}
-}));
-
-export default function NewPatientCard() {
-	const classes = useStyles();
-
-	return (
-		<Button
-			variant="contained"
-			className={classes.buttonTile}
-			component={Link}
-			to="/new/patient"
-		>
-			Add a new patient
-			<FaceIcon className={classes.rightIcon} />
-		</Button>
-	);
-}
+export default NewPatientCard;
