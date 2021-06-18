cd952bf65590756207e6abfe9babffe51c6aaeea
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-06-18 11:25:17 +0200

    :wrench: config(husky): Typecheck staged files only.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,51 +1,22 @@
 'use strict';
 
-// The escape code comes from https://github.com/okonet/lint-staged/pull/875/files#diff-1f3abab6f042ef9a50a2cce81b57a98b9b111c8374311c4b532777dbd21ecd80
+const fs = require('fs');
 
-/**
- * Escape a shell argument for Unix shells:
- * - Escape all weird characters with `\`
- * - Double-escape spaces for execa (`\\ `)
- * @see https://qntm.org/cmd
- * @see https://github.com/sindresorhus/execa/blob/a827d82203a1440e585276bef5d399a5953801f1/lib/command.js#L20
- * @param {string} arg the argument
- * @returns {string} the escaped argument
- */
-const escapeUnixShellArg = (arg) =>
-	`${arg.replace(/(\W)/g, '\\$1').replace(/( )/g, '\\$1')}`;
-
-/**
- * Temporary no-op function.
- * @todo Actually figure out how to escape paths on Windows
- *
- * @param {string} _arg the argument
- * @returns {string} the escape argument
- */
-const escapeWinCmdArg = (_arg) => {
-	throw new Error('not implemented');
+const tsc = (paths) => {
+	const encoding = 'utf8';
+	const originalTSConfigPath = 'tsconfig.json';
+	// NOTE: MUST be in same directory.
+	const lintStagedTSConfigPath = 'lint-staged.tsconfig.json';
+	const originalTSConfig = JSON.parse(
+		fs.readFileSync(originalTSConfigPath, encoding)
+	);
+	const lintStagedTSConfig = {...originalTSConfig, include: paths};
+	fs.writeFileSync(lintStagedTSConfigPath, JSON.stringify(lintStagedTSConfig));
+	return `npm run tsc -- --noEmit --project ${lintStagedTSConfigPath}`;
 };
 
-/**
- * "Internally" expose the platform option for testing
- * @param {string} arg the argument
- * @param {string} platform the platform
- * @returns {string} the escaped command line argument
- */
-const escapeArg = (arg, platform) => {
-	const isWin = platform === 'win32';
-	return isWin ? escapeWinCmdArg(arg) : escapeUnixShellArg(arg);
-};
-
-/**
- * Escape a command line argument based on the current platform
- * @param {string} arg the raw command line argument
- * @returns {string} the escaped command line argument
- */
-const escape = (arg) => escapeArg(arg, process.platform);
+const lint = 'npm run lint-and-fix';
 
 module.exports = {
-	'*.{js,ts,tsx}': (filenames) => [
-		`npm run lint-and-fix -- ${filenames.map(escape).join(' ')}`,
-		'npm run tsc'
-	]
+	'*.{js,ts,tsx}': [lint, tsc]
 };
