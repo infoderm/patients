d3aaa6e6e2ea65f4eb224ddd7a3fe5336dde1963
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-07-05 11:17:29 +0200

    :recycle: refactor(i18/currency): Extract useCurrencyFormatObject.

--- imports/i18n/currency.ts
+++ imports/i18n/currency.ts
@@ -2,12 +2,16 @@ import {useMemo} from 'react';
 
 import {useNumberFormat} from './number';
 
-export const useCurrencyFormat = (currency: string, options?: any) => {
-	const numberFormat = useNumberFormat({
+export const useCurrencyFormatObject = (currency: string, options?: any) => {
+	return useNumberFormat({
 		style: 'currency',
 		currency,
 		...options
 	});
+};
+
+export const useCurrencyFormat = (currency: string, options?: any) => {
+	const numberFormat = useCurrencyFormatObject(currency, options);
 
 	return useMemo(() => {
 		return (value: number | bigint) => numberFormat.format(value);
