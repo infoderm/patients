263aa8df944dcf29a079b0735261cc75ddc52dd6
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-07-05 11:34:15 +0200

    :adhesive_bandage: fix(useNumberFormat): Avoid throwing on undefined currency.

--- imports/i18n/number.ts
+++ imports/i18n/number.ts
@@ -5,6 +5,14 @@ import useLocales from './useLocales';
 export const useNumberFormat = (options: any) => {
 	const locales = useLocales();
 	return useMemo(() => {
-		return new Intl.NumberFormat(locales, options);
+		try {
+			return new Intl.NumberFormat(locales, options);
+		} catch (error: unknown) {
+			if (error instanceof TypeError) {
+				return undefined;
+			}
+
+			throw error;
+		}
 	}, [JSON.stringify(options), ...locales]);
 };
