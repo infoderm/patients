bac11691bcf9f77970265fe0e62ad1454f7db4b5
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-08-20 15:23:13 +0200

    :recycle: refactor(ConfirmationDialog): Rewrite as TypeScript.

--- imports/ui/modal/ConfirmationDialog.tsx
+++ imports/ui/modal/ConfirmationDialog.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {ElementType} from 'react';
 
+import {PropTypes as MuiPropTypes} from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -8,22 +8,36 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-import DoneIcon from '@material-ui/icons/Done';
-import CancelIcon from '@material-ui/icons/Cancel';
+import DefaultConfirmIcon from '@material-ui/icons/Done';
+import DefaultCancelIcon from '@material-ui/icons/Cancel';
+
+interface Props {
+	open?: boolean;
+	onCancel: () => void;
+	onConfirm: () => void;
+	title: string;
+	text: string;
+	cancel: string;
+	confirm: string;
+	CancelIcon?: ElementType;
+	ConfirmIcon?: ElementType;
+	cancelColor?: MuiPropTypes.Color;
+	confirmColor?: MuiPropTypes.Color;
+}
 
 const ConfirmationDialog = ({
-	open,
+	open = false,
 	onCancel,
 	onConfirm,
 	title,
 	text,
 	cancel,
 	confirm,
-	CancelIcon,
-	ConfirmIcon,
-	cancelColor,
-	confirmColor,
-}) => (
+	CancelIcon = DefaultCancelIcon,
+	ConfirmIcon = DefaultConfirmIcon,
+	cancelColor = 'default',
+	confirmColor = 'primary',
+}: Props) => (
 	<Dialog open={open} /* component="form" */ onClose={onCancel}>
 		<DialogTitle>{title}</DialogTitle>
 		<DialogContent>
@@ -48,27 +62,5 @@ const ConfirmationDialog = ({
 		</DialogActions>
 	</Dialog>
 );
-
-ConfirmationDialog.defaultProps = {
-	open: false,
-	CancelIcon,
-	ConfirmIcon: DoneIcon,
-	cancelColor: 'default',
-	confirmColor: 'primary',
-};
-
-ConfirmationDialog.propTypes = {
-	open: PropTypes.bool,
-	onCancel: PropTypes.func.isRequired,
-	onConfirm: PropTypes.func.isRequired,
-	title: PropTypes.string.isRequired,
-	text: PropTypes.string.isRequired,
-	cancel: PropTypes.string.isRequired,
-	confirm: PropTypes.string.isRequired,
-	CancelIcon: PropTypes.elementType,
-	ConfirmIcon: PropTypes.elementType,
-	cancelColor: PropTypes.string,
-	confirmColor: PropTypes.string,
-};
 
 export default ConfirmationDialog;
