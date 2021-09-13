c7817c534c36e65774648551bbd9ee655ce0b3f2
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2021-09-13 18:38:13 +0200

    :sparkles: feat(search): Improve patient searching.
    By dropping language-dependent particle splitting and using trigrams of
    concatenated names and unconcatenated (with word boundaries) names for
    approximate matching instead of subtrings of unconcatenated names.

--- imports/api/string.ts
+++ imports/api/string.ts
@@ -5,9 +5,12 @@ import {all, min} from '@iterable-iterator/reduce';
 import {list} from '@iterable-iterator/list';
 import {len} from '@functional-abstraction/operator';
 import {map} from '@iterable-iterator/map';
+import {_chain} from '@iterable-iterator/chain';
 import {sorted} from '@iterable-iterator/sorted';
-import {increasing, decreasing} from '@total-order/primitive';
+import {window} from '@iterable-iterator/window';
+import {increasing} from '@total-order/primitive';
 import {len as byLength} from '@total-order/key';
+import {combinations} from '@combinatorics/n-combinations';
 import escapeStringRegexp from 'escape-string-regexp';
 
 export {default as escapeStringRegexp} from 'escape-string-regexp';
@@ -27,6 +30,9 @@ export const onlyASCII = (string: string) => deburr(string);
 
 export const onlyLowerCaseASCII = (string: string) =>
 	onlyASCII(string.toLowerCase());
+
+export const onlyLowerCaseAlphabetical = (string: string, replacement = '') =>
+	onlyLowerCaseASCII(string).replace(/[^a-z]+/g, replacement);
 
 export const makeIndex = (data: string) => {
 	const needles = onlyLowerCaseASCII(data).split(' ');
@@ -92,108 +98,49 @@ export const makeRegExpIndex = (patterns: Iterable<string>) => {
 	};
 };
 
-const PARTICLES_FR: string[] = ['du', 'de', 'des', "d'", 'le', 'la'];
-const PARTICLES_NL: string[] = [
-	'de',
-	'den',
-	'op',
-	"t'",
-	"'t",
-	'ten',
-	'ter',
-	'te',
-	'van',
-	'der',
-];
-const PARTICLES_DE: string[] = [
-	'am',
-	'an',
-	'af',
-	'auf',
-	'aus',
-	'der',
-	'im',
-	'von',
-	'und',
-	'zu',
-	'zum',
-	'zur',
-];
+const split = (string: string): string[] => {
+	const trimmed = string.replace(/^\s+/, '').replace(/\s+$/, '');
+	return trimmed === '' ? [] : trimmed.split(/\s+/);
+};
 
-const PARTICLES: Set<string> = new Set([
-	...PARTICLES_FR,
-	...PARTICLES_NL,
-	...PARTICLES_DE,
-]);
-const PARTICLES_ORDERED: string[] = sorted(byLength(decreasing), PARTICLES);
+export const words = (string: string): string[] =>
+	split(onlyLowerCaseAlphabetical(string, ' '));
 
-const words = (string: string) => string.trim().split(/\s+/);
+const trigrams = (string: string): IterableIterator<string> =>
+	map(([a, b, c]: string[]) => a + b + c, window(3, string));
 
-function* splitParticles(data: string) {
-	const queue = words(data).reverse();
-	outer: while (queue.length > 0) {
-		const word = queue.pop();
-		// greedy match
-		// TODO use prefix tree
-		for (const particle of PARTICLES_ORDERED) {
-			if (word.startsWith(particle)) {
-				yield particle;
-				const rest = word.slice(particle.length);
-				if (rest) queue.push(rest);
-				continue outer;
-			}
-		}
+const wrapTrigram = (x: string) => `0${x}0`;
 
-		yield word;
+export const stringTrigrams = (string: string) =>
+	map(wrapTrigram, trigrams(onlyLowerCaseAlphabetical(string)));
+const textTrigrams = (text: string) =>
+	map(wrapTrigram, trigrams(`11${words(text).join('1')}1`));
+
+const _boundaryTrigrams = function* (
+	strings: string[],
+): IterableIterator<string> {
+	for (const a of strings) {
+		assert(a.length > 0);
+		const wrapped = `11${a}1`;
+		yield wrapped.slice(0, 3);
+		yield wrapped.slice(1, 4);
+		yield wrapped.slice(-3);
 	}
-}
+
+	for (const [a, b] of combinations(strings, 2)) {
+		yield `${a[a.length - 1]}1${b[0]}`;
+	}
+};
+
+export const boundaryTrigrams = (strings: string[]) =>
+	map(wrapTrigram, _boundaryTrigrams(strings));
 
 export const normalizeSearch = (data: string) =>
-	[...splitParticles(onlyLowerCaseASCII(data))].join(' ');
+	[...words(data), ...textTrigrams(data), ...stringTrigrams(data)].join(' ');
 
-function* nonEmptySubstrings(string: string) {
-	const n = string.length;
-	for (let i = 0; i < n; ++i) {
-		for (let j = i + 1; j <= n; ++j) {
-			yield string.slice(i, j);
-		}
-	}
-}
-
-const SHATTER_SHORT = 2;
-const SHATTER_MEDIUM = 4;
-const SHATTER_LONG = 6;
-
-export const shatter = (data: string) => {
-	const parts = splitParticles(onlyLowerCaseASCII(data));
-	const index = {
-		whole: [],
-		particles: [],
-		substring_long: [],
-		substring_medium: [],
-		substring_short: [],
-	};
-	for (const part of parts) {
-		if (PARTICLES.has(part)) {
-			index.particles.push(part);
-		} else {
-			for (const _part of part.split(/[^a-z]+/)) {
-				index.whole.push(_part);
-				for (const substring of nonEmptySubstrings(_part)) {
-					if (substring.length < SHATTER_SHORT) continue;
-					else if (substring.length === _part.length) continue;
-					else if (substring.length < SHATTER_MEDIUM)
-						index.substring_short.push(substring);
-					else if (substring.length < SHATTER_LONG)
-						index.substring_medium.push(substring);
-					else index.substring_long.push(substring);
-				}
-			}
-		}
-	}
-
-	return index;
-};
+export const keepUnique = <T>(...iterables: Array<Iterable<T>>) => [
+	...new Set<T>(_chain(iterables)),
+];
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
