baadca98a1306de409add0508871ad17477f410c
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-10-27 02:53:04 +0200

    :bicyclist: perf(useSortedWorkSchedule): Memoize.

--- imports/ui/settings/useSortedWorkSchedule.ts
+++ imports/ui/settings/useSortedWorkSchedule.ts
@@ -1,10 +1,11 @@
+import {useMemo} from 'react';
 import {useSettingCached} from '../../client/settings';
 import useWorkScheduleSort from './useWorkScheduleSort';
 
 const useSortedWorkSchedule = () => {
 	const sort = useWorkScheduleSort();
 	const {value} = useSettingCached('work-schedule');
-	return sort(value);
+	return useMemo(() => sort(value), [sort, value]);
 };
 
 export default useSortedWorkSchedule;
