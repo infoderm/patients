9b8236db224988a68a6d90cfc78f52ba74c22c20
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-10-31 02:08:34 +0200

    :recycle: refactor(api/ics): Extract getPermissionsForToken.

--- imports/api/permissions/token.ts
+++ imports/api/permissions/token.ts
@@ -1,6 +1,72 @@
+import {PermissionTokens} from '../collection/permissionTokens';
+import {
+	extractSignature,
+	Document,
+	SignedDocument,
+	verify,
+} from '../../lib/hmac';
 import {encode as _encode, decode as _decode} from '../../lib/base64/rfc7515';
+import {ICS_CALENDAR_READ} from './codes';
 
 export const encode = (_id: string, key: string) =>
 	_encode(JSON.stringify({_id, key}));
 
 export const decode = (token: string) => JSON.parse(_decode(token));
+
+const extractPermissions = ({_id, ...rest}) => rest as SignedDocument;
+
+export class PermissionTokenValidationError extends Error {
+	_HTTPErrorCode: number;
+	constructor(HTTPErrorCode: number) {
+		const message = `PermissionTokenValidationError(${HTTPErrorCode})`;
+		super(message);
+		this._HTTPErrorCode = HTTPErrorCode;
+	}
+
+	getHTTPErrorCode() {
+		return this._HTTPErrorCode;
+	}
+}
+
+export const getPermissionsForToken = async (
+	token: string,
+): Promise<Document> => {
+	let decoded;
+	try {
+		decoded = decode(token);
+	} catch {
+		throw new PermissionTokenValidationError(422);
+	}
+
+	const {_id, key} = decoded;
+
+	if (typeof _id !== 'string' || typeof key !== 'string') {
+		throw new PermissionTokenValidationError(422);
+	}
+
+	const now = new Date();
+
+	const document = PermissionTokens.findOne({
+		_id,
+		permissions: ICS_CALENDAR_READ,
+		validFrom: {$lte: now},
+		validUntil: {$gt: now}, // TODO use mongo's internal Date?
+	});
+
+	if (!document) {
+		throw new PermissionTokenValidationError(404);
+	}
+
+	const permissions = extractPermissions(document); // _id is not part of signature
+
+	const signatureIsValid: boolean = await verify(key, permissions);
+
+	if (!signatureIsValid) {
+		// throw new PermissionTokenValidationError(401); // see https://stackoverflow.com/q/45153773 and https://stackoverflow.com/a/1960453
+		throw new PermissionTokenValidationError(
+			404 /* stealth, although timing side-channel is exposed */,
+		);
+	}
+
+	return extractSignature(permissions).document;
+};
--- server/api/ics/index.ts
+++ server/api/ics/index.ts
@@ -1,72 +1,37 @@
 import {Meteor} from 'meteor/meteor';
 import express from 'express';
 import ical, {ICalEventStatus} from 'ical-generator';
-import {Consultations} from '../../../imports/api/collection/consultations';
-import {PermissionTokens} from '../../../imports/api/collection/permissionTokens';
 
+import {Consultations} from '../../../imports/api/collection/consultations';
 import {event} from '../../../imports/api/events';
-import {decode} from '../../../imports/api/permissions/token';
-import {SignedDocument, verify} from '../../../imports/lib/hmac';
-import {ICS_CALENDAR_READ} from '../../../imports/api/permissions/codes';
+import {
+	getPermissionsForToken,
+	PermissionTokenValidationError,
+} from '../../../imports/api/permissions/token';
 import rateLimiter from './rateLimiter';
 
 const routes = express();
 routes.use(rateLimiter);
 
-const extractPermissions = ({_id, ...rest}) => rest;
-
 routes.get('/calendar/:token/events.ics', async (req, res, _next) => {
 	const {token} = req.params;
-	let decoded;
+	let permissions;
+
 	try {
-		decoded = decode(token);
-	} catch {
-		res.writeHead(422);
-		res.end();
-		return;
-	}
+		permissions = await getPermissionsForToken(token);
+	} catch (error: unknown) {
+		if (error instanceof PermissionTokenValidationError) {
+			res.writeHead(error.getHTTPErrorCode()).end();
+			return;
+		}
 
-	const {_id, key} = decoded;
-
-	if (typeof _id !== 'string' || typeof key !== 'string') {
-		res.writeHead(422);
-		res.end();
-		return;
-	}
-
-	const now = new Date();
-
-	const document = PermissionTokens.findOne({
-		_id,
-		permissions: ICS_CALENDAR_READ,
-		validFrom: {$lte: now},
-		validUntil: {$gt: now}, // TODO use mongo's internal Date?
-	});
-
-	if (!document) {
-		res.writeHead(404);
-		res.end();
-		return;
-	}
-
-	const permissions = extractPermissions(document); // _id is not part of signature
-
-	const signatureIsValid: boolean = await verify(
-		key,
-		permissions as SignedDocument,
-	);
-	if (!signatureIsValid) {
-		// res.writeHead(401); // see https://stackoverflow.com/q/45153773 and https://stackoverflow.com/a/1960453
-		res.writeHead(404 /* stealth, although timing side-channel is exposed */);
-		res.end();
-		return;
+		throw error;
 	}
 
 	const {userId} = permissions;
 
 	if (userId === undefined || userId.length === 0) {
-		res.writeHead(503);
-		res.end();
+		res.writeHead(503).end();
 		return;
 	}
 
