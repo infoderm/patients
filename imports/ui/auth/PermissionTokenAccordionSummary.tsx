50c87300f24bb21eb49a152923f143b895b551e6
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-10-31 02:12:40 +0200

    :sparkles: feat(auth): Display last use time and ip address.

--- imports/ui/auth/PermissionTokenAccordionSummary.tsx
+++ imports/ui/auth/PermissionTokenAccordionSummary.tsx
@@ -4,15 +4,21 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import {PermissionTokenDocument} from '../../api/collection/permissionTokens';
+import {useDateFormatRelative} from '../../i18n/datetime';
 
 interface Props {
 	item: PermissionTokenDocument;
 }
 
-const PermissionTokenAccordionSummary = ({item}: Props) => (
-	<AccordionSummary expandIcon={<ExpandMoreIcon />}>
-		{item._id}
-	</AccordionSummary>
-);
+const PermissionTokenAccordionSummary = ({item}: Props) => {
+	const localizedDateFormatRelative = useDateFormatRelative();
+	return (
+		<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+			{item._id} (last used{' '}
+			{localizedDateFormatRelative(item.lastUsedAt, new Date())} from{' '}
+			{item.lastUsedIPAddress})
+		</AccordionSummary>
+	);
+};
 
 export default PermissionTokenAccordionSummary;
