26c567607e5ea3d65901c6598863d510d9040fa8
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-10-31 23:53:49 +0100

    :sparkles: feat(api/ics): Cache route.

--- server/api/ics/index.ts
+++ server/api/ics/index.ts
@@ -10,12 +10,15 @@ import {
 } from '../../../imports/api/permissions/token';
 import rateLimiter from './rateLimiter';
 
+const cache = new Map(); // TODO allow to clear cache / use LRU cache
+
 const routes = express();
 
 routes.set('trust proxy', 1);
 
 routes.use(rateLimiter);
-routes.get('/calendar/:token/events.ics', async (req, res, _next) => {
+const filename = 'events.ics';
+routes.get(`/calendar/:token/${filename}`, async (req, res, _next) => {
 	const {token} = req.params;
 	let permissions;
 
@@ -37,24 +40,52 @@ routes.get('/calendar/:token/events.ics', async (req, res, _next) => {
 		return;
 	}
 
-	const calendar = ical({name: `Calendar for ${JSON.stringify(userId)}`});
-
-	for (const {_id, ...fields} of Consultations.find({
+	const query = {
 		owner: {$in: userId},
 		isDone: false,
-	}).fetch()) {
-		const {begin, end, title, isCancelled, uri} = event(_id, fields);
-		const path = uri.slice(1);
-		calendar.createEvent({
-			start: begin,
-			end,
-			summary: title,
-			status: isCancelled ? ICalEventStatus.CANCELLED : null,
-			url: Meteor.absoluteUrl(path),
+	};
+
+	const lastModified = Consultations.findOne(query, {
+		sort: {lastModifiedAt: -1},
+	});
+
+	const cacheKey = JSON.stringify(query);
+
+	if (
+		!cache.has(cacheKey) ||
+		cache.get(cacheKey).lastModifiedAt < lastModified.lastModifiedAt
+	) {
+		const calendar = ical({name: `Calendar for ${JSON.stringify(userId)}`});
+
+		Consultations.find(query, {sort: {lastModifiedAt: -1}}).forEach(
+			({_id, ...fields}) => {
+				const {begin, end, title, isCancelled, uri} = event(_id, fields);
+				const path = uri.slice(1);
+				calendar.createEvent({
+					start: begin,
+					end,
+					summary: title,
+					status: isCancelled ? ICalEventStatus.CANCELLED : null,
+					url: Meteor.absoluteUrl(path),
+				});
+			},
+		);
+
+		cache.set(cacheKey, {
+			lastModifiedAt: lastModified.lastModifiedAt,
+			body: calendar.toString(),
 		});
 	}
 
-	calendar.serve(res);
+	const {lastModifiedAt, body} = cache.get(cacheKey);
+
+	res
+		.set({
+			'Last-Modified': lastModifiedAt.toUTCString(),
+			'Content-Type': 'text/calendar; charset=utf-8',
+			'Content-Disposition': `attachment; filename="${filename}"`,
+		})
+		.send(body);
 });
 
 export default routes;
