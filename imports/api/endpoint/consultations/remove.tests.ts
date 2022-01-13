17d9acc2e64a5c5f4b8094d5d43a612af2d76eed
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-13 13:43:30 +0100

    :microscope: test(consultations/remove): Increase coverage.

---
+++ imports/api/endpoint/consultations/remove.tests.ts
@@ -0,0 +1,145 @@
+// eslint-disable-next-line import/no-unassigned-import
+import 'regenerator-runtime/runtime.js';
+import {assert} from 'chai';
+
+import {Meteor} from 'meteor/meteor';
+import {Random} from 'meteor/random';
+
+import invoke from '../invoke';
+
+import {Patients, newPatient} from '../../collection/patients.mock';
+import {throws} from '../../../test/fixtures';
+import {
+	Consultations,
+	newConsultation,
+} from '../../collection/consultations.mock';
+import {newUpload, Uploads} from '../../uploads.mock';
+import consultationsRemove from './remove';
+import consultationsAttach from './attach';
+
+if (Meteor.isServer) {
+	describe('endpoint', () => {
+		describe('consultations', () => {
+			describe('remove', () => {
+				beforeEach(() => {
+					Patients.remove({});
+					Consultations.remove({});
+					Uploads.remove({});
+				});
+
+				it('can remove consultation', async () => {
+					const userId = Random.id();
+
+					const patientId = await newPatient({userId});
+
+					const {insertedId: consultationId} = await newConsultation(
+						{userId},
+						{patientId},
+					);
+
+					assert.equal(Consultations.find({_id: consultationId}).count(), 1);
+					assert.equal(Consultations.find({patientId}).count(), 1);
+
+					await invoke(consultationsRemove, {userId}, [consultationId]);
+
+					assert.equal(Consultations.find({patientId}).count(), 0);
+					assert.equal(Consultations.find({_id: consultationId}).count(), 0);
+				});
+
+				it("cannot remove other user's consultation", async () => {
+					const userId = Random.id();
+
+					const patientAId = await newPatient({userId});
+
+					const {insertedId: consultationId} = await newConsultation(
+						{userId},
+						{patientId: patientAId},
+					);
+
+					return throws(
+						() =>
+							invoke(consultationsRemove, {userId: `${userId}x`}, [
+								consultationId,
+							]),
+						/not-found/,
+					);
+				});
+
+				it("detaches removed consultation's attachments", async () => {
+					const userId = Random.id();
+
+					assert.equal(Patients.find({}).count(), 0);
+
+					const patientId = await newPatient({userId});
+
+					assert.equal(Patients.find({}).count(), 1);
+
+					assert.equal(Consultations.find({}).count(), 0);
+
+					const {insertedId: consultationId} = await newConsultation(
+						{userId},
+						{patientId},
+					);
+
+					assert.equal(Consultations.find({}).count(), 1);
+
+					assert.equal(Uploads.find({}).count(), 0);
+
+					const uploadA = await newUpload({userId});
+					const uploadB = await newUpload({userId});
+
+					assert.equal(Uploads.find({}).count(), 2);
+					assert.equal(
+						Uploads.find({
+							'meta.attachedToConsultations': consultationId,
+						}).count(),
+						0,
+					);
+					assert.equal(
+						Uploads.find({'meta.attachedToPatients': patientId}).count(),
+						0,
+					);
+
+					await invoke(consultationsAttach, {userId}, [
+						consultationId,
+						uploadA._id,
+					]);
+					await invoke(consultationsAttach, {userId}, [
+						consultationId,
+						uploadB._id,
+					]);
+
+					assert.equal(Patients.find({}).count(), 1);
+					assert.equal(Consultations.find({}).count(), 1);
+					assert.equal(Uploads.find({}).count(), 2);
+					assert.equal(
+						Uploads.find({
+							'meta.attachedToConsultations': consultationId,
+						}).count(),
+						2,
+					);
+					assert.equal(
+						Uploads.find({'meta.attachedToPatients': patientId}).count(),
+						0,
+					);
+
+					await invoke(consultationsRemove, {userId}, [consultationId]);
+
+					assert.equal(Uploads.find({}).count(), 2);
+					assert.equal(
+						Uploads.find({
+							'meta.attachedToConsultations': consultationId,
+						}).count(),
+						0,
+					);
+					assert.equal(
+						Uploads.find({'meta.attachedToPatients': patientId}).count(),
+						0,
+					);
+					assert.equal(Consultations.find({}).count(), 0);
+					assert.equal(Patients.find({}).count(), 1);
+				});
+			});
+		});
+	});
+}
