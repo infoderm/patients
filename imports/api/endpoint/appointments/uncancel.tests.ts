a65bdd92b710816989a399857c52eee75f5f49c4
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-13 14:56:06 +0100

    :microscope: test(appointments/uncancel): Increase coverage.

---
+++ imports/api/endpoint/appointments/uncancel.tests.ts
@@ -0,0 +1,108 @@
+// eslint-disable-next-line import/no-unassigned-import
+import 'regenerator-runtime/runtime.js';
+import {assert} from 'chai';
+
+import {Meteor} from 'meteor/meteor';
+import {Random} from 'meteor/random';
+
+import {Appointments, newAppointment} from '../../collection/appointments.mock';
+
+import {Availability} from '../../collection/availability.mock';
+import {initialSlot, slot} from '../../availability';
+import {beginningOfTime, endOfTime} from '../../../util/datetime';
+import {dropId, dropIds, throws} from '../../../test/fixtures';
+
+import invoke from '../invoke';
+import appointmentsCancel from './cancel';
+import appointmentsUncancel from './uncancel';
+
+if (Meteor.isServer) {
+	describe('endpoint', () => {
+		describe('appointments', () => {
+			describe('uncancel', () => {
+				beforeEach(() => {
+					Appointments.remove({});
+					Availability.remove({});
+				});
+
+				it('can uncancel appointment', async () => {
+					const userId = Random.id();
+
+					const appointmentId = await newAppointment({userId});
+
+					assert.equal(Appointments.find({}).count(), 1);
+					assert.equal(Appointments.find({_id: appointmentId}).count(), 1);
+
+					await invoke(appointmentsCancel, {userId}, [appointmentId, '', '']);
+
+					assert.equal(Appointments.find({}).count(), 1);
+
+					assert.deepInclude(Appointments.findOne({_id: appointmentId}), {
+						isCancelled: true,
+					});
+
+					await invoke(appointmentsUncancel, {userId}, [appointmentId]);
+
+					assert.equal(Appointments.find({}).count(), 1);
+
+					assert.deepInclude(Appointments.findOne({_id: appointmentId}), {
+						isCancelled: false,
+					});
+				});
+
+				it("cannot cancel other user's consultation", async () => {
+					const userId = Random.id();
+
+					const appointmentId = await newAppointment({userId});
+
+					await invoke(appointmentsCancel, {userId}, [appointmentId, '', '']);
+
+					return throws(
+						() =>
+							invoke(appointmentsUncancel, {userId: `${userId}x`}, [
+								appointmentId,
+							]),
+						/not-found/,
+					);
+				});
+
+				it('fills availability', async () => {
+					const userId = Random.id();
+
+					const appointmentId = await newAppointment({userId});
+
+					const {begin, end} = Appointments.findOne(appointmentId);
+
+					const before = Availability.find().fetch();
+
+					const expected = [
+						{
+							...slot(beginningOfTime(), begin, 0),
+							owner: userId,
+						},
+						{
+							...slot(begin, end, 1),
+							owner: userId,
+						},
+						{
+							...slot(end, endOfTime(), 0),
+							owner: userId,
+						},
+					];
+
+					assert.deepEqual(dropIds(before), expected);
+
+					await invoke(appointmentsCancel, {userId}, [appointmentId, '', '']);
+
+					const during = Availability.find().fetch();
+					assert.deepEqual(dropIds(during), [dropId(initialSlot(userId))]);
+
+					await invoke(appointmentsUncancel, {userId}, [appointmentId]);
+					const after = Availability.find().fetch();
+
+					assert.deepEqual(dropIds(after), expected);
+				});
+			});
+		});
+	});
+}
