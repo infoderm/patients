630dbee607ef1230b68607f296a8f4e01e0bea7d
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-13 17:09:01 +0100

    :truck: refactor(client/main): Move router to App to make it standalone.
    This is in preparation for UI tests.

--- client/main.tsx
+++ client/main.tsx
@@ -4,16 +4,10 @@ import React from 'react';
 
 import {Meteor} from 'meteor/meteor';
 import {render} from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
 
 import App from '../imports/ui/App';
 import rootNode from '../imports/ui/rootNode';
 
 Meteor.startup(() => {
-	render(
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>,
-		rootNode(),
-	);
+	render(<App />, rootNode());
 });
