27f3367350a5dbae466a5aa2af3edf2e473d935a
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-13 14:35:45 +0100

    :microscope: test(appointments/beginConsultation): Increase coverage.

---
+++ imports/api/endpoint/appointments/beginConsultation.tests.ts
@@ -0,0 +1,94 @@
+// eslint-disable-next-line import/no-unassigned-import
+import 'regenerator-runtime/runtime.js';
+import {assert} from 'chai';
+
+import {Meteor} from 'meteor/meteor';
+import {Random} from 'meteor/random';
+
+import {Appointments, newAppointment} from '../../collection/appointments.mock';
+
+import {Availability} from '../../collection/availability.mock';
+import {initialSlot, slot} from '../../availability';
+import {beginningOfTime, endOfTime} from '../../../util/datetime';
+import {dropId, dropIds, throws} from '../../../test/fixtures';
+
+import invoke from '../invoke';
+import appointmentsBeginConsultation from './beginConsultation';
+
+if (Meteor.isServer) {
+	describe('endpoint', () => {
+		describe('appointments', () => {
+			describe('beginConsultation', () => {
+				beforeEach(() => {
+					Appointments.remove({});
+					Availability.remove({});
+				});
+
+				it('can begin consultation', async () => {
+					const userId = Random.id();
+
+					const appointmentId = await newAppointment({userId});
+
+					assert.equal(Appointments.find({}).count(), 1);
+					assert.equal(Appointments.find({_id: appointmentId}).count(), 1);
+
+					await invoke(appointmentsBeginConsultation, {userId}, [
+						appointmentId,
+					]);
+
+					assert.equal(Appointments.find({}).count(), 1);
+
+					assert.deepInclude(Appointments.findOne({_id: appointmentId}), {
+						isDone: true,
+					});
+				});
+
+				it("cannot begin other user's consultation", async () => {
+					const userId = Random.id();
+
+					const appointmentId = await newAppointment({userId});
+
+					return throws(
+						() =>
+							invoke(appointmentsBeginConsultation, {userId: `${userId}x`}, [
+								appointmentId,
+							]),
+						/not-found/,
+					);
+				});
+
+				it('restores availability', async () => {
+					const userId = Random.id();
+
+					const appointmentId = await newAppointment({userId});
+
+					const {begin, end} = Appointments.findOne(appointmentId);
+
+					const before = Availability.find().fetch();
+
+					assert.deepEqual(dropIds(before), [
+						{
+							...slot(beginningOfTime(), begin, 0),
+							owner: userId,
+						},
+						{
+							...slot(begin, end, 1),
+							owner: userId,
+						},
+						{
+							...slot(end, endOfTime(), 0),
+							owner: userId,
+						},
+					]);
+
+					await invoke(appointmentsBeginConsultation, {userId}, [
+						appointmentId,
+					]);
+
+					const after = Availability.find().fetch();
+					assert.deepEqual(dropIds(after), [dropId(initialSlot(userId))]);
+				});
+			});
+		});
+	});
+}
