6ea375c3410616720fc3d5b97af3e7fa68ae161b
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-18 18:07:11 +0100

    :adhesive_bandage: fix(api/documents): Exit early when encoding is null.

--- imports/api/documents/decodeText.ts
+++ imports/api/documents/decodeText.ts
@@ -8,7 +8,8 @@ const decodeText = async (encoding: string, array: Uint8Array) => {
 	// const decoded = decoder.decode(array.buffer, {stream: false});
 	const iconv = await import('iconv-lite');
 	// `as Buffer` below is a temporary type hack.
-	// Waiting for https://github.com/ashtuchkin/iconv-lite/issues/235.
+	// Waiting for https://github.com/ashtuchkin/iconv-lite/issues/235
+	// NOTE will throw a meaningful Error if it cannot find the codec
 	return iconv.decode(array as Buffer, encoding);
 };
 
--- imports/api/documents/detectTextEncoding.ts
+++ imports/api/documents/detectTextEncoding.ts
@@ -2,11 +2,13 @@
 // 'iso-8859-1': 'windows-1252',
 // } ;
 
-const detectTextEncoding = async (array: Uint8Array) => {
+const detectTextEncoding = async (
+	array: Uint8Array,
+): Promise<null | string> => {
 	const chardet = await import('chardet');
 	// Const utfLabel = chardet.detect(array).toLowerCase();
 	// const encoding = utfLabelToEncoding[utfLabel] || utfLabel;
-	const encoding = chardet.detect(array).toLowerCase();
+	const encoding = chardet.detect(array)?.toLowerCase();
 	return encoding;
 };
 
