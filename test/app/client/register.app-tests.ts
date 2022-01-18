8d43472e8b6d5ea14206dd6dfbd5b8e2e0fcf3b5
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-18 14:53:54 +0100

    :test_tube: test(integration): Test logging in and out.

--- test/app/client/register.app-tests.ts
+++ test/app/client/register.app-tests.ts
@@ -14,22 +14,78 @@ const setupApp = () => {
 	};
 };
 
+const createUserWithPasswordAndLogin = async (
+	{getByRole, findByRole, getByLabelText, user},
+	username: string,
+	password: string,
+) => {
+	await user.click(await findByRole('button', {name: 'Sign in'}));
+	await user.click(await findByRole('button', {name: 'Create account?'}));
+	await findByRole('button', {name: 'Register'});
+	await user.type(getByLabelText('Username'), username);
+	await user.type(getByLabelText('Password'), password);
+	await user.click(getByRole('button', {name: 'Register'}));
+	await waitForElementToBeRemoved(() => {
+		return getByRole('button', {name: 'Register'});
+	});
+	await findByRole('button', {name: `Logged in as ${username}`});
+};
+
+const logout = async ({getByRole, findByRole, user}) => {
+	await user.click(await findByRole('button', {name: /^Logged in as /}));
+	await user.click(await findByRole('menuitem', {name: 'Logout'}));
+	await waitForElementToBeRemoved(() => {
+		return getByRole('menuitem', {name: 'Logout'});
+	});
+	await findByRole('button', {name: 'Sign in'});
+};
+
+const loginWithPassword = async (
+	{getByRole, findByRole, getByLabelText, user},
+	username: string,
+	password: string,
+) => {
+	await user.click(await findByRole('button', {name: 'Sign in'}));
+	await findByRole('button', {name: 'Log in'});
+	await user.type(getByLabelText('Username'), username);
+	await user.type(getByLabelText('Password'), password);
+	await user.click(getByRole('button', {name: 'Log in'}));
+	await waitForElementToBeRemoved(() => {
+		return getByRole('button', {name: 'Log in'});
+	});
+	await findByRole('button', {name: `Logged in as ${username}`});
+};
+
+const createUserWithPassword = async (
+	app,
+	username: string,
+	password: string,
+) => {
+	await createUserWithPasswordAndLogin(app, username, password);
+	await logout(app);
+};
+
 client(__filename, () => {
 	it('should allow to register a new user', async () => {
 		const username = randomUserId();
 		const password = randomPassword();
-		const {getByRole, findByRole, getByLabelText, user} = setupApp();
-		await user.click(await findByRole('button', {name: 'Sign in'}));
-		await user.click(await findByRole('button', {name: 'Create account?'}));
-		await findByRole('button', {name: 'Register'});
-		await user.type(getByLabelText('Username'), username);
-		await user.type(getByLabelText('Password'), password);
-		await user.click(getByRole('button', {name: 'Register'}));
-		await waitForElementToBeRemoved(() => {
-			return getByRole('button', {name: 'Register'});
-		});
-		await user.click(
-			await findByRole('button', {name: `Logged in as ${username}`}),
-		);
+		const app = setupApp();
+		await createUserWithPasswordAndLogin(app, username, password);
+	});
+
+	it('should allow to log out', async () => {
+		const username = randomUserId();
+		const password = randomPassword();
+		const app = setupApp();
+		await createUserWithPasswordAndLogin(app, username, password);
+		await logout(app);
+	});
+
+	it('should allow to log in', async () => {
+		const username = randomUserId();
+		const password = randomPassword();
+		const app = setupApp();
+		await createUserWithPassword(app, username, password);
+		await loginWithPassword(app, username, password);
 	});
 });
