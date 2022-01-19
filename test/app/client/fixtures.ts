27ee25ab3b1e5226db097c045ef77fe4757dce55
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-19 15:39:58 +0100

    :test_tube: test(client): Add debug statements for login and logout.

--- test/app/client/fixtures.ts
+++ test/app/client/fixtures.ts
@@ -13,25 +13,39 @@ export const createUserWithPasswordAndLogin = async (
 	username: string,
 	password: string,
 ) => {
+	console.debug('Waiting for Sign in button');
 	await user.click(await findByRole('button', {name: 'Sign in'}));
+	console.debug('Waiting for Create account button');
 	await user.click(await findByRole('button', {name: 'Create account?'}));
+	console.debug('Waiting for Register button');
 	await findByRole('button', {name: 'Register'});
+	console.debug('Filling in username');
 	await user.type(getByLabelText('Username'), username);
+	console.debug('Filling in password');
 	await user.type(getByLabelText('Password'), password);
+	console.debug('Clicking the register button');
 	await user.click(getByRole('button', {name: 'Register'}));
+	console.debug('Waiting for the register button to be removed');
 	await waitForElementToBeRemoved(() => {
 		return getByRole('button', {name: 'Register'});
 	});
+	console.debug('Waiting for "Logged in as ..." button to appear');
 	await findByRole('button', {name: `Logged in as ${username}`});
+	console.debug('User successfully created and logged in');
 };
 
 export const logout = async ({getByRole, findByRole, user}) => {
+	console.debug('Waiting for button "Logged in as ..."');
 	await user.click(await findByRole('button', {name: /^Logged in as /}));
+	console.debug('Waiting for Logout menuitem');
 	await user.click(await findByRole('menuitem', {name: 'Logout'}));
+	console.debug('Waiting for Logout menuitem to be removed');
 	await waitForElementToBeRemoved(() => {
 		return getByRole('menuitem', {name: 'Logout'});
 	});
+	console.debug('Waiting for Sign in button to appear');
 	await findByRole('button', {name: 'Sign in'});
+	console.debug('User succesfully logged out');
 };
 
 export const loginWithPassword = async (
