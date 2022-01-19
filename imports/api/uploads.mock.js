d58cf166dd4fcd1824763234c068e95b586fbe16
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-19 15:25:37 +0100

    :recycle: refactor(uploads.mock): Extract random PNG utilities.

--- imports/api/uploads.mock.js
+++ imports/api/uploads.mock.js
@@ -1,15 +1,14 @@
+import {randomPNGBuffer, randomPNGDataURI} from '../test/png';
 import {Uploads} from './uploads';
 
 export const newUpload = async (invocation) => {
 	const type = 'image/png';
 	const fileName = 'pic.png';
-	const base64Encoded =
-		'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
 	if (Meteor.isServer) {
-		const {Buffer} = await import('buffer');
+		const buffer = await randomPNGBuffer();
 		return new Promise((resolve, reject) => {
 			Uploads.write(
-				Buffer.from(base64Encoded, 'base64'),
+				buffer,
 				{
 					fileName,
 					type,
@@ -30,7 +29,7 @@ export const newUpload = async (invocation) => {
 	return new Promise((resolve, reject) => {
 		const upload = Uploads.insert(
 			{
-				file: `data:image/png;base64,${base64Encoded}`,
+				file: randomPNGDataURI(),
 				isBase64: true,
 				fileName,
 				chunkSize: 'dynamic',
