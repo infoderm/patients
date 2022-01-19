cdf0f6d0eb92a4b91b997a178bb72f002a8556f4
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-19 15:25:37 +0100

    :recycle: refactor(uploads.mock): Extract random PNG utilities.

---
+++ imports/test/png.ts
@@ -0,0 +1,23 @@
+const base64Encoded =
+	'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
+
+export const randomPNGBuffer = async () => {
+	const {Buffer} = await import('buffer');
+	return Buffer.from(base64Encoded, 'base64');
+};
+
+export const randomPNGDataURI = (): string =>
+	`data:image/png;base64,${base64Encoded}`;
+
+export const randomPNGResponse = async (): Promise<Response> =>
+	fetch(randomPNGDataURI());
+
+export const randomPNGArrayBuffer = async (): Promise<ArrayBuffer> => {
+	const response = await randomPNGResponse();
+	return response.arrayBuffer();
+};
+
+export const randomPNGBlob = async (): Promise<Blob> => {
+	const response = await randomPNGResponse();
+	return response.blob();
+};
