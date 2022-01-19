365a287dd9b91f9820b9b762b74d4a2e47f55ba4
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-19 11:42:14 +0100

    :test_tube: test(NavigationDrawer): Cover clicking all enabled links.

--- test/app/client/navigate.app-tests.ts
+++ test/app/client/navigate.app-tests.ts
@@ -4,29 +4,30 @@ import {
 	randomUserId,
 } from '../../../imports/test/fixtures';
 
+import {navigationDrawerBlocks} from '../../../imports/ui/NavigationDrawer';
+
 import {setupApp, createUserWithPasswordAndLogin} from './fixtures';
 
-const navigateTo = async ({findByRole, user}, title, url) => {
+const navigateTo = async ({findByRole, user}, title: string, url: string) => {
 	await user.click(await findByRole('button', {name: title}));
 	await findByRole('heading', {name: url});
 };
 
+const testNavigation = (title: string, url: string) => {
+	it(`should allow to reach ${url}`, async () => {
+		const username = randomUserId();
+		const password = randomPassword();
+		const app = setupApp();
+		await createUserWithPasswordAndLogin(app, username, password);
+
+		await navigateTo(app, title, url);
+	});
+};
+
 client(__filename, () => {
-	it('should allow to reach /consultation/last', async () => {
-		const username = randomUserId();
-		const password = randomPassword();
-		const app = setupApp();
-		await createUserWithPasswordAndLogin(app, username, password);
-
-		await navigateTo(app, 'Dernière', '/consultation/last');
-	});
-
-	it('should allow to reach /stats', async () => {
-		const username = randomUserId();
-		const password = randomPassword();
-		const app = setupApp();
-		await createUserWithPasswordAndLogin(app, username, password);
-
-		await navigateTo(app, 'Stats', '/stats');
-	});
+	for (const {links} of navigationDrawerBlocks) {
+		for (const {disabled, title, to} of links) {
+			if (!disabled) testNavigation(title, to);
+		}
+	}
 });
