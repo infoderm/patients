503abeced85611d4ba3face8649358b564967655
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-20 10:05:53 +0100

    :test_tube: test(navigate): Check one can open the monthly agenda.

--- test/app/client/navigate.app-tests.ts
+++ test/app/client/navigate.app-tests.ts
@@ -25,4 +25,14 @@ client(__filename, () => {
 			if (!disabled) testNavigation(title, to);
 		}
 	}
+
+	it('should allow to reach /calendar/month/* from Agenda', async () => {
+		const username = randomUserId();
+		const password = randomPassword();
+		const app = setupApp();
+		await createUserWithPasswordAndLogin(app, username, password);
+
+		await navigateTo(app, 'Agenda', '/calendar/week/current');
+		await navigateTo(app, 'Month', /^\/calendar\/month\//);
+	});
 });
