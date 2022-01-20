c9bb94f2d8ddde6ca084a8cdb590ca6c1932be9f
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-20 10:43:04 +0100

    :test_tube: test: Cover searching for a patient.

---
+++ test/app/client/patient/search.app-tests.ts
@@ -0,0 +1,36 @@
+import {
+	client,
+	randomPassword,
+	randomUserId,
+} from '../../../../imports/test/fixtures';
+
+import {
+	setupApp,
+	fillIn,
+	createUserWithPasswordAndLogin,
+	createNewPatient,
+} from '../fixtures';
+
+client(__filename, () => {
+	it('should allow to search for a patient', async () => {
+		const username = randomUserId();
+		const password = randomPassword();
+		const app = setupApp();
+		await createUserWithPasswordAndLogin(app, username, password);
+
+		const patientId = await createNewPatient(app, {
+			firstname: 'John',
+			lastname: 'Doe',
+		});
+
+		const {findByRole, getByPlaceholderText, user} = app;
+
+		await fillIn(app, getByPlaceholderText(/^Search a patient/), 'John Doe');
+
+		await findByRole('heading', {name: /^Results for query/});
+
+		await user.click(await findByRole('link', {name: /doe john/i}));
+
+		await findByRole('heading', {name: `/patient/${patientId}`});
+	}).timeout(3000);
+});
