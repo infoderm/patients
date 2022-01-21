a2ccdcdb6f7733e8164ec22aa46fba58970db552
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-21 14:27:27 +0100

    :recycle: refactor(test/app): Extract scheduleAppointmentForPatient.

--- test/app/client/patient/appointments.app-tests.ts
+++ test/app/client/patient/appointments.app-tests.ts
@@ -12,6 +12,71 @@ import {
 	searchForPatient,
 } from '../fixtures';
 
+const selectMobileClockButton = async ({user}, button) => {
+	while (!Array.from(button.classList).includes('Mui-focusVisible')) {
+		console.debug(button.classList);
+		// eslint-disable-next-line no-await-in-loop
+		await user.keyboard('[Tab]');
+	}
+
+	await user.keyboard('[Enter]');
+};
+
+const scheduleAppointmentForPatient = async (
+	app,
+	{patientId, firstname, lastname},
+) => {
+	await searchForPatient(app, `${firstname} ${lastname}`, {
+		name: `${firstname} ${lastname}`,
+		id: patientId,
+	});
+
+	const {user, findByRole, findAllByRole} = app;
+	await user.click(await findByRole('button', {name: /^more actions/i}));
+
+	await user.click(
+		await findByRole('button', {name: 'Programmer un nouveau rendez-vous'}),
+	);
+
+	await findByRole('heading', {name: `/new/appointment/for/${patientId}`});
+	const buttons = await findAllByRole('button', {
+		name: /^Schedule an appointment/,
+	});
+	console.debug({buttons: buttons.length});
+	await user.click(buttons[0]);
+	console.debug('Click on choose time');
+	await user.click(await findByRole('textbox', {name: /^Choose time/}));
+	console.debug('Select 1');
+	await selectMobileClockButton(
+		app,
+		await findByRole('button', {name: '1 hours'}),
+	);
+	console.debug('Select 05');
+	await selectMobileClockButton(
+		app,
+		await findByRole('button', {name: '05 minutes'}),
+	);
+	console.debug('Click on PM');
+	await user.click(await findByRole('button', {name: 'PM'}));
+	console.debug('Click on OK');
+	await user.click(await findByRole('button', {name: 'OK'}));
+	console.debug('Set duration to 30m');
+	await user.click(await findByRole('button', {name: '15m'}));
+	await user.click(await findByRole('option', {name: '30m'}));
+	console.debug('Click on Schedule');
+	await user.click(await findByRole('button', {name: 'Schedule'}));
+	console.debug('Check heading');
+	await findByRole('heading', {name: /^\/consultation\//});
+	console.debug('Check button');
+	await findByRole('button', {name: 'Cancel'});
+	await historyBack();
+	await user.click(await findByRole('button', {name: 'Show no-shows'}));
+	await findByRole('link', {
+		name: `13:05-13:35 ${lastname} ${firstname}`,
+		exact: false,
+	});
+};
+
 client(__filename, () => {
 	it('should allow to schedule an appointment for a patient', async () => {
 		const username = randomUserId();
@@ -19,49 +84,14 @@ client(__filename, () => {
 		const app = setupApp();
 		await createUserWithPasswordAndLogin(app, username, password);
 
+		const firstname = 'John';
+		const lastname = 'Doe';
+
 		const patientId = await createNewPatient(app, {
-			firstname: 'John',
-			lastname: 'Doe',
+			firstname,
+			lastname,
 		});
 
-		await searchForPatient(app, 'John Doe', {name: 'John Doe', id: patientId});
-
-		const {user, findByRole, findAllByRole} = app;
-		await user.click(await findByRole('button', {name: /^more actions/i}));
-
-		await user.click(
-			await findByRole('button', {name: 'Programmer un nouveau rendez-vous'}),
-		);
-
-		await findByRole('heading', {name: `/new/appointment/for/${patientId}`});
-		const buttons = await findAllByRole('button', {
-			name: /^Schedule an appointment/,
-		});
-		console.debug({buttons: buttons.length});
-		await user.click(buttons[0]);
-		console.debug('Click on choose time');
-		await user.click(await findByRole('textbox', {name: /^Choose time/}));
-		console.debug('Select 1');
-		await findByRole('button', {name: '1 hours'});
-		await user.keyboard('[Tab][Tab][Tab][Tab][Enter]');
-		console.debug('Select 05');
-		await findByRole('button', {name: '05 minutes'});
-		await user.keyboard('[Tab][Tab][Tab][Tab][Enter]');
-		console.debug('Click on PM');
-		await user.click(await findByRole('button', {name: 'PM'}));
-		console.debug('Click on OK');
-		await user.click(await findByRole('button', {name: 'OK'}));
-		console.debug('Set duration to 30m');
-		await user.click(await findByRole('button', {name: '15m'}));
-		await user.click(await findByRole('option', {name: '30m'}));
-		console.debug('Click on Schedule');
-		await user.click(await findByRole('button', {name: 'Schedule'}));
-		console.debug('Check heading');
-		await findByRole('heading', {name: /^\/consultation\//});
-		console.debug('Check button');
-		await findByRole('button', {name: 'Cancel'});
-		await historyBack();
-		await user.click(await findByRole('button', {name: 'Show no-shows'}));
-		await findByRole('link', {name: /13:05-13:35 doe john/i}, {timeout: 3000});
-	}).timeout(20_000);
+		await scheduleAppointmentForPatient(app, {patientId, firstname, lastname});
+	}).timeout(10_000);
 });
