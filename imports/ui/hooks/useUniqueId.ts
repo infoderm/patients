fdc6f4f35f748c32f4e41d1180f02ad3d5378468
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-21 22:19:14 +0100

    :recycle: refactor(useUniqueId): Mark as deprecated.
    Once we move to MUI v5 we won't need this as MUI v5 solves the problem
    useUniqueId solves.

--- imports/ui/hooks/useUniqueId.ts
+++ imports/ui/hooks/useUniqueId.ts
@@ -1,5 +1,9 @@
 import useUniqueIncreasingBigNumber from './useUniqueIncreasingBigNumber';
 
+/**
+ * @deprecated mui v5 handles this for all inputs and that's the only place we
+ * need this
+ */
 const useUniqueId = (prefix: string) => {
 	const i = useUniqueIncreasingBigNumber();
 	return `${prefix}-${i}`;
