f425bd4b0d216db1ea15bd8521e540c8d60e178e
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-21 14:09:58 +0100

    :sparkles: feat(AppointmentCancellationDialog): Improve accessibility.

--- imports/ui/appointments/AppointmentCancellationDialog.tsx
+++ imports/ui/appointments/AppointmentCancellationDialog.tsx
@@ -8,9 +8,6 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 
@@ -22,6 +19,7 @@ import call from '../../api/endpoint/call';
 import {useSetting} from '../settings/hooks';
 import withLazyOpening from '../modal/withLazyOpening';
 import cancel from '../../api/endpoint/appointments/cancel';
+import useUniqueId from '../hooks/useUniqueId';
 
 const AppointmentCancellationDialog = (props) => {
 	const {open, onClose, appointment} = props;
@@ -29,7 +27,7 @@ const AppointmentCancellationDialog = (props) => {
 	const {loading, value: reasons} = useSetting(
 		'appointment-cancellation-reason',
 	);
-	const [reason, setReason] = useState(loading ? '' : reasons[0] || '');
+	const [reason, setReason] = useState('');
 	const [explanation, setExplanation] = useState('');
 
 	const cancelThisAppointment = async (event) => {
@@ -43,44 +41,48 @@ const AppointmentCancellationDialog = (props) => {
 		}
 	};
 
+	const dialogId = useUniqueId('dialog-appointment-cancellation');
+	const titleId = `${dialogId}-title`;
+	const cancellationReasonInputId = `${dialogId}-input-reason`;
+	const cancellationExplanationInputId = `${dialogId}-input-explanation`;
+
 	return (
 		<Dialog
+			id={dialogId}
 			open={open}
-			aria-labelledby="appointment-cancellation-dialog-title"
+			aria-labelledby={titleId}
 			onClose={onClose}
 		>
-			<DialogTitle id="appointment-cancellation-dialog-title">
-				Cancel this appointment
-			</DialogTitle>
+			<DialogTitle id={titleId}>Cancel this appointment</DialogTitle>
 			<DialogContent>
 				<DialogContentText>
 					If you do not want to cancel this appointment, click cancel. If you
 					really want to cancel this appointment from the system, click cancel.
 				</DialogContentText>
-				<FormControl fullWidth margin="normal">
-					<InputLabel htmlFor="cancellation-reason">
-						Cancellation Reason
-					</InputLabel>
-					<Select
-						readOnly={loading}
-						value={reason}
-						inputProps={{
-							name: 'cancellation-reason',
-							id: 'cancellation-reason',
-						}}
-						onChange={(e) => {
-							setReason(e.target.value);
-						}}
-					>
-						{reasons.map((x) => (
-							<MenuItem key={x} value={x}>
-								{x}
-							</MenuItem>
-						))}
-					</Select>
-				</FormControl>
+				<TextField
+					select
+					fullWidth
+					id={cancellationReasonInputId}
+					margin="normal"
+					label="Cancellation reason"
+					InputProps={{
+						readOnly: loading,
+					}}
+					value={reason}
+					error={!reason}
+					onChange={(e) => {
+						setReason(e.target.value);
+					}}
+				>
+					{reasons.map((x) => (
+						<MenuItem key={x} value={x}>
+							{x}
+						</MenuItem>
+					))}
+				</TextField>
 				<TextField
 					fullWidth
+					id={cancellationExplanationInputId}
 					margin="normal"
 					label="Explanation for cancellation"
 					placeholder="Please give an explanation for the cancellation (optional)"
