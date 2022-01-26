b9b5c6ca213cd602dfa9dea00da49c7ebcbc0878
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-26 10:02:43 +0100

    :test_tube: test(books): Cover more complex state.

--- imports/api/endpoint/books/rename.tests.ts
+++ imports/api/endpoint/books/rename.tests.ts
@@ -16,13 +16,15 @@ import bookRename from './rename';
 server(__filename, () => {
 	it('renaming a book updates its consultation', async () => {
 		const userId = Random.id();
-		const invocation = {userId};
 
 		const patientAId = await newPatient({userId});
 
-		const {insertedId: consultationAId} = await newConsultation(invocation, {
-			patientId: patientAId,
-		});
+		const {insertedId: consultationAId} = await newConsultation(
+			{userId},
+			{
+				patientId: patientAId,
+			},
+		);
 
 		const {datetime, book: oldBookNumber} =
 			Consultations.findOne(consultationAId);
@@ -33,7 +35,7 @@ server(__filename, () => {
 
 		const {_id} = Books.findOne({name});
 
-		await invoke(bookRename, invocation, [_id, newBookNumber]);
+		await invoke(bookRename, {userId}, [_id, newBookNumber]);
 
 		const {book: updatedBookNumber} = Consultations.findOne(consultationAId);
 
@@ -42,28 +44,36 @@ server(__filename, () => {
 
 	it('renaming a book updates its consultations', async () => {
 		const userId = Random.id();
-		const invocation = {userId};
 
 		const patientAId = await newPatient({userId});
 
-		const {insertedId: consultationAId} = await newConsultation(invocation, {
-			patientId: patientAId,
-		});
+		const {insertedId: consultationAId} = await newConsultation(
+			{userId},
+			{
+				patientId: patientAId,
+			},
+		);
 
 		const {datetime, book: oldBookNumber} =
 			Consultations.findOne(consultationAId);
 
-		const {insertedId: consultationBId} = await newConsultation(invocation, {
-			patientId: patientAId,
-			datetime,
-			book: oldBookNumber,
-		});
+		const {insertedId: consultationBId} = await newConsultation(
+			{userId},
+			{
+				patientId: patientAId,
+				datetime,
+				book: oldBookNumber,
+			},
+		);
 
-		const {insertedId: consultationCId} = await newConsultation(invocation, {
-			patientId: patientAId,
-			datetime,
-			book: oldBookNumber,
-		});
+		const {insertedId: consultationCId} = await newConsultation(
+			{userId},
+			{
+				patientId: patientAId,
+				datetime,
+				book: oldBookNumber,
+			},
+		);
 
 		const newBookNumber = `renamed-${oldBookNumber}`;
 
@@ -71,10 +81,94 @@ server(__filename, () => {
 
 		const {_id} = Books.findOne({name});
 
-		await invoke(bookRename, invocation, [_id, newBookNumber]);
+		await invoke(bookRename, {userId}, [_id, newBookNumber]);
 
 		assert.equal(Consultations.findOne(consultationAId).book, newBookNumber);
 		assert.equal(Consultations.findOne(consultationBId).book, newBookNumber);
 		assert.equal(Consultations.findOne(consultationCId).book, newBookNumber);
+	});
+
+	it('renaming a book updates its consultations only', async () => {
+		const userId = Random.id();
+
+		const patientAId = await newPatient({userId});
+		const patientXId = await newPatient({userId: `${userId}x`});
+
+		const {insertedId: consultationAId} = await newConsultation(
+			{userId},
+			{
+				patientId: patientAId,
+			},
+		);
+
+		const {datetime, book: oldBookNumber} =
+			Consultations.findOne(consultationAId);
+
+		const {insertedId: consultationBId} = await newConsultation(
+			{userId},
+			{
+				patientId: patientAId,
+				datetime,
+				book: oldBookNumber,
+			},
+		);
+
+		const {insertedId: consultationCId} = await newConsultation(
+			{userId},
+			{
+				patientId: patientAId,
+				datetime,
+				book: oldBookNumber,
+			},
+		);
+
+		const {insertedId: consultationDId} = await newConsultation(
+			{userId: `${userId}x`},
+			{
+				patientId: patientXId,
+				datetime,
+				book: oldBookNumber,
+			},
+		);
+
+		const {insertedId: consultationEId} = await newConsultation(
+			{userId},
+			{
+				patientId: patientAId,
+				datetime,
+				book: `${oldBookNumber}x`,
+			},
+		);
+
+		const {insertedId: consultationFId} = await newConsultation(
+			{userId: `${userId}x`},
+			{
+				patientId: patientXId,
+				datetime,
+				book: `${oldBookNumber}x`,
+			},
+		);
+
+		const newBookNumber = `renamed-${oldBookNumber}`;
+
+		const name = books.name(datetime, oldBookNumber);
+
+		const {_id} = Books.findOne({name, owner: userId});
+
+		await invoke(bookRename, {userId}, [_id, newBookNumber]);
+
+		assert.equal(Consultations.findOne(consultationAId).book, newBookNumber);
+		assert.equal(Consultations.findOne(consultationBId).book, newBookNumber);
+		assert.equal(Consultations.findOne(consultationCId).book, newBookNumber);
+
+		assert.equal(Consultations.findOne(consultationDId).book, oldBookNumber);
+		assert.equal(
+			Consultations.findOne(consultationEId).book,
+			`${oldBookNumber}x`,
+		);
+		assert.equal(
+			Consultations.findOne(consultationFId).book,
+			`${oldBookNumber}x`,
+		);
 	});
 });
