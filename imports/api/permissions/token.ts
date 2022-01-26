d25a3129a5350fe047a6350d417e8d25e7920a3f
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-26 17:40:00 +0100

    :recycle: refactor: Better types in lib/hmac and api/permissions.

--- imports/api/permissions/token.ts
+++ imports/api/permissions/token.ts
@@ -1,11 +1,8 @@
-import {PermissionTokens} from '../collection/permissionTokens';
 import {
-	extractSignature,
-	sign,
-	Document,
-	SignedDocument,
-	verify,
-} from '../../lib/hmac';
+	PermissionTokenDocument,
+	PermissionTokens,
+} from '../collection/permissionTokens';
+import {extractSignature, sign, SignedDocument, verify} from '../../lib/hmac';
 import {encode as _encode, decode as _decode} from '../../lib/base64/rfc7515';
 import executeTransaction from '../transaction/executeTransaction';
 import {ICS_CALENDAR_READ} from './codes';
@@ -15,7 +12,12 @@ export const encode = (_id: string, key: string) =>
 
 export const decode = (token: string) => JSON.parse(_decode(token));
 
-const extractPermissions = ({_id, ...rest}) => rest as SignedDocument;
+type AddIdKey<T> = T & {_id: string};
+
+const extractPermissions = <T>({
+	_id,
+	...rest
+}: T extends SignedDocument ? AddIdKey<T> : never) => rest;
 
 export class PermissionTokenValidationError extends Error {
 	_HTTPErrorCode: number;
@@ -33,65 +35,67 @@ export class PermissionTokenValidationError extends Error {
 export const getPermissionsForToken = async (
 	token: string,
 	ipAddress: string,
-): Promise<Document> =>
-	executeTransaction(async (db) => {
-		let decoded;
-		try {
-			decoded = decode(token);
-		} catch {
-			throw new PermissionTokenValidationError(422);
-		}
+) =>
+	executeTransaction(
+		async (db): Promise<Omit<PermissionTokenDocument, '_id' | 'signature'>> => {
+			let decoded;
+			try {
+				decoded = decode(token);
+			} catch {
+				throw new PermissionTokenValidationError(422);
+			}
 
-		const {_id, key} = decoded;
+			const {_id, key} = decoded;
 
-		if (typeof _id !== 'string' || typeof key !== 'string') {
-			throw new PermissionTokenValidationError(422);
-		}
+			if (typeof _id !== 'string' || typeof key !== 'string') {
+				throw new PermissionTokenValidationError(422);
+			}
 
-		const now = new Date();
+			const now = new Date();
 
-		const document = await db.findOne(PermissionTokens, {
-			_id,
-			permissions: ICS_CALENDAR_READ,
-			validFrom: {$lte: now},
-			validUntil: {$gt: now}, // TODO use mongo's internal Date?
-		});
+			const document = await db.findOne(PermissionTokens, {
+				_id,
+				permissions: ICS_CALENDAR_READ,
+				validFrom: {$lte: now},
+				validUntil: {$gt: now}, // TODO use mongo's internal Date?
+			});
 
-		if (document === null) {
-			throw new PermissionTokenValidationError(404);
-		}
+			if (document === null) {
+				throw new PermissionTokenValidationError(404);
+			}
 
-		const permissions = extractPermissions(document); // _id is not part of signature
+			const permissions = extractPermissions(document); // _id is not part of signature
 
-		const signatureIsValid: boolean = await verify(key, permissions);
+			const signatureIsValid: boolean = await verify(key, permissions);
 
-		if (!signatureIsValid) {
-			// throw new PermissionTokenValidationError(401); // see https://stackoverflow.com/q/45153773 and https://stackoverflow.com/a/1960453
-			throw new PermissionTokenValidationError(
-				404 /* stealth, although timing side-channel is exposed */,
-			);
-		}
+			if (!signatureIsValid) {
+				// throw new PermissionTokenValidationError(401); // see https://stackoverflow.com/q/45153773 and https://stackoverflow.com/a/1960453
+				throw new PermissionTokenValidationError(
+					404 /* stealth, although timing side-channel is exposed */,
+				);
+			}
 
-		const lastUsedIPAddress = ipAddress;
-		const lastUsedAt = now;
-		const newPermissions = {
-			...extractSignature(permissions).document,
-			lastUsedIPAddress,
-			lastUsedAt,
-		};
-		const newSignature = await sign(key, newPermissions);
+			const lastUsedIPAddress = ipAddress;
+			const lastUsedAt = now;
+			const newPermissions = {
+				...extractSignature(permissions).document,
+				lastUsedIPAddress,
+				lastUsedAt,
+			};
+			const newSignature = await sign(key, newPermissions);
 
-		await db.updateOne(
-			PermissionTokens,
-			{_id},
-			{
-				$set: {
-					lastUsedAt,
-					lastUsedIPAddress,
-					signature: newSignature,
+			await db.updateOne(
+				PermissionTokens,
+				{_id},
+				{
+					$set: {
+						lastUsedAt,
+						lastUsedIPAddress,
+						signature: newSignature,
+					},
 				},
-			},
-		);
+			);
 
-		return newPermissions;
-	});
+			return newPermissions;
+		},
+	);
