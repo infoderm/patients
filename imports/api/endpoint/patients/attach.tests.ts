5f2c3e0965f2cdb8ce01aec1eea2699158495082
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-27 13:31:02 +0100

    :test_tube: test(patient): Cover attach API endpoint.

---
+++ imports/api/endpoint/patients/attach.tests.ts
@@ -0,0 +1,135 @@
+// eslint-disable-next-line import/no-unassigned-import
+import 'regenerator-runtime/runtime.js';
+import {assert} from 'chai';
+
+import {randomUserId, server, throws} from '../../../test/fixtures';
+
+import {Patients} from '../../collection/patients';
+import {Attachments} from '../../collection/attachments';
+
+import {newPatient} from '../../_dev/populate/patients';
+import {newUpload} from '../../_dev/populate/uploads';
+
+import invoke from '../invoke';
+
+import patientsAttach from './attach';
+
+server(__filename, () => {
+	it('can attach uploads to patients', async () => {
+		const userId = randomUserId();
+		const invocation = {userId};
+
+		const patientAId = await newPatient({userId});
+		const patientBId = await newPatient({userId});
+		await newPatient({userId});
+
+		const {_id: uploadAId} = await newUpload({userId});
+		const {_id: uploadBId} = await newUpload({userId});
+
+		assert.equal(Patients.find().count(), 3);
+		assert.equal(Attachments.find().count(), 2);
+
+		await invoke(patientsAttach, invocation, [patientAId, uploadAId]);
+		await invoke(patientsAttach, invocation, [patientBId, uploadBId]);
+
+		assert.deepNestedInclude(Attachments.findOne(uploadAId), {
+			'meta.attachedToPatients': [patientAId],
+		});
+
+		assert.deepNestedInclude(Attachments.findOne(uploadBId), {
+			'meta.attachedToPatients': [patientBId],
+		});
+		assert.equal(Patients.find().count(), 3);
+		assert.equal(Attachments.find().count(), 2);
+	});
+
+	it('is idempotent', async () => {
+		const userId = randomUserId();
+
+		const patientId = await newPatient({userId});
+
+		const {_id: uploadId} = await newUpload({userId});
+
+		assert.equal(
+			Attachments.findOne({
+				'meta.attachedToPatients': patientId,
+			}),
+			undefined,
+		);
+
+		await invoke(patientsAttach, {userId}, [patientId, uploadId]);
+
+		assert.deepInclude(
+			Attachments.findOne({
+				'meta.attachedToPatients': patientId,
+			}),
+			{
+				_id: uploadId,
+			},
+		);
+
+		await invoke(patientsAttach, {userId}, [patientId, uploadId]);
+
+		assert.deepInclude(
+			Attachments.findOne({
+				'meta.attachedToPatients': patientId,
+			}),
+			{
+				_id: uploadId,
+			},
+		);
+	});
+
+	it('cannot attach upload to a patient that is not owned', async () => {
+		const userId = randomUserId();
+
+		const patientXId = await newPatient({userId: `${userId}x`});
+
+		const {_id: uploadId} = await newUpload({userId});
+
+		return throws(
+			async () => invoke(patientsAttach, {userId}, [patientXId, uploadId]),
+			/not-found/,
+		);
+	});
+
+	it('cannot attach non-owned upload to a patient', async () => {
+		const userId = randomUserId();
+
+		const patientId = await newPatient({userId});
+
+		const {_id: uploadXId} = await newUpload({userId: `${userId}x`});
+
+		return throws(
+			async () => invoke(patientsAttach, {userId}, [patientId, uploadXId]),
+			/not-found/,
+		);
+	});
+
+	it('cannot attach upload to a patient when both are not owned', async () => {
+		const userId = randomUserId();
+
+		const patientXId = await newPatient({userId: `${userId}x`});
+
+		const {_id: uploadXId} = await newUpload({userId: `${userId}x`});
+
+		return throws(
+			async () => invoke(patientsAttach, {userId}, [patientXId, uploadXId]),
+			/not-found/,
+		);
+	});
+
+	it('cannot attach upload to a patient when not logged in', async () => {
+		const userId = randomUserId();
+
+		const patientId = await newPatient({userId});
+
+		const {_id: uploadId} = await newUpload({userId});
+
+		return throws(
+			async () =>
+				invoke(patientsAttach, {userId: undefined}, [patientId, uploadId]),
+			/not-authorized/,
+		);
+	});
+});
