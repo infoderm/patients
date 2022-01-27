a4aa1978815684b9622581b30093aa91c40710a4
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-27 12:53:32 +0100

    :test_tube: test(uploads): Cover all API endpoints.

---
+++ imports/api/endpoint/uploads/delete.tests.ts
@@ -0,0 +1,74 @@
+// eslint-disable-next-line import/no-unassigned-import
+import 'regenerator-runtime/runtime.js';
+
+import {assert} from 'chai';
+
+import {randomUserId, server, throws} from '../../../test/fixtures';
+
+import {Attachments} from '../../collection/attachments';
+import {newUpload} from '../../_dev/populate/uploads';
+
+import invoke from '../invoke';
+import uploadsDelete from './delete';
+
+server(__filename, () => {
+	it('can mark an upload as deleted', async () => {
+		const userId = randomUserId();
+
+		const {_id: uploadId} = await newUpload({userId});
+
+		assert.notDeepNestedInclude(Attachments.findOne(uploadId), {
+			'meta.isDeleted': true,
+		});
+
+		await invoke(uploadsDelete, {userId}, [uploadId]);
+
+		assert.deepNestedInclude(Attachments.findOne(uploadId), {
+			'meta.isDeleted': true,
+		});
+	});
+
+	it('is idempotent', async () => {
+		const userId = randomUserId();
+
+		const {_id: uploadId} = await newUpload({userId});
+
+		await invoke(uploadsDelete, {userId}, [uploadId]);
+
+		await invoke(uploadsDelete, {userId}, [uploadId]);
+
+		assert.deepNestedInclude(Attachments.findOne(uploadId), {
+			'meta.isDeleted': true,
+		});
+	});
+
+	it("cannot mark another user's upload as deleted", async () => {
+		const userId = randomUserId();
+
+		const {_id: uploadId} = await newUpload({userId});
+
+		await throws(
+			async () => invoke(uploadsDelete, {userId: `${userId}x`}, [uploadId]),
+			/not-found/,
+		);
+
+		assert.notDeepNestedInclude(Attachments.findOne(uploadId), {
+			'meta.isDeleted': true,
+		});
+	});
+
+	it('cannot mark an upload as deleted if not logged in', async () => {
+		const userId = randomUserId();
+
+		const {_id: uploadId} = await newUpload({userId});
+
+		await throws(
+			async () => invoke(uploadsDelete, {userId: undefined}, [uploadId]),
+			/not-authorized/,
+		);
+
+		assert.notDeepNestedInclude(Attachments.findOne(uploadId), {
+			'meta.isDeleted': true,
+		});
+	});
+});
---
+++ imports/api/endpoint/uploads/restore.tests.ts
@@ -0,0 +1,85 @@
+// eslint-disable-next-line import/no-unassigned-import
+import 'regenerator-runtime/runtime.js';
+
+import {assert} from 'chai';
+
+import {randomUserId, server, throws} from '../../../test/fixtures';
+
+import {Attachments} from '../../collection/attachments';
+import {newUpload} from '../../_dev/populate/uploads';
+
+import invoke from '../invoke';
+import uploadsDelete from './delete';
+import uploadsRestore from './restore';
+
+server(__filename, () => {
+	it('can restore an upload', async () => {
+		const userId = randomUserId();
+
+		const {_id: uploadId} = await newUpload({userId});
+
+		await invoke(uploadsDelete, {userId}, [uploadId]);
+
+		await invoke(uploadsRestore, {userId}, [uploadId]);
+
+		assert.deepNestedInclude(Attachments.findOne(uploadId), {
+			'meta.isDeleted': false,
+		});
+	});
+
+	it('is idempotent', async () => {
+		const userId = randomUserId();
+
+		const {_id: uploadId} = await newUpload({userId});
+
+		await invoke(uploadsRestore, {userId}, [uploadId]);
+
+		assert.deepNestedInclude(Attachments.findOne(uploadId), {
+			'meta.isDeleted': false,
+		});
+
+		await invoke(uploadsDelete, {userId}, [uploadId]);
+
+		await invoke(uploadsRestore, {userId}, [uploadId]);
+
+		await invoke(uploadsRestore, {userId}, [uploadId]);
+
+		assert.deepNestedInclude(Attachments.findOne(uploadId), {
+			'meta.isDeleted': false,
+		});
+	});
+
+	it("cannot restore another user's upload", async () => {
+		const userId = randomUserId();
+
+		const {_id: uploadId} = await newUpload({userId});
+
+		await invoke(uploadsDelete, {userId}, [uploadId]);
+
+		await throws(
+			async () => invoke(uploadsRestore, {userId: `${userId}x`}, [uploadId]),
+			/not-found/,
+		);
+
+		assert.deepNestedInclude(Attachments.findOne(uploadId), {
+			'meta.isDeleted': true,
+		});
+	});
+
+	it('cannot mark an upload as deleted if not logged in', async () => {
+		const userId = randomUserId();
+
+		const {_id: uploadId} = await newUpload({userId});
+
+		await invoke(uploadsDelete, {userId}, [uploadId]);
+
+		await throws(
+			async () => invoke(uploadsRestore, {userId: undefined}, [uploadId]),
+			/not-authorized/,
+		);
+
+		assert.deepNestedInclude(Attachments.findOne(uploadId), {
+			'meta.isDeleted': true,
+		});
+	});
+});
