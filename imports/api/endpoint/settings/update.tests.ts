79a8d77b09563189d0ac0eced730bf14fb3bb96c
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-27 11:12:48 +0100

    :test_tube: test(settings): Cover update API endpoint.

---
+++ imports/api/endpoint/settings/update.tests.ts
@@ -0,0 +1,77 @@
+// eslint-disable-next-line import/no-unassigned-import
+import 'regenerator-runtime/runtime.js';
+
+import {assert} from 'chai';
+
+import {randomUserId, server, throws} from '../../../test/fixtures';
+
+import invoke from '../invoke';
+import {settings, get} from '../../settings';
+import update from './update';
+
+server(__filename, () => {
+	it('can set a setting', async () => {
+		const userId = randomUserId();
+
+		const key = 'x';
+		const value = 123_819_832;
+
+		assert.equal(get(userId, key), settings.defaults[key]);
+
+		await invoke(update, {userId}, [key, value]);
+
+		assert.equal(get(userId, key), value);
+	});
+
+	it('can update a setting', async () => {
+		const userId = randomUserId();
+
+		const key = 'x';
+		const value = 123_819_832;
+		const updated = `updated-${value}`;
+
+		await invoke(update, {userId}, [key, value]);
+
+		assert.equal(get(userId, key), value);
+
+		await invoke(update, {userId}, [key, updated]);
+
+		assert.equal(get(userId, key), updated);
+	});
+
+	it('cannot set a setting if not logged in', async () => {
+		const key = 'x';
+		const value = 123_819_832;
+
+		return throws(
+			async () => invoke(update, {userId: undefined}, [key, value]),
+			/not-authorized/,
+		);
+	});
+
+	it('cannot update a setting if not logged in', async () => {
+		const userId = randomUserId();
+
+		const key = 'x';
+		const value = 123_819_832;
+		const updated = `updated-${value}`;
+
+		await invoke(update, {userId}, [key, value]);
+
+		return throws(
+			async () => invoke(update, {userId: undefined}, [key, updated]),
+			/not-authorized/,
+		);
+	});
+
+	it("does not read other user's settings", async () => {
+		const userId = randomUserId();
+
+		const key = 'x';
+		const value = 123_819_832;
+
+		await invoke(update, {userId}, [key, value]);
+
+		assert.equal(get(`${userId}x`, key), settings.defaults[key]);
+	});
+});
