5ac0b389991e1993545b3ab82cc1b686e8ad3a72
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-27 11:18:45 +0100

    :test_tube: test(settings): Cover reset API endpoint.

---
+++ imports/api/endpoint/settings/reset.tests.ts
@@ -0,0 +1,84 @@
+// eslint-disable-next-line import/no-unassigned-import
+import 'regenerator-runtime/runtime.js';
+
+import {assert} from 'chai';
+
+import {randomUserId, server, throws} from '../../../test/fixtures';
+
+import invoke from '../invoke';
+import {settings, get} from '../../settings';
+import update from './update';
+import reset from './reset';
+
+server(__filename, () => {
+	it('can reset a setting', async () => {
+		const userId = randomUserId();
+
+		const key = 'currency';
+		const value = 123_819_832;
+
+		assert.equal(get(userId, key), settings.defaults[key]);
+
+		await invoke(update, {userId}, [key, value]);
+
+		assert.equal(get(userId, key), value);
+
+		await invoke(reset, {userId}, [key]);
+
+		assert.equal(get(userId, key), settings.defaults[key]);
+	});
+
+	it('is idempotent', async () => {
+		const userId = randomUserId();
+
+		const key = 'x';
+		const value = 123_819_832;
+
+		assert.equal(get(userId, key), settings.defaults[key]);
+
+		await invoke(reset, {userId}, [key]);
+
+		assert.equal(get(userId, key), settings.defaults[key]);
+
+		await invoke(update, {userId}, [key, value]);
+
+		assert.equal(get(userId, key), value);
+
+		await invoke(reset, {userId}, [key]);
+
+		assert.equal(get(userId, key), settings.defaults[key]);
+
+		await invoke(reset, {userId}, [key]);
+
+		assert.equal(get(userId, key), settings.defaults[key]);
+	});
+
+	it('cannot reset a setting if not logged in', async () => {
+		const key = 'x';
+
+		return throws(
+			async () => invoke(reset, {userId: undefined}, [key]),
+			/not-authorized/,
+		);
+	});
+
+	it("does not reset other user's settings", async () => {
+		const userId = randomUserId();
+
+		const key = 'currency';
+		const value = 123_819_832;
+		const valueX = `x${value}`;
+
+		await invoke(update, {userId}, [key, value]);
+		await invoke(update, {userId: `${userId}x`}, [key, valueX]);
+
+		assert.equal(get(userId, key), value);
+		assert.equal(get(`${userId}x`, key), valueX);
+
+		await invoke(reset, {userId}, [key]);
+		assert.equal(get(`${userId}x`, key), valueX);
+
+		await invoke(reset, {userId}, [key]);
+		assert.equal(get(`${userId}x`, key), valueX);
+	});
+});
