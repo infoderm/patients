d094b884237f56a6194cc7636f3a630513e3704f
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-01-27 16:13:23 +0100

    :test_tube: test(documents): Cover insert endpoint.

---
+++ imports/api/endpoint/documents/insert.tests.ts
@@ -0,0 +1,134 @@
+// eslint-disable-next-line import/no-unassigned-import
+import 'regenerator-runtime/runtime.js';
+import {assert} from 'chai';
+
+import {randomUserId, server, throws} from '../../../test/fixtures';
+
+import {Documents} from '../../collection/documents';
+import {newPatient} from '../../_dev/populate/patients';
+
+import invoke from '../invoke';
+import {
+	exampleHealthoneLab,
+	exampleHealthoneReport,
+} from '../../_dev/populate/documents';
+import insert from './insert';
+
+server(__filename, () => {
+	it('cannot insert a document when not logged in', async () => {
+		await throws(
+			async () =>
+				invoke(insert, {userId: undefined}, [
+					{
+						array: new Uint8Array(0),
+					},
+				]),
+			/not-authorized/,
+		);
+
+		assert.equal(Documents.find().count(), 0);
+	});
+
+	it('cannot insert a document linked to a non-existing patient', async () => {
+		const userId = randomUserId();
+		await throws(
+			async () =>
+				invoke(insert, {userId}, [
+					{
+						patientId: 'x',
+						array: new Uint8Array(0),
+					},
+				]),
+			/not-found/,
+		);
+
+		assert.equal(Documents.find().count(), 0);
+	});
+
+	it('cannot insert a document linked to a patient that is not owned', async () => {
+		const userId = randomUserId();
+		const patientId = await newPatient({userId: `${userId}x`});
+		await throws(
+			async () =>
+				invoke(insert, {userId}, [
+					{
+						patientId,
+						array: new Uint8Array(0),
+					},
+				]),
+			/not-found/,
+		);
+
+		assert.equal(Documents.find().count(), 0);
+	});
+
+	it('can insert a document linked to a patient that is owned', async () => {
+		const userId = randomUserId();
+		const patientId = await newPatient({userId});
+		const documentIds = await invoke(insert, {userId}, [
+			{
+				patientId,
+				array: new TextEncoder().encode(exampleHealthoneLab.contents),
+			},
+		]);
+
+		assert.equal(Documents.find().count(), exampleHealthoneLab.count);
+		for (const documentId of documentIds) {
+			assert.deepInclude(Documents.findOne(documentId), {
+				patientId,
+			});
+		}
+	});
+
+	it('can automatically link to an owned patient', async () => {
+		const userId = randomUserId();
+		const firstname = 'Jane';
+		const lastname = 'Doe';
+		await newPatient({userId: `${userId}A`}, {firstname, lastname});
+		const patientId = await newPatient({userId}, {firstname, lastname});
+		await newPatient({userId: `${userId}B`}, {firstname, lastname});
+		await invoke(insert, {userId}, [
+			{
+				array: new TextEncoder().encode(exampleHealthoneReport.contents),
+			},
+		]);
+
+		assert.equal(Documents.find().count(), exampleHealthoneReport.count);
+		assert.equal(Documents.find({patientId}).count(), 1);
+	});
+
+	it('set patientId overwrites patient matching', async () => {
+		const userId = randomUserId();
+		const firstname = 'John';
+		const lastname = 'Doe';
+		const patientId = await newPatient({userId}, {firstname, lastname});
+		await invoke(insert, {userId}, [
+			{
+				patientId,
+				array: new TextEncoder().encode(exampleHealthoneReport.contents),
+			},
+		]);
+
+		assert.equal(Documents.find().count(), exampleHealthoneReport.count);
+		assert.equal(
+			Documents.find({patientId}).count(),
+			exampleHealthoneReport.count,
+		);
+	});
+
+	it('does not automatically link when multiple patients match', async () => {
+		const userId = randomUserId();
+		const firstname = 'Jane';
+		const lastname = 'Doe';
+		await newPatient({userId}, {firstname, lastname});
+		await newPatient({userId}, {firstname, lastname});
+		await invoke(insert, {userId}, [
+			{
+				array: new TextEncoder().encode(exampleHealthoneReport.contents),
+			},
+		]);
+
+		assert.equal(Documents.find().count(), exampleHealthoneReport.count);
+		assert.equal(Documents.find({patientId: {$exists: true}}).count(), 0);
+	});
+});
