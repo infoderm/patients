infoderm/patients

commit bad432dfb216b505db182d25e9cc49d384c0dc9a

:recycle: refactor(Router): Only use children prop to render routes.
This is progress towards upgrading react-router to v6 (#424).
Had to narrow types of ~60 components on the way.

imports/ui/planner/WeeklyPlannerRoute.tsx
@@ -1,0 +1,24 @@
+import React from 'react';
+import {useParams} from 'react-router-dom';
+import PropsOf from '../../util/PropsOf';
+import WeeklyPlanner from './WeeklyPlanner';
+
+interface Params {
+	year?: string;
+	week?: string;
+}
+
+type Props = Omit<PropsOf<typeof WeeklyPlanner>, keyof Params>;
+
+const WeeklyPlannerRoute = (props: Props) => {
+	const {year, week} = useParams<Params>();
+	return (
+		<WeeklyPlanner
+			year={Number.parseInt(year, 10)}
+			week={Number.parseInt(week, 10)}
+			{...props}
+		/>
+	);
+};
+
+export default WeeklyPlannerRoute;

commit 639ea9d4bd9aeacf1bebd6270893601abc7487b8

:adhesive_bandage: fix: Display long availability slots correctly.
This bug was introduced in bad432d. TODO squash with that commit.

imports/ui/planner/WeeklyPlannerRoute.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
 import PropsOf from '../../util/PropsOf';
-import WeeklyPlanner from './WeeklyPlanner';
+import PreconfiguredWeeklyPlanner from './PreconfiguredWeeklyPlanner';
 
 interface Params {
 	year?: string;
 	week?: string;
 }
 
-type Props = Omit<PropsOf<typeof WeeklyPlanner>, keyof Params>;
+type Props = Omit<PropsOf<typeof PreconfiguredWeeklyPlanner>, keyof Params>;
 
 const WeeklyPlannerRoute = (props: Props) => {
 	const {year, week} = useParams<Params>();
 	return (
-		<WeeklyPlanner
+		<PreconfiguredWeeklyPlanner
 			year={Number.parseInt(year, 10)}
 			week={Number.parseInt(week, 10)}
 			{...props}