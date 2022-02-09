bf873516d37d2433bd7ecb974a0e998035bbd9db
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-02-09 20:19:04 +0100

    :recycle: refactor(ui/users): Use TypeScript, styled, and Props.

--- imports/ui/users/Popover.ts
+++ imports/ui/users/Popover.ts
@@ -1,15 +1,18 @@
-import makeStyles from '@mui/styles/makeStyles';
+import {styled} from '@mui/material/styles';
+import MuiPopover from '@mui/material/Popover';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
-export const useStyles = makeStyles((theme) => ({
-	popover: {
-		display: 'flex',
-	},
-	row: {
-		display: 'block',
-		margin: theme.spacing(1),
-		width: 200,
-	},
-	form: {
-		display: 'block',
-	},
-}));
+export const Popover = styled(MuiPopover)({
+	display: 'flex',
+});
+
+const row = ({theme}) => ({
+	display: 'block',
+	margin: theme.spacing(1),
+	width: 200,
+});
+
+export const RowTextField = styled(TextField)(row);
+export const RowButton = styled(Button)(row);
+export const Form = styled('form')({display: 'block'});
