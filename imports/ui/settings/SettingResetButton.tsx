968ff7d52976e28bfcbaf74aff7154abb04421c6
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-02-09 11:53:12 +0100

    :construction: progress: Define SettingResetButton.

---
+++ imports/ui/settings/SettingResetButton.tsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+
+import LoadingButton from '@mui/lab/LoadingButton';
+import CancelIcon from '@mui/icons-material/Cancel';
+
+import {useSetting} from './hooks';
+
+interface Props {
+	setting: string;
+}
+
+const SettingResetButton = ({setting}: Props) => {
+	const {loading, resetValue} = useSetting(setting);
+	const [resetting, setResetting] = useState(false);
+
+	const onClick = async () => {
+		setResetting(true);
+		try {
+			await resetValue();
+		} catch (error: unknown) {
+			console.error(error);
+		}
+
+		setResetting(false);
+	};
+
+	return (
+		<LoadingButton
+			disabled={loading}
+			color="secondary"
+			loading={resetting}
+			loadingPosition="end"
+			endIcon={<CancelIcon />}
+			onClick={onClick}
+		>
+			Reset
+		</LoadingButton>
+	);
+};
+
+export default SettingResetButton;
