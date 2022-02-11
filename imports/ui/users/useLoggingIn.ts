931bf3ab0caaf4bb9c3b7411d54e5080ddb7e812
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-02-11 13:37:42 +0100

    :adhesive_bandage: fix(ui/users): Fix and simplify hooks.

--- imports/ui/users/useLoggingIn.ts
+++ imports/ui/users/useLoggingIn.ts
@@ -1,21 +1,6 @@
 import {Meteor} from 'meteor/meteor';
 import {useTracker} from 'meteor/react-meteor-data';
 
-import {useDebounce} from 'use-debounce';
-
-import {TIMEOUT_REACTIVITY_DEBOUNCE} from '../constants';
-
-const useLoggingIn = () => {
-	const loggingIn = useTracker(() => Meteor.loggingIn(), []);
-
-	const [debouncedLoggingIn] = useDebounce(
-		loggingIn,
-		TIMEOUT_REACTIVITY_DEBOUNCE,
-	);
-
-	const displayLoggingIn = loggingIn && debouncedLoggingIn;
-
-	return displayLoggingIn;
-};
+const useLoggingIn = () => useTracker(() => Meteor.loggingIn());
 
 export default useLoggingIn;
