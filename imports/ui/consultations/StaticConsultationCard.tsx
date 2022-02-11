infoderm/patients

commit d64cb74004f7f58fbc983b2a5c4f283c06dbdbd2

:recycle: refactor: Replace last uses of defaultProps.
Fixes #305.

imports/ui/consultations/StaticConsultationCard.tsx
@@ -4,6 +4,8 @@ import {styled} from '@mui/material/styles';
 import MuiAccordion from '@mui/material/Accordion';
 import Divider from '@mui/material/Divider';
 
+import PropsOf from '../../util/PropsOf';
+
 import virtualFields from '../../api/consultations/virtualFields';
 
 import StaticConsultationCardSummary from './StaticConsultationCardSummary';
@@ -36,13 +38,18 @@ const Accordion = styled(MuiAccordion, {shouldForwardProp})<AdditionalProps>(
 	}),
 );
 
-interface StaticConsultationCardProps {
+interface StaticConsultationCardProps
+	extends Omit<
+		PropsOf<typeof StaticConsultationCardActions>,
+		| 'isNoShow'
+		| 'missingPaymentData'
+		| 'didNotOrWillNotHappen'
+		| 'owes'
+		| 'owed'
+	> {
 	consultation: any;
 	loading?: boolean;
 	found?: boolean;
-	attachments?: any[];
-	PatientChip?: JSX.Element;
-	showPrice?: boolean;
 	defaultExpanded?: boolean;
 }
 

imports/ui/consultations/StaticConsultationCardChips.tsx
@@ -84,15 +84,15 @@ function paymentMethodIcon(payment_method) {
 	}
 }
 
-export interface StaticConsultationCardChipsProps {
+interface StaticConsultationCardChipsProps {
 	isNoShow: boolean;
-	patient: PatientDocument;
+	patient?: PatientDocument;
 	consultation: ConsultationDocument;
 	PatientChip: React.ElementType;
-	showDate: boolean;
-	showTime: boolean;
-	showPrice: boolean;
-	loadingPatient: boolean;
+	showDate?: boolean;
+	showTime?: boolean;
+	showPrice?: boolean;
+	loadingPatient?: boolean;
 	missingPaymentData: boolean;
 	didNotOrWillNotHappen: boolean;
 	owes: boolean;
@@ -160,7 +160,7 @@ const StaticConsultationCardChips = (
 				<PatientChip
 					loading={loadingPatient}
 					found={Boolean(patient)}
-					patient={patient || {_id: patientId}}
+					patient={patient ?? {_id: patientId}}
 				/>
 			)}
 			{isCancelled && <Chip didNotOrWillNotHappen bold label="cancelled" />}

imports/ui/consultations/StaticConsultationDetails.tsx
@@ -4,19 +4,10 @@ import {styled} from '@mui/material/styles';
 import Loading from '../navigation/Loading';
 import NoContent from '../navigation/NoContent';
 
-import ReactivePatientChip from '../patients/ReactivePatientChip';
 import ReactiveConsultationCard from './ReactiveConsultationCard';
 
-const PREFIX = 'StaticConsultationDetails';
-
-const classes = {
-	container: `${PREFIX}-container`,
-};
-
 const Root = styled('div')(({theme}) => ({
-	[`&.${classes.container}`]: {
-		padding: theme.spacing(3),
-	},
+	padding: theme.spacing(3),
 }));
 
 const StaticConsultationDetails = ({loading, found, consultation}) => {
@@ -29,12 +20,8 @@ const StaticConsultationDetails = ({loading, found, consultation}) => {
 	}
 
 	return (
-		<Root className={classes.container}>
-			<ReactiveConsultationCard
-				defaultExpanded
-				PatientChip={ReactivePatientChip}
-				consultation={consultation}
-			/>
+		<Root>
+			<ReactiveConsultationCard defaultExpanded consultation={consultation} />
 		</Root>
 	);
 };

imports/ui/issues/ConsultationsMissingABook.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import ReactiveConsultationCard from '../consultations/ReactiveConsultationCard';
-import ReactivePatientChip from '../patients/ReactivePatientChip';
 
 import {useConsultationsMissingABook} from '../../api/issues';
 
@@ -22,7 +21,6 @@ const ConsultationsMissingABook = (props) => {
 				<ReactiveConsultationCard
 					key={consultation._id}
 					consultation={consultation}
-					PatientChip={ReactivePatientChip}
 				/>
 			))}
 		</div>

imports/ui/issues/ConsultationsMissingAPrice.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import ReactiveConsultationCard from '../consultations/ReactiveConsultationCard';
-import ReactivePatientChip from '../patients/ReactivePatientChip';
 
 import {useConsultationsMissingAPrice} from '../../api/issues';
 
@@ -25,7 +24,6 @@ const ConsultationsMissingAPrice = (props) => {
 				<ReactiveConsultationCard
 					key={consultation._id}
 					consultation={consultation}
-					PatientChip={ReactivePatientChip}
 				/>
 			))}
 		</div>

imports/ui/consultations/ConsultationAdvancedActionsDialog.tsx
@@ -15,38 +15,27 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import RestoreIcon from '@mui/icons-material/Restore';
 import LinkOffIcon from '@mui/icons-material/LinkOff';
 
-import {blue, red} from '@mui/material/colors';
+import {red} from '@mui/material/colors';
 
 import ReactivePatientChip from '../patients/ReactivePatientChip';
 
 import AppointmentDeletionDialog from '../appointments/AppointmentDeletionDialog';
-import StaticConsultationCardChips, {
-	StaticConsultationCardChipsProps,
-} from './StaticConsultationCardChips';
+import PropsOf from '../../util/PropsOf';
+import StaticConsultationCardChips from './StaticConsultationCardChips';
 import ConsultationDeletionDialog from './ConsultationDeletionDialog';
 import ConsultationAppointmentRestorationDialog from './ConsultationAppointmentRestorationDialog';
 import ConsultationTransferDialog from './ConsultationTransferDialog';
 
-const PREFIX = 'ConsultationAdvancedActionsDialog';
-
-const classes = {
-	primary: `${PREFIX}-primary`,
-	secondary: `${PREFIX}-secondary`,
-};
-
-// TODO jss-to-styled codemod: The Fragment root was replaced by div. Change the tag if needed.
-const Root = styled('div')({
-	[`& .${classes.primary}`]: {
-		backgroundColor: blue[100],
-		color: blue[600],
-	},
-	[`& .${classes.secondary}`]: {
-		backgroundColor: red[100],
-		color: red[600],
-	},
+const SecondaryAvatar = styled(Avatar)({
+	backgroundColor: red[100],
+	color: red[600],
 });
 
-interface Props extends StaticConsultationCardChipsProps {
+interface Props
+	extends Omit<
+		PropsOf<typeof StaticConsultationCardChips>,
+		'showDate' | 'showTime' | 'showPrice' | 'PatientChip'
+	> {
 	open: boolean;
 	onClose: () => void;
 }
@@ -59,7 +48,7 @@ const ConsultationAdvancedActionsDialog = ({open, onClose, ...rest}: Props) => {
 	const [transferring, setTransferring] = useState(false);
 
 	return (
-		<Root>
+		<>
 			<Dialog open={open} onClose={onClose}>
 				<DialogTitle>What is the problem?</DialogTitle>
 				<DialogContent>
@@ -81,9 +70,9 @@ const ConsultationAdvancedActionsDialog = ({open, onClose, ...rest}: Props) => {
 							}}
 						>
 							<ListItemAvatar>
-								<Avatar className={classes.secondary}>
+								<SecondaryAvatar>
 									<RestoreIcon />
-								</Avatar>
+								</SecondaryAvatar>
 							</ListItemAvatar>
 							<ListItemText primary="This consultation should not have been started yet" />
 						</ListItem>
@@ -96,9 +85,9 @@ const ConsultationAdvancedActionsDialog = ({open, onClose, ...rest}: Props) => {
 						}}
 					>
 						<ListItemAvatar>
-							<Avatar className={classes.secondary}>
+							<SecondaryAvatar>
 								<LinkOffIcon />
-							</Avatar>
+							</SecondaryAvatar>
 						</ListItemAvatar>
 						<ListItemText
 							primary={
@@ -116,9 +105,9 @@ const ConsultationAdvancedActionsDialog = ({open, onClose, ...rest}: Props) => {
 						}}
 					>
 						<ListItemAvatar>
-							<Avatar className={classes.secondary}>
+							<SecondaryAvatar>
 								<DeleteIcon />
-							</Avatar>
+							</SecondaryAvatar>
 						</ListItemAvatar>
 						<ListItemText
 							primary={
@@ -163,7 +152,7 @@ const ConsultationAdvancedActionsDialog = ({open, onClose, ...rest}: Props) => {
 					setTransferring(false);
 				}}
 			/>
-		</Root>
+		</>
 	);
 };
 

imports/ui/consultations/ManageConsultationsForPatientButton.tsx
@@ -1,20 +1,22 @@
 import React, {useState} from 'react';
 
-import Button from '@mui/material/Button';
-
-import PropTypes, {InferProps} from 'prop-types';
 import AssistantIcon from '@mui/icons-material/Assistant';
 
 import PropsOf from '../../util/PropsOf';
 import ManageConsultationsForPatientDialog from './ManageConsultationsForPatientDialog';
 
-const ManageConsultationsForPatientButton = ({
+interface ExtraProps<B> {
+	Button: B;
+	patientId: string;
+	children: React.ReactNode;
+}
+
+const ManageConsultationsForPatientButton = <B extends React.ElementType>({
 	Button,
 	patientId,
 	children,
 	...rest
-}: InferProps<typeof ManageConsultationsForPatientButton.propTypes> &
-	PropsOf<typeof Button>) => {
+}: ExtraProps<B> & PropsOf<B>) => {
 	const [open, setOpen] = useState(false);
 	return (
 		<>
@@ -35,16 +37,6 @@ const ManageConsultationsForPatientButton = ({
 			/>
 		</>
 	);
-};
-
-ManageConsultationsForPatientButton.propTypes = {
-	Button: PropTypes.elementType,
-	patientId: PropTypes.string.isRequired,
-	children: PropTypes.node,
-};
-
-ManageConsultationsForPatientButton.defaultProps = {
-	Button,
 };
 
 export default ManageConsultationsForPatientButton;

imports/ui/consultations/ReactiveConsultationCard.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import PropsOf from '../../util/PropsOf';
 import useAttachments from '../attachments/useAttachments';
 
 import StaticConsultationCard from './StaticConsultationCard';
 
-const ReactiveConsultationCard = ({consultation, ...rest}) => {
+type Props = Omit<PropsOf<typeof StaticConsultationCard>, 'attachments'>;
+
+const ReactiveConsultationCard = ({consultation, ...rest}: Props) => {
 	const query = {'meta.attachedToConsultations': consultation._id};
 
 	const options = {
@@ -17,13 +20,13 @@ const ReactiveConsultationCard = ({consultation, ...rest}) => {
 
 	const {results: attachments} = useAttachments(query, options, deps);
 
-	const props = {
-		consultation,
-		attachments,
-		...rest,
-	};
-
-	return <StaticConsultationCard {...props} />;
+	return (
+		<StaticConsultationCard
+			consultation={consultation}
+			attachments={attachments}
+			{...rest}
+		/>
+	);
 };
 
 ReactiveConsultationCard.projection = StaticConsultationCard.projection;

commit 77d9492fa8827b5f15c35829f5e27dcd67a08860

:adhesive_bandage: fix: Restore patient chip in most consultations list.
This was broken in d64cb7400.

imports/ui/consultations/StaticConsultationCard.tsx
@@ -38,20 +38,22 @@ const Accordion = styled(MuiAccordion, {shouldForwardProp})<AdditionalProps>(
 	}),
 );
 
-interface StaticConsultationCardProps
-	extends Omit<
-		PropsOf<typeof StaticConsultationCardActions>,
-		| 'isNoShow'
-		| 'missingPaymentData'
-		| 'didNotOrWillNotHappen'
-		| 'owes'
-		| 'owed'
-	> {
+type StaticConsultationCardProps = {
 	consultation: any;
 	loading?: boolean;
 	found?: boolean;
 	defaultExpanded?: boolean;
-}
+} & Omit<
+	PropsOf<typeof StaticConsultationCardSummary> &
+		PropsOf<typeof StaticConsultationCardActions> &
+		PropsOf<typeof StaticConsultationCardDetails>,
+	| 'deleted'
+	| 'didNotOrWillNotHappen'
+	| 'isNoShow'
+	| 'missingPaymentData'
+	| 'owes'
+	| 'owed'
+>;
 
 const StaticConsultationCard = (props: StaticConsultationCardProps) => {
 	const {

imports/ui/consultations/StaticConsultationCardChips.tsx
@@ -88,7 +88,7 @@ interface StaticConsultationCardChipsProps {
 	isNoShow: boolean;
 	patient?: PatientDocument;
 	consultation: ConsultationDocument;
-	PatientChip: React.ElementType;
+	PatientChip?: React.ElementType;
 	showDate?: boolean;
 	showTime?: boolean;
 	showPrice?: boolean;
@@ -100,36 +100,32 @@ interface StaticConsultationCardChipsProps {
 	attachments: any[];
 }
 
-const StaticConsultationCardChips = (
-	props: StaticConsultationCardChipsProps,
-) => {
-	const {
-		isNoShow,
-		didNotOrWillNotHappen,
-		missingPaymentData,
-		owes,
-		owed,
-		PatientChip,
-		showDate = true,
-		showTime = true,
-		showPrice = false,
-		loadingPatient,
-		patient,
-		consultation: {
-			_id,
-			patientId,
-			datetime,
-			doneDatetime,
-			duration,
-			isDone,
-			isCancelled,
-			currency,
-			payment_method,
-			price,
-		},
-		attachments,
-	} = props;
-
+const StaticConsultationCardChips = ({
+	isNoShow,
+	didNotOrWillNotHappen,
+	missingPaymentData,
+	owes,
+	owed,
+	PatientChip,
+	showDate = true,
+	showTime = true,
+	showPrice = false,
+	loadingPatient,
+	patient,
+	consultation: {
+		_id,
+		patientId,
+		datetime,
+		doneDatetime,
+		duration,
+		isDone,
+		isCancelled,
+		currency,
+		payment_method,
+		price,
+	},
+	attachments,
+}: StaticConsultationCardChipsProps) => {
 	const localizedDateFormat = useDateFormat();
 	const currencyFormat = useCurrencyFormat(currency);
 

imports/ui/consultations/StaticConsultationCardSummary.tsx
@@ -6,26 +6,19 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import AccordionSummary from '@mui/material/AccordionSummary';
 
+import PropsOf from '../../util/PropsOf';
+
 import StaticConsultationCardChips from './StaticConsultationCardChips';
 
-const PREFIX = 'StaticConsultationCardSummary';
-
-const classes = {
-	summary: `${PREFIX}-summary`,
-};
-
 const StyledAccordionSummary = styled(AccordionSummary)({
-	[`&.${classes.summary}`]: {
-		position: 'relative',
-	},
+	position: 'relative',
 });
 
-const StaticConsultationCardSummary = (props) => {
+const StaticConsultationCardSummary = (
+	props: PropsOf<typeof StaticConsultationCardChips>,
+) => {
 	return (
-		<StyledAccordionSummary
-			className={classes.summary}
-			expandIcon={<ExpandMoreIcon />}
-		>
+		<StyledAccordionSummary expandIcon={<ExpandMoreIcon />}>
 			<StaticConsultationCardChips {...props} />
 		</StyledAccordionSummary>
 	);

imports/ui/consultations/StaticConsultationDetails.tsx
@@ -4,13 +4,21 @@ import {styled} from '@mui/material/styles';
 import Loading from '../navigation/Loading';
 import NoContent from '../navigation/NoContent';
 
+import ReactivePatientChip from '../patients/ReactivePatientChip';
+import {ConsultationDocument} from '../../api/collection/consultations';
 import ReactiveConsultationCard from './ReactiveConsultationCard';
 
 const Root = styled('div')(({theme}) => ({
 	padding: theme.spacing(3),
 }));
 
-const StaticConsultationDetails = ({loading, found, consultation}) => {
+interface Props {
+	loading: boolean;
+	found: boolean;
+	consultation: ConsultationDocument;
+}
+
+const StaticConsultationDetails = ({loading, found, consultation}: Props) => {
 	if (loading) {
 		return <Loading />;
 	}
@@ -21,7 +29,11 @@ const StaticConsultationDetails = ({loading, found, consultation}) => {
 
 	return (
 		<Root>
-			<ReactiveConsultationCard defaultExpanded consultation={consultation} />
+			<ReactiveConsultationCard
+				defaultExpanded
+				consultation={consultation}
+				PatientChip={ReactivePatientChip}
+			/>
 		</Root>
 	);
 };

imports/ui/issues/ConsultationsMissingABook.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import ReactiveConsultationCard from '../consultations/ReactiveConsultationCard';
-
 import {useConsultationsMissingABook} from '../../api/issues';
+
+import ReactiveConsultationCard from '../consultations/ReactiveConsultationCard';
+import ReactivePatientChip from '../patients/ReactivePatientChip';
 
 const ConsultationsMissingABook = (props) => {
 	const {loading, results: consultations} = useConsultationsMissingABook();
@@ -21,6 +22,7 @@ const ConsultationsMissingABook = (props) => {
 				<ReactiveConsultationCard
 					key={consultation._id}
 					consultation={consultation}
+					PatientChip={ReactivePatientChip}
 				/>
 			))}
 		</div>

imports/ui/issues/ConsultationsMissingAPrice.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import ReactiveConsultationCard from '../consultations/ReactiveConsultationCard';
-
 import {useConsultationsMissingAPrice} from '../../api/issues';
+
+import ReactiveConsultationCard from '../consultations/ReactiveConsultationCard';
+import ReactivePatientChip from '../patients/ReactivePatientChip';
 
 const ConsultationsMissingAPrice = (props) => {
 	const {loading, results: consultations} = useConsultationsMissingAPrice({
@@ -24,6 +25,7 @@ const ConsultationsMissingAPrice = (props) => {
 				<ReactiveConsultationCard
 					key={consultation._id}
 					consultation={consultation}
+					PatientChip={ReactivePatientChip}
 				/>
 			))}
 		</div>