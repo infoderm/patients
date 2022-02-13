infoderm/patients

commit 7091defaf838175c7e0529086689ab22bb4bc600

:adhesive_bandage: fix(api/consultations/computedFields): Transactional.

imports/api/consultations.ts
@@ -208,13 +208,18 @@ function sanitize({
 	};
 }
 
-export const computedFields = (owner: string, state, changes) => {
-	const isLastConsultation =
-		Consultations.findOne({
-			owner,
-			isDone: true,
-			datetime: {$gt: changes.datetime ?? state?.datetime},
-		}) === undefined;
+export const computedFields = async (
+	db: TransactionDriver,
+	owner: string,
+	state,
+	changes,
+) => {
+	const laterConsultation = await db.findOne(Consultations, {
+		owner,
+		isDone: true,
+		datetime: {$gt: changes.datetime ?? state?.datetime},
+	});
+	const isLastConsultation = laterConsultation === undefined;
 
 	if (!isLastConsultation) return undefined;
 

imports/api/endpoint/consultations/insert.ts
@@ -39,9 +39,11 @@ export default define({
 		const createdAt = new Date();
 		const lastModifiedAt = createdAt;
 
+		const computed = await computedFields(db, this.userId, undefined, fields);
+
 		const document = {
 			...fields,
-			...computedFields(this.userId, undefined, fields),
+			...computed,
 			createdAt,
 			lastModifiedAt,
 			owner,

imports/api/endpoint/consultations/update.ts
@@ -73,9 +73,11 @@ export default define({
 			};
 		}
 
+		const computed = await computedFields(db, owner, existing, fields);
+
 		const $set = {
 			...fields,
-			...computedFields(owner, existing, fields),
+			...computed,
 		};
 
 		const modifier: Mongo.Modifier<ConsultationDocument> = {

commit 3223258557afad321d2114a2d33a60eb47a75402

:adhesive_bandage: fix(api/consultations): Correct computedFields.
Fixes bug introduced in 7091defa.

imports/api/consultations.ts
@@ -219,7 +219,7 @@ export const computedFields = async (
 		isDone: true,
 		datetime: {$gt: changes.datetime ?? state?.datetime},
 	});
-	const isLastConsultation = laterConsultation === undefined;
+	const isLastConsultation = laterConsultation === null;
 
 	if (!isLastConsultation) return undefined;
 