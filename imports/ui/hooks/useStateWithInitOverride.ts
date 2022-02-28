964022d6e5c8e1cf120c50dba3471d6ad88600b9
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-02-28 17:15:51 +0100

    :adhesive_bandage: fix(useStateWithInitOverride): Remove useless branch.

--- imports/ui/hooks/useStateWithInitOverride.ts
+++ imports/ui/hooks/useStateWithInitOverride.ts
@@ -3,7 +3,7 @@ import {DependencyList, useState, useEffect} from 'react';
 const useStateWithInitOverride = (init: any, deps: DependencyList = [init]) => {
 	const [value, setValue] = useState(init);
 	useEffect(() => {
-		if (init !== value) setValue(init);
+		setValue(init);
 	}, deps);
 	return [value, setValue];
 };
