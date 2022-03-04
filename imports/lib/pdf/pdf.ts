86ec1a964fbaf2e274f5bfe98b4a9b3a958c18c8
Aurélien Ooms <aurelien.ooms+patient@gmail.com>
2022-03-04 17:54:50 +0100

    :adhesive_bandage: fix(lib/pdf): Correctly point to necessary resources.
    Also point to minified worker source on client, maximize chances of
    correctly rendering fonts on server, and correctly point to worker
    source on server.

--- imports/lib/pdf/pdf.ts
+++ imports/lib/pdf/pdf.ts
@@ -1,26 +1,34 @@
 import {DocumentInitParameters} from 'pdfjs-dist/types/src/display/api';
 
 export const WORKER_URL = Meteor.isClient
-	? '/pdfjs-dist/build/pdf.worker.js'
-	: '';
-// export const WORKER_URL = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-export const CMAP_URL = Meteor.isClient ? '/pdfjs-dist/cmaps/' : undefined;
+	? '/pdfjs-dist/build/pdf.worker.min.js'
+	: 'pdfjs-dist/legacy/build/pdf.worker.js';
+export const CMAP_URL = Meteor.isClient
+	? '/pdfjs-dist/cmaps/'
+	: './npm/node_modules/pdfjs-dist/cmaps/';
 export const CMAP_PACKED = true;
+export const STANDARD_FONT_DATA_URL = Meteor.isClient
+	? '/pdfjs-dist/standard_fonts/'
+	: './npm/node_modules/pdfjs-dist/standard_fonts/';
 
 export async function fetchPDF({
 	cMapUrl = CMAP_URL,
 	cMapPacked = CMAP_PACKED,
+	standardFontDataUrl = STANDARD_FONT_DATA_URL,
 	...rest
 }: DocumentInitParameters) {
-	const pdfjs = Meteor.isServer
-		? await import('pdfjs-dist/legacy/build/pdf.js')
-		: await import('pdfjs-dist');
+	const pdfjs = Meteor.isClient
+		? await import('pdfjs-dist')
+		: await import('pdfjs-dist/legacy/build/pdf.js');
 
 	if (pdfjs.GlobalWorkerOptions.workerSrc === '') {
 		pdfjs.GlobalWorkerOptions.workerSrc = WORKER_URL;
+		// NOTE For CDN delivery
+		// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 	}
 
-	return pdfjs.getDocument({cMapUrl, cMapPacked, ...rest}).promise;
+	return pdfjs.getDocument({cMapUrl, cMapPacked, standardFontDataUrl, ...rest})
+		.promise;
 }
 
 export async function saveHTMLElementAsPDF(
