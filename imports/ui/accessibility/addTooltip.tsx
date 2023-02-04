a4130d78e3c7912920a038c9180c756c816407cf
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2023-02-04 18:10:14 +0100

    :recycle: refactor(addTooltip): Improve argument names.

--- imports/ui/accessibility/addTooltip.tsx
+++ imports/ui/accessibility/addTooltip.tsx
@@ -3,11 +3,11 @@ import type PropsOf from '../../util/PropsOf';
 import Tooltip from './Tooltip';
 
 type Props<C> = PropsOf<C> & {tooltip?: string};
-type Transform<C> = (props: PropsOf<C>, x: string) => string;
+type Transform<C> = (props: PropsOf<C>, tooltip: string) => string;
 
 const addTooltip = (
 	Component: React.ElementType,
-	transform: Transform<typeof Component> = (_props, x) => x,
+	transform: Transform<typeof Component> = (_props, tooltip) => tooltip,
 ) =>
 	React.forwardRef(({tooltip, ...rest}: Props<typeof Component>, ref) => {
 		const title = transform(rest, tooltip);
