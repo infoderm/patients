09140bdfcf4e9345b1141033b567601e143c6fb9
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2023-02-13 17:50:16 +0100

    :recycle: refactor(useMergeInfo): Exploit useSubscription and useCursor.
    Fixes #552 and fixes #591.

--- imports/ui/merge/useMergeInfo.ts
+++ imports/ui/merge/useMergeInfo.ts
@@ -1,33 +1,93 @@
-import {list} from '@iterable-iterator/list';
-import {map} from '@iterable-iterator/map';
-import {_chain as chain} from '@iterable-iterator/chain';
+import {useMemo} from 'react';
 
-import {Patients} from '../../api/collection/patients';
+import {map} from '@iterable-iterator/map';
+
+import {Patients, type PatientDocument} from '../../api/collection/patients';
 import {Consultations} from '../../api/collection/consultations';
 import {Attachments} from '../../api/collection/attachments';
 import {Documents} from '../../api/collection/documents';
 
 import {patients} from '../../api/patients';
-import useReactive from '../../api/publication/useReactive';
-import subscribe from '../../api/publication/subscribe';
+
+import useCursor from '../../api/publication/useCursor';
+import useSubscription from '../../api/publication/useSubscription';
+
 import patientsPub from '../../api/publication/patients/patients';
-import consultationsAndAppointments from '../../api/publication/consultationsAndAppointments/find';
+import consultationsPub from '../../api/publication/consultationsAndAppointments/find';
 import attachmentsPub from '../../api/publication/attachments/attachments';
 import documentsPub from '../../api/publication/documents/find';
 
-const useMergeInfo = (toMerge) =>
-	useReactive(() => {
-		subscribe(patientsPub, {_id: {$in: toMerge}});
-		subscribe(consultationsAndAppointments, {patientId: {$in: toMerge}});
-		subscribe(documentsPub, {patientId: {$in: toMerge}});
-		subscribe(attachmentsPub, {'meta.attachedToPatients': {$in: toMerge}});
+const index = <T>(keys: (item: T) => Iterable<string>, items: Iterable<T>) => {
+	const table = {};
 
+	for (const item of items) {
+		for (const key of keys(item)) {
+			if (table[key] === undefined) {
+				table[key] = [];
+			}
+
+			table[key].push(item);
+		}
+	}
+
+	return table;
+};
+
+const useMergeInfo = (toMerge: string[]) => {
+	const patientsLoading = useSubscription(patientsPub, {_id: {$in: toMerge}});
+	const consultationsLoading = useSubscription(consultationsPub, {
+		patientId: {$in: toMerge},
+	});
+	const documentsLoading = useSubscription(documentsPub, {
+		patientId: {$in: toMerge},
+	});
+	const attachmentsLoading = useSubscription(attachmentsPub, {
+		'meta.attachedToPatients': {$in: toMerge},
+	});
+
+	const loading =
+		patientsLoading() ||
+		consultationsLoading() ||
+		documentsLoading() ||
+		attachmentsLoading();
+
+	const fetchedPatients = useCursor(
+		() => Patients.find({_id: {$in: toMerge}}),
+		toMerge,
+	);
+
+	const newConsultations = useCursor(
+		() =>
+			Consultations.find({patientId: {$in: toMerge}}, {sort: {datetime: -1}}),
+		toMerge,
+	);
+
+	const newDocuments = useCursor(
+		() => Documents.find({patientId: {$in: toMerge}}, {sort: {createdAt: -1}}),
+		toMerge,
+	);
+
+	const newAttachments = useCursor(
+		() =>
+			Attachments.find(
+				{'meta.attachedToPatients': {$in: toMerge}},
+				{sort: {'meta.createdAt': -1}},
+			),
+		toMerge,
+	);
+
+	const results = useMemo(() => {
 		const oldPatients = [];
-		const consultations = {};
-		const attachments = {};
-		const documents = {};
+
+		const fetchedPatientIndex = new Map(
+			map(
+				(patient: PatientDocument) => [patient._id, patient],
+				fetchedPatients,
+			),
+		);
+
 		for (const patientId of toMerge) {
-			const patient = Patients.findOne(patientId);
+			const patient = fetchedPatientIndex.get(patientId);
 			if (patient === undefined) {
 				const error = {
 					message: `Cannot merge because patient #${patientId} does not exist in the database.`,
@@ -35,36 +95,43 @@ const useMergeInfo = (toMerge) =>
 				return {error};
 			}
 
-			const consultationsForPatient = Consultations.find(
-				{patientId},
-				{sort: {datetime: -1}},
-			).fetch();
-			const attachmentsForPatient = Attachments.find(
-				{'meta.attachedToPatients': patientId},
-				{sort: {'meta.createdAt': -1}},
-			).fetch();
-			const documentsForPatient = Documents.find(
-				{patientId},
-				{sort: {createdAt: -1}},
-			).fetch();
 			oldPatients.push(patient);
-			consultations[patientId] = consultationsForPatient;
-			attachments[patientId] = attachmentsForPatient;
-			documents[patientId] = documentsForPatient;
 		}
+
+		const consultations = index(
+			(consultation) => [consultation.patientId],
+			newConsultations,
+		);
+		const documents = index((document) => [document.patientId], newDocuments);
+		const attachments = index(
+			(attachment) => attachment.meta.attachedToPatients,
+			newAttachments,
+		);
+
+		const newPatient = patients.merge(oldPatients);
 
 		return {
 			oldPatients,
 			consultations,
-			attachments,
 			documents,
-			newPatient: patients.merge(oldPatients),
-			newConsultations: list(
-				chain(map((x) => consultations[x] || [], toMerge)),
-			),
-			newAttachments: list(chain(map((x) => attachments[x] || [], toMerge))),
-			newDocuments: list(chain(map((x) => documents[x] || [], toMerge))),
+			attachments,
+			newPatient,
 		};
-	});
+	}, [
+		toMerge,
+		fetchedPatients,
+		newConsultations,
+		newDocuments,
+		newAttachments,
+	]);
+
+	return {
+		loading,
+		...results,
+		newConsultations,
+		newDocuments,
+		newAttachments,
+	};
+};
 
 export default useMergeInfo;
