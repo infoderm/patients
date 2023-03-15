infoderm/patients

commit 340485b15aa467680761875641f53c06ae6872d8

:recycle: refactor: Migrate from @mui/styles to tss-react.

imports/ui/input/Suggestions.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import classNames from 'classnames';
 
 import {styled} from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 
 import {type UseComboboxGetItemPropsOptions} from 'downshift';
+import type PropsOf from '../../lib/types/PropsOf';
 import Suggestion from './Suggestion';
 import CreateItemSuggestion from './CreateItemSuggestion';
 
@@ -24,35 +24,30 @@ type SuggestionsProps<Item> = {
 	Item?: React.ElementType;
 };
 
-const PREFIX = 'Suggestions';
-
-const classes = {
-	root: `${PREFIX}-root`,
-	hidden: `${PREFIX}-hidden`,
-	list: `${PREFIX}-list`,
-};
-
-const StyledPaper = styled(Paper)(({theme}) => ({
-	[`&.${classes.root}`]: {
-		position: 'absolute',
-		zIndex: 1,
-		marginTop: theme.spacing(1),
-		left: 0,
-		right: 0,
-	},
-	[`&.${classes.hidden}`]: {
-		display: 'none',
-	},
-	[`& .${classes.list}`]: {
-		padding: 0,
-		margin: 0,
-	},
+const Root = styled(
+	React.forwardRef(
+		(
+			{open, ...rest}: PropsOf<typeof Paper> & {open: boolean},
+			ref: React.Ref<HTMLDivElement>,
+		) => <Paper ref={ref} square {...rest} />,
+	),
+)(({theme, open}) => ({
+	position: 'absolute',
+	zIndex: 1,
+	marginTop: theme.spacing(1),
+	left: 0,
+	right: 0,
+	display: open ? 'block' : 'none',
 }));
+
+const UnorderedList = styled('ul')({
+	padding: 0,
+	margin: 0,
+});
 
 const Suggestions = React.forwardRef(
 	<ItemType,>(
 		{
-			className,
 			hide = false,
 			loading = false,
 			suggestions,
@@ -70,16 +65,10 @@ const Suggestions = React.forwardRef(
 		ref: React.Ref<HTMLDivElement>,
 	) => {
 		const createItemSuggestion = createNewItem && !error && inputValue;
+		const open = hide || (!suggestions?.length && !createItemSuggestion);
 		return (
-			<StyledPaper
-				ref={ref}
-				square
-				className={classNames(classes.root, {
-					[classes.hidden]:
-						hide || (!suggestions?.length && !createItemSuggestion),
-				})}
-			>
-				<ul className={classNames(className, classes.list)} {...rest}>
+			<Root ref={ref} open={open}>
+				<UnorderedList {...rest}>
 					{!hide && (
 						<>
 							{createItemSuggestion && (
@@ -104,8 +93,8 @@ const Suggestions = React.forwardRef(
 							))}
 						</>
 					)}
-				</ul>
-			</StyledPaper>
+				</UnorderedList>
+			</Root>
 		);
 	},
 );

imports/ui/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 import {BrowserRouter} from 'react-router-dom';
 
-import {ThemeProvider, StyledEngineProvider} from '@mui/material/styles';
+import {CacheProvider} from '@emotion/react';
+import createCache from '@emotion/cache';
+import {ThemeProvider} from '@mui/material/styles';
 
 import {SnackbarProvider} from 'notistack';
 
@@ -21,6 +23,11 @@ import useUserTheme from './useUserTheme';
 import StatusNotifications from './StatusNotifications';
 import PlannerProvider from './planner/PlannerProvider';
 
+export const muiCache = createCache({
+	key: 'mui',
+	prepend: true,
+});
+
 const App = () => {
 	const locale = useLocale();
 	const theme = useUserTheme();
@@ -28,7 +35,7 @@ const App = () => {
 	return (
 		<BrowserRouter>
 			<LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={locale}>
-				<StyledEngineProvider injectFirst>
+				<CacheProvider value={muiCache}>
 					<ThemeProvider theme={theme}>
 						<ModalProvider>
 							<CssBaseline />
@@ -43,7 +50,7 @@ const App = () => {
 							</PlannerProvider>
 						</ModalProvider>
 					</ThemeProvider>
-				</StyledEngineProvider>
+				</CacheProvider>
 			</LocalizationProvider>
 		</BrowserRouter>
 	);

imports/ui/attachments/AttachmentsGallery.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import makeStyles from '@mui/styles/makeStyles';
 
 import dateFormat from 'date-fns/format';
 
@@ -12,11 +11,12 @@ import {groupby} from '@iterable-iterator/group';
 
 import Loading from '../navigation/Loading';
 
+import makeStyles from '../styles/makeStyles';
 import useAttachments from './useAttachments';
 import type AttachmentInfo from './AttachmentInfo';
 import AttachmentsGrid from './AttachmentsGrid';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles()((theme) => ({
 	group: {
 		padding: theme.spacing(2),
 		marginBottom: theme.spacing(2),
@@ -34,7 +34,7 @@ const StaticAttachmentsGallery = ({
 	attachmentsInfo,
 	attachments,
 }: StaticAttachmentsGalleryProps) => {
-	const classes = useStyles();
+	const {classes} = useStyles();
 
 	if (loading) {
 		return <Loading />;

imports/ui/calendar/CalendarData.tsx
@@ -1,7 +1,5 @@
 import React, {useMemo} from 'react';
-import classNames from 'classnames';
 
-import {makeStyles, createStyles, type CSSProperties} from '@mui/styles';
 import Paper from '@mui/material/Paper';
 
 import differenceInDays from 'date-fns/differenceInDays';
@@ -15,17 +13,19 @@ import {list} from '@iterable-iterator/list';
 import {take} from '@iterable-iterator/slice';
 import {range} from '@iterable-iterator/range';
 import {enumerate} from '@iterable-iterator/zip';
+import makeStyles from '../styles/makeStyles';
 import {useDateFormat} from '../../i18n/datetime';
 
 import {ALL_WEEK_DAYS, generateDays} from '../../lib/datetime';
-import EventFragment from './EventFragment';
+import type CSSObject from '../styles/CSSObject';
 import type Event from './Event';
+import EventFragment from './EventFragment';
 
-const ColumnHeader = ({classes, day, col}) => {
+const ColumnHeader = ({classes, cx, day, col}) => {
 	const getDayName = useDateFormat('cccc');
 	return (
 		<div
-			className={classNames(classes.columnHeader, {
+			className={cx(classes.columnHeader, {
 				[classes[`col${col}`]]: true,
 			})}
 		>
@@ -34,7 +34,7 @@ const ColumnHeader = ({classes, day, col}) => {
 	);
 };
 
-const DayBox = ({classes, day, row, col, onSlotClick}) => {
+const DayBox = ({classes, cx, day, row, col, onSlotClick}) => {
 	const ariaLabel = `Schedule an appointment on ${dateFormat(day, 'PPPP')}`;
 	const role = onSlotClick ? 'button' : undefined;
 	const onClick = useMemo(() => {
@@ -46,7 +46,7 @@ const DayBox = ({classes, day, row, col, onSlotClick}) => {
 	}, [onSlotClick, day]);
 	return (
 		<div
-			className={classNames(classes.dayBox, {
+			className={cx(classes.dayBox, {
 				[classes[`col${col}`]]: true,
 				[classes[`row${row}`]]: true,
 			})}
@@ -232,15 +232,7 @@ const More = ({className, count}) => (
 	<div className={className}>{count} more</div>
 );
 
-type CalendarDataGridStyles = {
-	[key: string]: CSSProperties;
-	header: CSSProperties;
-	corner: CSSProperties;
-	grid: CSSProperties;
-	weekNumber: CSSProperties;
-	dayHeader: CSSProperties;
-	more: CSSProperties;
-};
+type CalendarDataGridStyles = Record<string, CSSObject>;
 
 type CalendarDataGridClasses = {
 	[key in keyof CalendarDataGridStyles]: string;
@@ -249,7 +241,8 @@ type CalendarDataGridClasses = {
 type CalendarDataGridProps = {
 	DayHeader?: React.ElementType;
 	WeekNumber?: React.ElementType;
-	useStyles: () => CalendarDataGridClasses;
+	classes: CalendarDataGridClasses;
+	cx: any;
 	rowSize: number;
 	days: DayProps[];
 	events: EventProps[];
@@ -260,7 +253,8 @@ type CalendarDataGridProps = {
 };
 
 const CalendarDataGrid = ({
-	useStyles,
+	classes,
+	cx,
 	rowSize,
 	days,
 	events,
@@ -270,14 +264,12 @@ const CalendarDataGrid = ({
 	weekOptions,
 	onSlotClick,
 }: CalendarDataGridProps) => {
-	const classes = useStyles();
-
 	return (
 		<Paper>
 			<div className={classes.header}>
 				{WeekNumber && <div className={classes.corner} />}
 				{list(take(days, rowSize)).map((props, key) => (
-					<ColumnHeader key={key} classes={classes} {...props} />
+					<ColumnHeader key={key} classes={classes} cx={cx} {...props} />
 				))}
 			</div>
 			<div className={classes.grid}>
@@ -287,7 +279,7 @@ const CalendarDataGrid = ({
 						.map((props, key) => (
 							<WeekNumber
 								key={key}
-								className={classNames(classes.weekNumber, {
+								className={cx(classes.weekNumber, {
 									[classes[`row${props.row}`]]: true,
 								})}
 								weekOptions={weekOptions}
@@ -298,6 +290,7 @@ const CalendarDataGrid = ({
 					<DayBox
 						key={key}
 						classes={classes}
+						cx={cx}
 						{...props}
 						onSlotClick={onSlotClick}
 					/>
@@ -305,7 +298,7 @@ const CalendarDataGrid = ({
 				{days.map((props, key) => (
 					<DayHeader
 						key={key}
-						className={classNames(classes.dayHeader, {
+						className={cx(classes.dayHeader, {
 							[classes[`col${props.col}`]]: true,
 							[classes[`row${props.row}`]]: true,
 						})}
@@ -317,7 +310,7 @@ const CalendarDataGrid = ({
 					.map((props, key) => (
 						<EventFragment
 							key={key}
-							className={classNames(classes.slot, {
+							className={cx(classes.slot, {
 								[classes[`slot${props.slots}`]]: true,
 								[classes[`day${props.day}slot${props.slot}`]]: true,
 							})}
@@ -330,7 +323,7 @@ const CalendarDataGrid = ({
 					.map((props, key) => (
 						<More
 							key={key}
-							className={classNames(classes.more, {
+							className={cx(classes.more, {
 								[classes[`day${props.day}more`]]: true,
 							})}
 							{...props}
@@ -369,151 +362,155 @@ type MakeGridStylesOptions = {
 	daysProps: DayProps[];
 };
 
-const makeGridStyles = ({
-	displayWeekNumbers,
-	nrows,
-	headerHeight,
-	lineHeight,
-	rowSize,
-	maxLines,
-	eventProps,
-	daysProps,
-}: MakeGridStylesOptions) => {
-	const gridTemplateColumns = [
-		displayWeekNumbers && '25px',
-		`repeat(${rowSize}, 1fr)`,
-	]
-		.filter(Boolean)
-		.join(' ');
+const useGridStyles = makeStyles<MakeGridStylesOptions>()(
+	(
+		_theme,
+		{
+			displayWeekNumbers,
+			nrows,
+			headerHeight,
+			lineHeight,
+			rowSize,
+			maxLines,
+			eventProps,
+			daysProps,
+		}: MakeGridStylesOptions,
+	) => {
+		const gridTemplateColumns = [
+			displayWeekNumbers && '25px',
+			`repeat(${rowSize}, 1fr)`,
+		]
+			.filter(Boolean)
+			.join(' ');
 
-	const gridStyles: CalendarDataGridStyles = {
-		header: {
-			display: 'grid',
-			gridTemplateColumns,
-			gridTemplateRows: `repeat(${headerHeight}, ${lineHeight})`,
-			lineHeight: `calc(2*${lineHeight})`,
-			backgroundColor: '#aaa',
-			gridGap: '1px',
-		},
-		columnHeader: {
-			backgroundColor: '#fff',
-			fontWeight: 'bold',
-			textAlign: 'center',
-			color: '#aaa',
-			padding: '5px 5px',
-			gridColumnEnd: 'span 1',
-			gridRowEnd: `span ${headerHeight}`,
-		},
-		corner: {
-			backgroundColor: '#fff',
-			gridColumnEnd: 'span 1',
-			gridRowEnd: `span ${headerHeight}`,
-		},
-		grid: {
-			display: 'grid',
-			gridTemplateColumns,
-			gridTemplateRows: `repeat(${nrows * maxLines}, ${lineHeight})`,
-			backgroundColor: '#aaa',
-			gridGap: '1px',
-		},
-		dayHeader: {
-			margin: '3px auto',
-			textAlign: 'center',
-			gridColumnEnd: 'span 1',
-			gridRowEnd: 'span 1',
-		},
-		weekNumber: {
-			backgroundColor: '#fff',
-			gridColumnEnd: 'span 1',
-			gridRowEnd: `span ${maxLines}`,
-			paddingTop: '5px',
-			textAlign: 'center',
-		},
-		dayBox: {
-			backgroundColor: '#fff',
-			gridColumnEnd: 'span 1',
-			gridRowEnd: `span ${maxLines}`,
-			'&:hover': {
-				backgroundColor: '#f5f5f5',
+		const gridStyles: CalendarDataGridStyles = {
+			header: {
+				display: 'grid',
+				gridTemplateColumns,
+				gridTemplateRows: `repeat(${headerHeight}, ${lineHeight})`,
+				lineHeight: `calc(2*${lineHeight})`,
+				backgroundColor: '#aaa',
+				gridGap: '1px',
 			},
-		},
-		slot: {
-			gridColumnEnd: 'span 1',
-			margin: '0 10px 3px 10px',
-			'&:hover': {
-				opacity: 0.8,
+			columnHeader: {
+				backgroundColor: '#fff',
+				fontWeight: 'bold',
+				textAlign: 'center',
+				color: '#aaa',
+				padding: '5px 5px',
+				gridColumnEnd: 'span 1',
+				gridRowEnd: `span ${headerHeight}`,
 			},
-		},
-		slot1: {
-			gridRowEnd: 'span 1',
-			textOverflow: 'ellipsis',
-			whiteSpace: 'nowrap',
-			overflow: 'hidden',
-		},
-		event: {
-			display: 'inline-block',
-			width: '100%',
-			height: '100%',
-			overflow: 'hidden',
-			textOverflow: 'ellipsis',
-			padding: '1px 6px',
-			borderRadius: '3px',
-		},
-		more: {
-			margin: '0 auto 0 15px',
-			gridColumnEnd: 'span 1',
-			gridRowEnd: 'span 1',
-			textOverflow: 'ellipsis',
-			whiteSpace: 'nowrap',
-			overflow: 'hidden',
-			fontWeight: 'bold',
-		},
-	};
-
-	const colOffset = displayWeekNumbers ? 1 : 0;
-
-	for (const i of range(1, rowSize + 1)) {
-		gridStyles[`col${i}`] = {
-			gridColumnStart: colOffset + (i as number),
+			corner: {
+				backgroundColor: '#fff',
+				gridColumnEnd: 'span 1',
+				gridRowEnd: `span ${headerHeight}`,
+			},
+			grid: {
+				display: 'grid',
+				gridTemplateColumns,
+				gridTemplateRows: `repeat(${nrows * maxLines}, ${lineHeight})`,
+				backgroundColor: '#aaa',
+				gridGap: '1px',
+			},
+			dayHeader: {
+				margin: '3px auto',
+				textAlign: 'center',
+				gridColumnEnd: 'span 1',
+				gridRowEnd: 'span 1',
+			},
+			weekNumber: {
+				backgroundColor: '#fff',
+				gridColumnEnd: 'span 1',
+				gridRowEnd: `span ${maxLines}`,
+				paddingTop: '5px',
+				textAlign: 'center',
+			},
+			dayBox: {
+				backgroundColor: '#fff',
+				gridColumnEnd: 'span 1',
+				gridRowEnd: `span ${maxLines}`,
+				'&:hover': {
+					backgroundColor: '#f5f5f5',
+				},
+			},
+			slot: {
+				gridColumnEnd: 'span 1',
+				margin: '0 10px 3px 10px',
+				'&:hover': {
+					opacity: 0.8,
+				},
+			},
+			slot1: {
+				gridRowEnd: 'span 1',
+				textOverflow: 'ellipsis',
+				whiteSpace: 'nowrap',
+				overflow: 'hidden',
+			},
+			event: {
+				display: 'inline-block',
+				width: '100%',
+				height: '100%',
+				overflow: 'hidden',
+				textOverflow: 'ellipsis',
+				padding: '1px 6px',
+				borderRadius: '3px',
+			},
+			more: {
+				margin: '0 auto 0 15px',
+				gridColumnEnd: 'span 1',
+				gridRowEnd: 'span 1',
+				textOverflow: 'ellipsis',
+				whiteSpace: 'nowrap',
+				overflow: 'hidden',
+				fontWeight: 'bold',
+			},
 		};
-	}
 
-	for (const i of range(1, nrows + 1)) {
-		gridStyles[`row${i}`] = {
-			gridRowStart: (i - 1) * maxLines + 1,
-		};
-	}
+		const colOffset = displayWeekNumbers ? 1 : 0;
 
-	for (const {day, row, col} of daysProps) {
-		const dayId = dayKey(day);
-		for (const j of range(1, maxLines)) {
-			gridStyles[`day${dayId}slot${j}`] = {
-				gridColumnStart: colOffset + col,
-				gridRowStart: (row - 1) * maxLines + 1 + (j as number),
+		for (const i of range(1, rowSize + 1)) {
+			gridStyles[`col${i}`] = {
+				gridColumnStart: colOffset + (i as number),
 			};
 		}
 
-		gridStyles[`day${dayId}more`] = {
-			gridColumnStart: colOffset + col,
-			gridRowStart: row * maxLines,
-		};
-	}
+		for (const i of range(1, nrows + 1)) {
+			gridStyles[`row${i}`] = {
+				gridRowStart: (i - 1) * maxLines + 1,
+			};
+		}
 
-	const slotTypes = new Set(
-		eventProps.map(({slots}) => slots).filter((x) => x !== 1),
-	);
-	for (const slots of slotTypes) {
-		gridStyles[`slot${slots}`] = {
-			gridRowEnd: `span ${slots}`,
-			textOverflow: 'ellipsis',
-			whiteSpace: 'pre-wrap',
-			overflow: 'hidden',
-		};
-	}
+		for (const {day, row, col} of daysProps) {
+			const dayId = dayKey(day);
+			for (const j of range(1, maxLines)) {
+				gridStyles[`day${dayId}slot${j}`] = {
+					gridColumnStart: colOffset + col,
+					gridRowStart: (row - 1) * maxLines + 1 + (j as number),
+				};
+			}
 
-	const styles = createStyles(() => gridStyles);
-	return makeStyles(styles) as () => CalendarDataGridClasses;
-};
+			gridStyles[`day${dayId}more`] = {
+				gridColumnStart: colOffset + col,
+				gridRowStart: row * maxLines,
+			};
+		}
+
+		const slotTypes = new Set(
+			eventProps.map(({slots}) => slots).filter((x) => x !== 1),
+		);
+		for (const slots of slotTypes) {
+			gridStyles[`slot${slots}`] = {
+				gridRowEnd: `span ${slots}`,
+				textOverflow: 'ellipsis',
+				whiteSpace: 'pre-wrap',
+				overflow: 'hidden',
+			};
+		}
+
+		return gridStyles;
+	},
+);
 
 const CalendarData = ({
 	events,
@@ -553,7 +550,7 @@ const CalendarData = ({
 	);
 	const moreProps = Array.from(generateMoreProps(occupancy, begin, end));
 
-	const useStyles = makeGridStyles({
+	const {classes, cx} = useGridStyles({
 		displayWeekNumbers: Boolean(WeekNumber),
 		nrows: days / 7,
 		lineHeight,
@@ -566,7 +563,8 @@ const CalendarData = ({
 
 	return (
 		<CalendarDataGrid
-			useStyles={useStyles}
+			classes={classes}
+			cx={cx}
 			rowSize={rowSize}
 			days={daysProps}
 			events={eventProps}

imports/ui/cards/AnimatedCardMedia.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import classnames from 'classnames';
 import {styled} from '@mui/material/styles';
 
 import {useTransition, animated} from 'react-spring';
@@ -10,15 +9,10 @@ import Skeleton from '@mui/material/Skeleton';
 const PREFIX = 'AnimatedCardMedia';
 
 const classes = {
-	container: `${PREFIX}-container`,
 	item: `${PREFIX}-item`,
 };
 
 const Root = styled('div')({
-	[`&.${classes.container}`]: {
-		position: 'relative',
-	},
-
 	[`& .${classes.item}`]: {
 		position: 'absolute',
 		top: 0,
@@ -49,7 +43,7 @@ const AnimatedCardMedia = ({
 	});
 
 	return (
-		<Root className={classnames(classes.container, className)}>
+		<Root className={className} sx={{position: 'relative'}}>
 			{transition((style, item) => {
 				const children =
 					(!loading && item !== '') || placeholder === undefined ? (

imports/ui/chips/ColorChip.tsx
@@ -1,57 +1,34 @@
 import React from 'react';
 
-import makeStyles from '@mui/styles/makeStyles';
-import {darken} from '@mui/material/styles';
-import classNames from 'classnames';
+import {styled, darken} from '@mui/material/styles';
 
 import Chip, {type ChipProps} from '@mui/material/Chip';
 
 import {colord} from 'colord';
 
-const styles = {
-	chip({color}) {
-		const backgroundColor = color;
-		const foregroundColor = color
-			? colord(color).isLight()
-				? '#111'
-				: '#ddd'
-			: undefined;
-		return {
-			backgroundColor,
-			color: foregroundColor,
-			'&:hover, &:focus': {
-				backgroundColor: backgroundColor && darken(backgroundColor, 0.1),
-			},
-		};
-	},
+type Props<C extends React.ElementType> = Omit<ChipProps<C>, 'color'> & {
+	color: string;
 };
 
-const useStyles = makeStyles(styles);
-
-type ColorChipExtraProps = {
-	color?: string;
-};
-
-const ColorChip = React.forwardRef(
-	(
-		{
-			color,
-			className,
-			component,
-			...rest
-		}: ColorChipExtraProps & Omit<ChipProps<typeof component>, 'color'>,
-		ref,
-	) => {
-		const classes = useStyles({color});
-		return (
-			<Chip
-				ref={ref}
-				component={component}
-				className={classNames(classes.chip, className)}
-				{...rest}
-			/>
-		);
-	},
-);
-
+const ColorChip = styled(
+	React.forwardRef(
+		({color, component, ...rest}: Props<typeof component>, ref) => (
+			<Chip ref={ref} component={component} {...rest} />
+		),
+	),
+)(({color}) => {
+	const backgroundColor = color;
+	const foregroundColor = color
+		? colord(color).isLight()
+			? '#111'
+			: '#ddd'
+		: undefined;
+	return {
+		backgroundColor,
+		color: foregroundColor,
+		'&:hover, &:focus': {
+			backgroundColor: backgroundColor && darken(backgroundColor, 0.1),
+		},
+	};
+});
 export default ColorChip;

imports/ui/consultations/ConsultationPaymentDialog.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import dateFormat from 'date-fns/format';
 
-import makeStyles from '@mui/styles/makeStyles';
-
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -14,6 +12,7 @@ import LinearProgress from '@mui/material/LinearProgress';
 import CircularProgress from '@mui/material/CircularProgress';
 
 import CloseIcon from '@mui/icons-material/Close';
+import makeStyles from '../styles/makeStyles';
 
 import {type ConsultationDocument} from '../../api/collection/consultations';
 
@@ -31,7 +30,7 @@ const SIZE_CODE = 256;
 const SIZE_PROGRESS = 128;
 const THICKNESS_PROGRESS = 3.6;
 
-const useStyles = makeStyles({
+const useStyles = makeStyles()({
 	code: {},
 	codeContainer: {
 		position: 'relative',
@@ -69,7 +68,7 @@ type Props = {
 };
 
 const ConsultationPaymentDialog = ({open, onClose, consultation}: Props) => {
-	const classes = useStyles();
+	const {classes} = useStyles();
 
 	const {loading: loadingAccountHolder, value: accountHolder} =
 		useSetting('account-holder');

imports/ui/consultations/ManageConsultationsForPatientDialog.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import {styled} from '@mui/material/styles';
 import {useNavigate} from 'react-router-dom';
-import classNames from 'classnames';
 
 import dateFormat from 'date-fns/format';
 import isToday from 'date-fns/isToday';
 
 import LinearProgress from '@mui/material/LinearProgress';
 
-import Avatar from '@mui/material/Avatar';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
 import ListItemText from '@mui/material/ListItemText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
@@ -27,30 +26,18 @@ import withLazyOpening from '../modal/withLazyOpening';
 
 import useUpcomingAppointmentsForPatient from '../appointments/useUpcomingAppointmentsForPatient';
 import beginConsultation from '../../api/endpoint/appointments/beginConsultation';
+import type PropsOf from '../../lib/types/PropsOf';
+import {type AppointmentDocument} from '../../api/collection/appointments';
+import {type ConsultationDocument} from '../../api/collection/consultations';
 import useConsultationsForPatient from './useConsultationsForPatient';
 
 const PREFIX = 'ManageConsultationsForPatientDialog';
 
 const classes = {
-	avatarBegin: `${PREFIX}-avatarBegin`,
-	avatarEdit: `${PREFIX}-avatarEdit`,
-	avatarSchedule: `${PREFIX}-avatarSchedule`,
+	iconBegin: `${PREFIX}-iconBegin`,
+	iconEdit: `${PREFIX}-iconEdit`,
+	iconSchedule: `${PREFIX}-iconSchedule`,
 };
-
-const StyledDialog = styled(Dialog)({
-	[`& .${classes.avatarBegin}`]: {
-		backgroundColor: green[100],
-		color: green[600],
-	},
-	[`& .${classes.avatarEdit}`]: {
-		backgroundColor: blue[100],
-		color: blue[600],
-	},
-	[`& .${classes.avatarSchedule}`]: {
-		backgroundColor: orange[100],
-		color: orange[600],
-	},
-});
 
 const formatDatetime = (datetime: Date) => {
 	if (isToday(datetime)) {
@@ -65,6 +52,76 @@ type Props = {
 	open: boolean;
 	patientId: string;
 };
+
+type BeginConsultationItemProps = PropsOf<typeof ListItemButton> & {
+	appointment: AppointmentDocument;
+};
+
+const BeginConsultationItem = styled(
+	({appointment: {datetime}, ...rest}: BeginConsultationItemProps) => (
+		<ListItemButton {...rest}>
+			<ListItemAvatar>
+				<Avatar className={isToday(datetime) ? classes.iconBegin : undefined}>
+					<AlarmIcon />
+				</Avatar>
+			</ListItemAvatar>
+			<ListItemText
+				primary={`Commencer le rendez-vous ${formatDatetime(datetime)}`}
+			/>
+		</ListItemButton>
+	),
+)({
+	[`& .${classes.iconBegin}`]: {
+		backgroundColor: green[100],
+		color: green[600],
+	},
+});
+
+type EditExistingConsultationItemProps = PropsOf<typeof ListItemButton> & {
+	consultation: ConsultationDocument;
+};
+
+const EditExistingConsultationItem = styled(
+	({consultation: {datetime}, ...rest}: EditExistingConsultationItemProps) => (
+		<ListItemButton {...rest}>
+			<ListItemAvatar>
+				<Avatar className={isToday(datetime) ? classes.iconEdit : undefined}>
+					<BookmarkIcon />
+				</Avatar>
+			</ListItemAvatar>
+			<ListItemText
+				primary={`Éditer la consultation ${formatDatetime(datetime)}`}
+			/>
+		</ListItemButton>
+	),
+)({
+	[`& .${classes.iconEdit}`]: {
+		backgroundColor: blue[100],
+		color: blue[600],
+	},
+});
+
+type ScheduleNewAppointmentItemProps = PropsOf<typeof ListItemButton> & {
+	emphasize: boolean;
+};
+
+const ScheduleNewAppointmentItem = styled(
+	({emphasize, ...rest}: ScheduleNewAppointmentItemProps) => (
+		<ListItemButton {...rest}>
+			<ListItemAvatar>
+				<Avatar className={emphasize ? classes.iconSchedule : undefined}>
+					<TodayIcon />
+				</Avatar>
+			</ListItemAvatar>
+			<ListItemText primary="Programmer un nouveau rendez-vous" />
+		</ListItemButton>
+	),
+)({
+	[`& .${classes.iconSchedule}`]: {
+		backgroundColor: orange[100],
+		color: orange[600],
+	},
+});
 
 const ManageConsultationsForPatientDialog = ({
 	open,
@@ -107,69 +164,39 @@ const ManageConsultationsForPatientDialog = ({
 	const loading = loadingAppointments || loadingConsultations;
 
 	return (
-		<StyledDialog open={open} onClose={onClose}>
+		<Dialog open={open} onClose={onClose}>
 			{loading && <LinearProgress />}
 			<DialogTitle>What do you want to do?</DialogTitle>
 			<List>
-				{appointments.map(({_id, datetime}, i) => (
-					<ListItem
-						key={_id}
-						button
+				{appointments.map((appointment, i) => (
+					<BeginConsultationItem
+						key={appointment._id}
+						appointment={appointment}
 						autoFocus={i === 0}
-						onClick={beginThisConsultation(_id)}
-					>
-						<ListItemAvatar>
-							<Avatar
-								className={classNames({
-									[classes.avatarBegin]: isToday(datetime),
-								})}
-							>
-								<AlarmIcon />
-							</Avatar>
-						</ListItemAvatar>
-						<ListItemText
-							primary={`Commencer le rendez-vous ${formatDatetime(datetime)}`}
-						/>
-					</ListItem>
+						onClick={beginThisConsultation(appointment._id)}
+					/>
 				))}
-				{consultations.map(({_id, datetime}) => (
-					<ListItem key={_id} button onClick={editExistingConsultation(_id)}>
-						<ListItemAvatar>
-							<Avatar
-								className={classNames({
-									[classes.avatarEdit]: isToday(datetime),
-								})}
-							>
-								<BookmarkIcon />
-							</Avatar>
-						</ListItemAvatar>
-						<ListItemText
-							primary={`Éditer la consultation ${formatDatetime(datetime)}`}
-						/>
-					</ListItem>
+				{consultations.map((consultation) => (
+					<EditExistingConsultationItem
+						key={consultation._id}
+						consultation={consultation}
+						onClick={editExistingConsultation(consultation._id)}
+					/>
 				))}
-				<ListItem button onClick={scheduleNewAppointment}>
-					<ListItemAvatar>
-						<Avatar
-							className={classNames({
-								[classes.avatarSchedule]: appointments.length === 0,
-							})}
-						>
-							<TodayIcon />
-						</Avatar>
-					</ListItemAvatar>
-					<ListItemText primary="Programmer un nouveau rendez-vous" />
-				</ListItem>
-				<ListItem button onClick={createNewConsultation}>
+				<ScheduleNewAppointmentItem
+					emphasize={appointments.length === 0}
+					onClick={scheduleNewAppointment}
+				/>
+				<ListItemButton onClick={createNewConsultation}>
 					<ListItemAvatar>
 						<Avatar>
 							<NewReleasesIcon />
 						</Avatar>
 					</ListItemAvatar>
 					<ListItemText primary="Créer une nouvelle consultation vierge" />
-				</ListItem>
+				</ListItemButton>
 			</List>
-		</StyledDialog>
+		</Dialog>
 	);
 };
 

commit b86a09b25599718000e4f9958df01e814bf8f0f7

:adhesive_bandage: fix(SetPicker): Correct suggestions hidding logic.
Was broken earlier in 340485b15aa467680761875641f53c06ae6872d8.

imports/ui/input/Suggestions.tsx
@@ -65,7 +65,7 @@ const Suggestions = React.forwardRef(
 		ref: React.Ref<HTMLDivElement>,
 	) => {
 		const createItemSuggestion = createNewItem && !error && inputValue;
-		const open = hide || (!suggestions?.length && !createItemSuggestion);
+		const open = !hide && Boolean(suggestions?.length || createItemSuggestion);
 		return (
 			<Root ref={ref} open={open}>
 				<UnorderedList {...rest}>