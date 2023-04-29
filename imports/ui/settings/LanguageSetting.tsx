78a1f271e9e0f7024f6915ace0c66ab213d46076
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2023-04-29 20:45:36 +0200

    :sparkles: feat(locale): React to browser languages changes.
    Before this, changing browser languages needed a tab refresh to be
    taken into account.

--- imports/ui/settings/LanguageSetting.tsx
+++ imports/ui/settings/LanguageSetting.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import availableLocales, {
 	localeDescriptions,
 } from '../../i18n/availableLocales';
-import {navigatorLocale} from '../../i18n/navigator';
+import {useNavigatorLocale} from '../../i18n/navigator';
 import tuple from '../../lib/types/tuple';
 
 import SelectOneSetting from './SelectOneSetting';
 
 const options = tuple('navigator' as const, ...availableLocales);
-const optionToString = (x: string) =>
-	x === 'navigator'
-		? `${localeDescriptions[navigatorLocale()]} (same as navigator)`
-		: localeDescriptions[x];
 
 const LanguageSetting = ({className}) => {
+	const navigatorLocale = useNavigatorLocale();
+
+	const optionToString = useCallback(
+		(x: string) =>
+			x === 'navigator'
+				? `${localeDescriptions[navigatorLocale]} (same as navigator)`
+				: localeDescriptions[x],
+		[navigatorLocale],
+	);
+
 	return (
 		<SelectOneSetting
 			className={className}
