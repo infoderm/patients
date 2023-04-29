93401a2485ac75882866f2cbbb7a710dd032caf9
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2023-04-29 22:32:24 +0200

    :sparkles: feat(locale): Handle catch-all navigator locales.
    Like "en", "fr", and "nl".

--- imports/i18n/availableLocales.ts
+++ imports/i18n/availableLocales.ts
@@ -1,3 +1,9 @@
+// NOTE These must be ordered from most number of users to least number of
+// users. For instance, "en-US" should be placed before the other
+// english variants so that that variant is preferred if a user as set their
+// browser language to "en" (catch-all for english variants).
+// If implementing a catch-all "en" locale, it should be placed before any
+// english variants.
 export const localeDescriptions = {
 	'en-US': 'English (US)',
 	'fr-BE': 'Français (Belgique)',
@@ -6,6 +12,20 @@ export const localeDescriptions = {
 
 export type AvailableLocale = keyof typeof localeDescriptions;
 
-export default new Set<AvailableLocale>(
-	Object.keys(localeDescriptions) as AvailableLocale[],
-);
+export const availableLocalesOrdered = Object.keys(
+	localeDescriptions,
+) as AvailableLocale[];
+
+const availableLocales = new Set<AvailableLocale>(availableLocalesOrdered);
+
+export const bestMatch = (locale: string): AvailableLocale | undefined => {
+	// TODO If there is no match on the prefix, we do not try to partially
+	// match the requested locale. We prefer to fallback to the user resolving
+	// the ambiguity manually. We should reevaluate this choice later.
+	// TODO Use a trie.
+	return availableLocalesOrdered.find((availableLocale) =>
+		availableLocale.startsWith(locale),
+	);
+};
+
+export default availableLocales;
