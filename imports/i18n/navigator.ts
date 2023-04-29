3ce99dd302d50a38b6dabc0dccf6574544ec1547
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2023-04-29 20:45:36 +0200

    :sparkles: feat(locale): React to browser languages changes.
    Before this, changing browser languages needed a tab refresh to be
    taken into account.

--- imports/i18n/navigator.ts
+++ imports/i18n/navigator.ts
@@ -1,3 +1,4 @@
+import {useEffect, useState} from 'react';
 import {defaults} from '../api/settings';
 import availableLocales, {type AvailableLocale} from './availableLocales';
 
@@ -7,4 +8,26 @@ export const navigatorLocale = () => {
 			availableLocales.has(key as AvailableLocale),
 		) ?? defaults.lang
 	);
+};
+
+export const useNavigatorLocale = () => {
+	const [state, setState] = useState(navigatorLocale());
+
+	useEffect(() => {
+		const handleEvent = () => {
+			setState(navigatorLocale());
+		};
+
+		window.addEventListener('languagechange', handleEvent);
+
+		// NOTE Simulate synthetic event in case any event occurred before we
+		// could subscribe.
+		handleEvent();
+
+		return () => {
+			window.removeEventListener('languagechange', handleEvent);
+		};
+	}, []);
+
+	return state;
 };
--- imports/i18n/useLocale.ts
+++ imports/i18n/useLocale.ts
@@ -1,9 +1,10 @@
 import {useSettingCached} from '../ui/settings/hooks';
-import {navigatorLocale} from './navigator';
+import {useNavigatorLocale} from './navigator';
 
 const useLocale = (): string => {
+	const navigatorLocale = useNavigatorLocale();
 	const {value} = useSettingCached('lang');
-	return value === 'navigator' ? navigatorLocale() : value;
+	return value === 'navigator' ? navigatorLocale : value;
 };
 
 export default useLocale;
