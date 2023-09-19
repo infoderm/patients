d0d3a9a322488d820881535fa2449c618539d032
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2023-09-20 00:12:27 +0200

    :wrench: config(renovate): Only ignore deps that are in sync with Meteor.

--- .upgrade/upgrade.js
+++ .upgrade/upgrade.js
@@ -1,24 +1,43 @@
 #!/usr/bin/env node
 
-const ncu = require('npm-check-updates');
 const fs = require('fs');
+
+const ncu = require('npm-check-updates');
 
 const renovateConfig = JSON.parse(fs.readFileSync('renovate.json').toString());
 const packageRules = renovateConfig.packageRules[0];
-const {
-	matchPackageNames,
-	matchPackagePrefixes,
-} = packageRules;
+const {matchPackageNames, matchPackagePrefixes} = packageRules;
 
-const reject = matchPackageNames.concat(matchPackagePrefixes.map((x) => `${x}*`));
+const reject = matchPackageNames
+	.concat(matchPackagePrefixes.map((x) => `${x}*`))
+	.concat([
+		'downshift',
+		'healthone',
+		'idb',
+		'medidoc',
+		'mem',
+		'pdfjs-dist',
+		'puppeteer',
+		'react-router-dom',
+		'react-spring',
+		'total-order',
+		'xo',
+		'@mui/x-date-pickers',
+		'@combinatorics/*',
+		'@total-order/*',
+		'@set-theory/*',
+		'@iterable-iterator/*',
+		'@heap-data-structure/*',
+		'@functional-abstraction/*',
+	]);
 
-ncu.run({
-  upgrade: process.argv[2] === '-u',
-  reject,
-  jsonUpgraded: false,
-  silent: false,
-}).catch(
-  (error) => {
-	console.error(error);
-  }
-);
+ncu
+	.run({
+		upgrade: process.argv[2] === '-u',
+		reject,
+		jsonUpgraded: false,
+		silent: false,
+	})
+	.catch((error) => {
+		console.error(error);
+	});
