55eb79bc9a77cf69fe9cecc8adb8ffe1a8bf17f8
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2023-09-20 22:29:21 +0200

    :woman_technologist: dx: Halt on duplicated collection or endpoint name in tests.

--- imports/api/collection/define.ts
+++ imports/api/collection/define.ts
@@ -5,8 +5,6 @@ const define = <T extends Document, U = T>(name: string) => {
 	return new Collection<T, U>(name, {
 		idGeneration: 'STRING',
 		defineMutationMethods: false,
-		// @ts-expect-error This is not part of the public API.
-		_suppressSameNameError: Boolean(Meteor.isTest || Meteor.isAppTest),
 	});
 };
 
