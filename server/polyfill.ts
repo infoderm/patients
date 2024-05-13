cf40e0701be062860ec3ce883e45e52648839c90
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-05-13 18:04:40 +0200

    :woman_technologist: dx(server): Add modern `fetch` polyfill.

--- server/polyfill.ts
+++ server/polyfill.ts
@@ -3,3 +3,6 @@ import 'regenerator-runtime/runtime.js';
 
 // eslint-disable-next-line import/no-unassigned-import
 import 'core-js/features/string/replace-all';
+
+// eslint-disable-next-line import/no-unassigned-import
+import './polyfill/fetch';
