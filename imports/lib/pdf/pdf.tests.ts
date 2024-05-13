fb09bd5efe66cc6ca610b7da9ccf4d04eb2e33c5
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-05-13 14:00:46 +0200

    :woman_technologist: dx: Fix warnings caused by example PDF used in tests.

--- imports/lib/pdf/pdf.tests.ts
+++ imports/lib/pdf/pdf.tests.ts
@@ -5,11 +5,20 @@ import {client, server} from '../../_test/fixtures';
 
 import {fetchPDF} from './pdf';
 
-const document = `%PDF-1.
-1 0 obj<</Pages 2 0 R>>endobj
-2 0 obj<</Kids[3 0 R]/Count 1>>endobj
-3 0 obj<</Parent 2 0 R>>endobj
-trailer <</Root 1 0 R>>
+const document = `%PDF-1.0
+1 0 obj<</Type/Catalog/Pages 2 0 R>>endobj
+2 0 obj<</Type/Pages/Kids[3 0 R]/Count 1>>endobj
+3 0 obj<</Type/Page/MediaBox[0 0 3 3]>>endobj
+xref
+0 4
+0000000000 65535 f
+0000000009 00000 n
+0000000052 00000 n
+0000000101 00000 n
+trailer<</Size 4/Root 1 0 R>>
+startxref
+147
+%EOF
 `;
 
 client(__filename, () => {
