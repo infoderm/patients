b048242e25c9c2759d3f8ac01c1754c1425d0e41
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-05-21 16:15:15 +0200

    :pencil2: typo: Correct note in `api/healthcheck` rate-limiter configuration.

--- server/api/healthcheck/rateLimiter.ts
+++ server/api/healthcheck/rateLimiter.ts
@@ -4,7 +4,7 @@ const rateLimiter = new RateLimiterMemory({
 	// NOTE key prefix is not necessary since each RateLimiterMemory is backed
 	// by its own storage.
 	keyPrefix: '',
-	points: 15, // NOTE 6 points
+	points: 15, // NOTE 15 points
 	duration: 60, // NOTE Per minute
 });
 
