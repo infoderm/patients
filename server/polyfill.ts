0725abceb2719c50a7d2e2f3b08eef4bf7f4ca02
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-05-23 10:01:55 +0200

    SQUASH remove node-fetch

--- server/polyfill.ts
+++ server/polyfill.ts
@@ -3,6 +3,3 @@ import 'regenerator-runtime/runtime.js';
 
 // eslint-disable-next-line import/no-unassigned-import
 import 'core-js/features/string/replace-all';
-
-// eslint-disable-next-line import/no-unassigned-import
-import './polyfill/fetch';
