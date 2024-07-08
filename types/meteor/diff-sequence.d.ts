54dd6f2c1562503170b77037624b5c543669c467
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-07-08 20:47:09 +0200

    :recycle: refactor(`meteor/diff-sequence`): Rename `Options` to `DiffOptions`.

--- types/meteor/diff-sequence.d.ts
+++ types/meteor/diff-sequence.d.ts
@@ -2,7 +2,7 @@ declare module 'meteor/diff-sequence' {
 	import {type Mongo} from 'meteor/mongo';
 	import {type Document} from 'mongodb';
 
-	type Options<T> = {
+	type DiffOptions<T> = {
 		projectionFn?: (document: T) => Partial<T>;
 	};
 
@@ -12,7 +12,7 @@ declare module 'meteor/diff-sequence' {
 			old_results: T[],
 			new_results: T[],
 			observer: Mongo.ObserveChangesCallbacks<T>,
-			options?: Options<T> | undefined,
+			options?: DiffOptions<T> | undefined,
 		): void;
 
 		function diffQueryChanges<T extends Document>(
@@ -20,21 +20,21 @@ declare module 'meteor/diff-sequence' {
 			old_results: Map<string, T>,
 			new_results: Map<string, T>,
 			observer: Mongo.ObserveChangesCallbacks<T>,
-			options?: Options<T> | undefined,
+			options?: DiffOptions<T> | undefined,
 		): void;
 
 		function diffQueryUnorderedChanges<T extends Document>(
 			old_results: Map<string, T>,
 			new_results: Map<string, T>,
 			observer: Mongo.ObserveChangesCallbacks<T>,
-			options?: Options<T> | undefined,
+			options?: DiffOptions<T> | undefined,
 		): void;
 
 		function diffQueryOrderedChanges<T extends Document>(
 			old_results: T[],
 			new_results: T[],
 			observer: Mongo.ObserveChangesCallbacks<T>,
-			options?: Options<T> | undefined,
+			options?: DiffOptions<T> | undefined,
 		): void;
 
 		function applyChanges<T extends Document>(
