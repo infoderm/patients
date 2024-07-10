700c8e1cb6ef44a0d29dd622afb6deef056ab7b4
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-07-10 16:26:20 +0200

    :woman_technologist: dx: Keep track of collections by name in a registry.

---
+++ imports/api/collection/registry.ts
@@ -0,0 +1,26 @@
+import assert from 'assert';
+
+import type Document from '../Document';
+import type Collection from '../Collection';
+
+const _registry = new Map<string, Collection<any, any>>();
+
+export const getCollection = <T extends Document, U = T>(
+	name: string,
+): Collection<T, U> => {
+	const collection = _registry.get(name);
+	assert(collection !== undefined);
+	return collection;
+};
+
+export const hasCollection = (name: string) => {
+	return _registry.has(name);
+};
+
+export const addCollection = <T extends Document, U = T>(
+	name: string,
+	collection: Collection<T, U>,
+) => {
+	assert(!hasCollection(name));
+	_registry.set(name, collection);
+};
