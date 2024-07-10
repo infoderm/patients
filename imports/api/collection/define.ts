17c3b1ceeb9402033cfe54a8d656358e8ee2b02c
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-07-10 16:26:20 +0200

    :woman_technologist: dx: Keep track of collections by name in a registry.

--- imports/api/collection/define.ts
+++ imports/api/collection/define.ts
@@ -1,11 +1,21 @@
+import assert from 'assert';
+
 import type Document from '../Document';
 import Collection from '../Collection';
 
+import {hasCollection, addCollection} from './registry';
+
 const define = <T extends Document, U = T>(name: string) => {
-	return new Collection<T, U>(name, {
+	assert(!hasCollection(name));
+
+	const collection = new Collection<T, U>(name, {
 		idGeneration: 'STRING',
 		defineMutationMethods: false,
 	});
+
+	addCollection(name, collection);
+
+	return collection;
 };
 
 export default define;
