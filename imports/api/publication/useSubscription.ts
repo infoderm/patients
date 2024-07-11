456c36bf1d96a1564d168c00459fe2f734e04d3f
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-07-11 18:16:48 +0200

    :adhesive_bandage: fix(`useSubscription`): Set `loading = false` on stop/error.

--- imports/api/publication/useSubscription.ts
+++ imports/api/publication/useSubscription.ts
@@ -1,6 +1,5 @@
 import {Meteor} from 'meteor/meteor';
-import {Tracker} from 'meteor/tracker';
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 
 import useChanged from '../../ui/hooks/useChanged';
 
@@ -8,26 +7,41 @@ import type Args from '../Args';
 
 import subscribe from './subscribe';
 import type Publication from './Publication';
+import type SubscriptionHandle from './SubscriptionHandle';
 
 const useSubscriptionClient = <A extends Args>(
 	publication?: Publication<A> | null,
 	...args: A
 ): (() => boolean) => {
 	const [loading, setLoading] = useState(true);
+	const handleRef = useRef<SubscriptionHandle | null>(null);
 
-	const deps = [publication, JSON.stringify(args)];
+	const deps = [setLoading, publication, JSON.stringify(args)];
 
 	useEffect(() => {
-		const computation = Tracker.nonreactive(() =>
-			Tracker.autorun(() => {
-				const ready = !publication || subscribe(publication, ...args).ready();
-				setLoading(!ready);
-			}),
-		);
+		if (!publication) return undefined;
+		const setNotLoading = () => {
+			if (handleRef.current === handle) setLoading(false);
+		};
 
-		// Stop the computation on when publication changes or unmount.
+		const callbacks = {
+			onReady: setNotLoading,
+			onStop: setNotLoading,
+			onError: setNotLoading,
+		};
+
+		const handle = subscribe(publication, ...args, callbacks);
+		handleRef.current = handle;
+
+		// NOTE `setLoading(true)` is called:
+		//   - on first execution,
+		//   - on subsequent executions, if the subscription is not ready yet
+		//     (e.g. double-render in strict mode in development, concurrent mode)
+		//   - when restarting a stopped or errored subscription
+		setLoading(!handle.ready());
+
 		return () => {
-			computation.stop();
+			handle.stop();
 		};
 	}, deps);
 
