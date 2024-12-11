dd7f6a4234a6ee06afc4b444f7d54f257f0cfee9
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-12-11 18:55:57 +0100

    :adhesive_bandage: fix(endpoints): Execute endpoints calls in isolation.

--- imports/api/_apply.ts
+++ imports/api/_apply.ts
@@ -3,9 +3,49 @@ import {DDP} from 'meteor/ddp';
 
 import promisify from '../lib/async/promisify';
 
+import createPromise from '../lib/async/createPromise';
+
 import type Options from './endpoint/Options';
 import type Args from './Args';
 import type Serializable from './Serializable';
+
+class LockReleaseHandle {
+	#internals = createPromise<void>();
+
+	public async _promise(): Promise<void> {
+		return this.#internals.promise;
+	}
+
+	public _resolve() {
+		this.#internals.resolve();
+	}
+}
+
+class Lock {
+	#lastHandle: LockReleaseHandle | null = null;
+
+	public async acquire() {
+		const release = this.#lastHandle?._promise();
+		const handle = new LockReleaseHandle();
+
+		this.#lastHandle = handle;
+
+		await release;
+
+		return handle;
+	}
+
+	public release(handle: LockReleaseHandle) {
+		if (handle === this.#lastHandle) {
+			// NOTE: Garbage collect.
+			this.#lastHandle = null;
+		}
+
+		handle._resolve();
+	}
+}
+
+const _lock = new Lock();
 
 const __meteor_apply_promisified = promisify(
 	// @ts-expect-error Private access.
@@ -24,18 +64,19 @@ const _apply = async <R extends Serializable>(
 		isFromCallAsync: true,
 		...options,
 	};
-	// @ts-expect-error Private access.
-	DDP._CurrentMethodInvocation._set();
-	// @ts-expect-error Private access.
-	DDP._CurrentMethodInvocation._setCallAsyncMethodRunning(true);
-	const result = (await __meteor_apply_promisified(
-		name,
-		args,
-		applyOptions,
-	)) as Promise<R>;
-	// @ts-expect-error Private access.
-	DDP._CurrentMethodInvocation._setCallAsyncMethodRunning(false);
-	return result;
+	const handle = await _lock.acquire();
+	try {
+		// @ts-expect-error Private access.
+		DDP._CurrentMethodInvocation._set();
+		// @ts-expect-error Private access.
+		DDP._CurrentMethodInvocation._setCallAsyncMethodRunning(true);
+		const result = await __meteor_apply_promisified(name, args, applyOptions);
+		// @ts-expect-error Private access.
+		DDP._CurrentMethodInvocation._setCallAsyncMethodRunning(false);
+		return result as R;
+	} finally {
+		_lock.release(handle);
+	}
 };
 
 export default _apply;
