5da84c54a8f5cf2a0c36052b247ccd2d66623300
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-12-22 10:16:16 +0100

    :construction: progress(PatientPersonalInformation): Fix undo.

--- imports/ui/patients/usePatientPersonalInformationReducer.ts
+++ imports/ui/patients/usePatientPersonalInformationReducer.ts
@@ -3,9 +3,14 @@ import {useReducer} from 'react';
 import {type PatientDocument} from '../../api/collection/patients';
 import {documentDiff} from '../../api/update';
 
+type UpdatedPatientDocument = Omit<PatientDocument, 'deathdate'> & {
+	deathdate?: Date | null;
+};
+
 type State = {
 	init: PatientDocument;
-	current: Omit<PatientDocument, 'deathdate'> & {deathdate?: Date | null};
+	last: PatientDocument;
+	current: UpdatedPatientDocument;
 	editing: boolean;
 	dirty: boolean;
 	deleting: boolean;
@@ -13,6 +18,7 @@ type State = {
 
 const initialState = (init: PatientDocument): State => ({
 	init,
+	last: init,
 	current: init,
 	editing: false,
 	dirty: false,
@@ -38,63 +44,46 @@ export const reducer = (state: State, action: Action): State => {
 
 			switch (action.key) {
 				case 'deathdateModifiedAt': {
+					const current: UpdatedPatientDocument = {
+						...state.current,
+						deathdateModifiedAt: action.value,
+						deathdate: null,
+					};
+					const changes = documentDiff(state.init, current);
+					const dirty = Object.keys(changes).length > 0;
 					return {
 						...state,
-						current: {
-							...state.current,
-							deathdateModifiedAt: action.value,
-							deathdate: null,
-						},
-						dirty: true,
+						current,
+						dirty,
 					};
 				}
 
 				case 'deathdate': {
+					const current = {
+						...state.current,
+						deathdateModifiedAt: new Date(),
+						deathdate: action.value,
+					};
+					const changes = documentDiff(state.init, current);
+					const dirty = Object.keys(changes).length > 0;
 					return {
 						...state,
-						current: {
-							...state.current,
-							deathdateModifiedAt: new Date(),
-							deathdate: action.value,
-						},
-						dirty: true,
+						current,
+						dirty,
 					};
 				}
 
 				default: {
+					const current = {...state.current, [action.key]: action.value};
+					const changes = documentDiff(state.init, current);
+					const dirty = Object.keys(changes).length > 0;
 					return {
 						...state,
-						current: {...state.current, [action.key]: action.value},
-						dirty: true,
+						current,
+						dirty,
 					};
 				}
 			}
-		}
-
-		case 'editing': {
-			return {...state, editing: true};
-		}
-
-		case 'not-editing': {
-			return {...state, editing: false, dirty: false};
-		}
-
-		case 'deleting': {
-			return {...state, deleting: true};
-		}
-
-		case 'not-deleting': {
-			return {...state, deleting: false};
-		}
-
-		case 'undo': {
-			return {
-				...state,
-				editing: false,
-				dirty: false,
-				deleting: false,
-				current: state.init,
-			};
 		}
 
 		case 'merge': {
@@ -124,14 +113,43 @@ export const reducer = (state: State, action: Action): State => {
 				  };
 		}
 
-		case 'init': {
-			if (state.editing) return state;
+		case 'editing': {
+			return {...state, editing: true};
+		}
 
-			return state.init === action.payload && state.current === action.payload
+		case 'not-editing': {
+			return {...state, editing: false};
+		}
+
+		case 'deleting': {
+			return {...state, deleting: true};
+		}
+
+		case 'not-deleting': {
+			return {...state, deleting: false};
+		}
+
+		case 'undo': {
+			return {
+				...state,
+				editing: false,
+				init: state.last,
+				current: state.last,
+			};
+		}
+
+		case 'init': {
+			return state.last === action.payload
 				? state
+				: state.editing
+				? {
+						...state,
+						last: action.payload,
+				  }
 				: {
 						...state,
 						init: action.payload,
+						last: action.payload,
 						current: action.payload,
 				  };
 		}
