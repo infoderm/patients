19277f1d55be4ef7abef915d73c03c5bbeef23c7
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-12-28 00:59:09 +0100

    :construction: progress: First draft for `observeSequenceChanges`.

---
+++ imports/api/query/observeSequenceChanges.ts
@@ -0,0 +1,42 @@
+import {type DiffOptions, DiffSequence} from 'meteor/diff-sequence';
+
+import type Collection from '../Collection';
+import type Document from '../Document';
+
+import type ObserveSequenceChangesCallbacks from '../ObserveSequenceChangesCallbacks';
+import {type Options} from '../transaction/TransactionDriver';
+
+import type Filter from './Filter';
+import watch from './watch';
+
+const _makeOnChange = <T extends Document>(
+	observer: ObserveSequenceChangesCallbacks<T>,
+	diffOptions: DiffOptions<T> | undefined,
+) => {
+	let previous: T[] = [];
+
+	return (next: T[]) => {
+		DiffSequence.diffQueryOrderedChanges<T>(
+			previous,
+			next,
+			observer,
+			diffOptions,
+		);
+		previous = next;
+	};
+};
+
+const observeSequenceChanges = async <T extends Document, U = T>(
+	collection: Collection<T, U>,
+	filter: Filter<T>,
+	options: Options,
+	observer: ObserveSequenceChangesCallbacks<T>,
+	diffOptions?: DiffOptions<T>,
+) => {
+	const handle = await watch(collection, filter, options);
+	handle.on('change', _makeOnChange(observer, diffOptions));
+	await handle.emit('start');
+	return handle;
+};
+
+export default observeSequenceChanges;
