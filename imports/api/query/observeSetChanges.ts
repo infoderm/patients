7ce64e574b9e86a7cea04fdce4483095c0cdb3a9
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-12-28 00:59:26 +0100

    :construction: progress: First draft for `observeSetChanges`.

---
+++ imports/api/query/observeSetChanges.ts
@@ -0,0 +1,46 @@
+import {type DiffOptions, DiffSequence} from 'meteor/diff-sequence';
+
+import type Collection from '../Collection';
+import type Document from '../Document';
+
+import type ObserveSetChangesCallbacks from '../ObserveSetChangesCallbacks';
+import {type Options} from '../transaction/TransactionDriver';
+
+import type Filter from './Filter';
+import watch from './watch';
+
+const _toSet = <T extends Document>(items: T[]): Map<string, T> =>
+	new Map(items.map((item) => [item._id, item]));
+
+const _makeOnChange = <T extends Document>(
+	observer: ObserveSetChangesCallbacks<T>,
+	diffOptions: DiffOptions<T> | undefined,
+) => {
+	let previous = _toSet<T>([]);
+
+	return (items: T[]) => {
+		const next = _toSet(items);
+		DiffSequence.diffQueryUnorderedChanges<T>(
+			previous,
+			next,
+			observer,
+			diffOptions,
+		);
+		previous = next;
+	};
+};
+
+const observeSetChanges = async <T extends Document, U = T>(
+	collection: Collection<T, U>,
+	filter: Filter<T>,
+	options: Options,
+	observer: ObserveSetChangesCallbacks<T>,
+	diffOptions?: DiffOptions<T>,
+) => {
+	const handle = await watch(collection, filter, options);
+	handle.on('change', _makeOnChange(observer, diffOptions));
+	await handle.emit('start');
+	return handle;
+};
+
+export default observeSetChanges;
