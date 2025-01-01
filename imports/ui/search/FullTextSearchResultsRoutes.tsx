0e862905a4a7bf446f508642f5ca6fbad2a27647
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-01-01 11:31:01 +0100

    :children_crossing: ux(`FullTextSearchResultRoutes`): Defer query input.

--- imports/ui/search/FullTextSearchResultsRoutes.tsx
+++ imports/ui/search/FullTextSearchResultsRoutes.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useDeferredValue} from 'react';
 import {styled} from '@mui/material/styles';
 import {useParams} from 'react-router-dom';
 
@@ -40,14 +40,15 @@ const FullTextSearchResults = () => {
 	const [key, refresh] = useRandom();
 	const {query: rawQuery} = useParams<Params>();
 	const query = myDecodeURIComponent(rawQuery);
+	const deferredQuery = useDeferredValue(query);
 	return (
 		<Root className={classes.root}>
 			<Typography className={classes.heading} variant="h3">
-				Results for query `{query}`.
+				Results for query `{deferredQuery}`.
 			</Typography>
 			<PatientsObservedSearchResultsPager
 				className={classes.results}
-				query={query}
+				query={deferredQuery}
 				refresh={refresh}
 				refreshKey={key}
 			/>
