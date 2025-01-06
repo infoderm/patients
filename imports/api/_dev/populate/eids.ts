646987b9bb9aae15b95462077743195eb24f6315
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-01-07 00:53:49 +0100

    :test_tube: test(patients): Cover creating a new patient from eid.

--- imports/api/_dev/populate/eids.ts
+++ imports/api/_dev/populate/eids.ts
@@ -3,6 +3,7 @@ import {faker} from '@faker-js/faker';
 import format from 'date-fns/format';
 
 import {makeTemplate} from '../../../_test/fixtures';
+import {randomPNGBase64} from '../../../_test/png';
 
 const DATE_FORMAT = 'yyyyMMdd';
 const AGE_MAX = 130;
@@ -51,3 +52,59 @@ export const newEidData = makeTemplate({
 		zip: () => faker.location.zipCode(),
 	},
 });
+
+export const exampleEidXML = (options?: {
+	name?: string;
+	firstname?: string;
+	middlenames?: string;
+	nationality?: string;
+	placeofbirth?: string;
+}) => {
+	const {name, firstname, middlenames, nationality, placeofbirth} = {
+		name: 'Name',
+		firstname: 'First Name',
+		middlenames: 'M',
+		nationality: 'Belge',
+		placeofbirth: 'Bruxelles',
+		...options,
+	};
+	// TODO: Generalizes and escape values.
+	return `<?xml version="1.0" encoding="UTF-8"?>
+<eid version="4.3" graphpersoversion="00">
+	<identity
+	  nationalnumber="70010112345"
+	  dateofbirth="19700101"
+	  gender="male"
+	  specialstatus="NO_STATUS"
+	>
+		<name>${name}</name>
+		<firstname>${firstname}</firstname>
+		<middlenames>${middlenames}</middlenames>
+		<nationality>${nationality}</nationality>
+		<placeofbirth>${placeofbirth}</placeofbirth>
+		<photo>${randomPNGBase64()}</photo>
+	</identity>
+	<card
+		documenttype="belgian_citizen"
+		carddata_appl_version="17"
+		cardnumber="012345678901"
+		chipnumber="0123456789ABCDEF0123456789ABCDEF"
+		validitydatebegin="20200101"
+		validitydateend="20300101"
+	>
+		<deliverymunicipality>Bruxelles</deliverymunicipality>
+	</card>
+	<address>
+		<streetandnumber>Rue de la montagne 58 </streetandnumber>
+		<zip>1000</zip>
+		<municipality>Bruxelles</municipality>
+	</address>
+	<certificates>
+		<root>ROOT</root>
+		<citizenca>CITIZENCA</citizenca>
+		<authentication>AUTHENTICATION</authentication>
+		<signing>SIGNING</signing>
+		<rrn>RRN</rrn>
+	</certificates>
+</eid>`;
+};
