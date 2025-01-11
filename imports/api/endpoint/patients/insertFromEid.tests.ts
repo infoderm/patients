61a17f840428eb81178269bbfc50c5d63a33921c
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-01-11 19:02:15 +0100

    :test_tube: test(`insertFromEid`): Check that eid entry is created.

--- imports/api/endpoint/patients/insertFromEid.tests.ts
+++ imports/api/endpoint/patients/insertFromEid.tests.ts
@@ -2,6 +2,7 @@ import {assert} from 'chai';
 
 import {randomUserId, server, throws} from '../../../_test/fixtures';
 
+import {Eids} from '../../collection/eids';
 import {Patients} from '../../collection/patients';
 import {Allergies} from '../../collection/allergies';
 import {Doctors} from '../../collection/doctors';
@@ -11,6 +12,8 @@ import {newEidData} from '../../_dev/populate/eids';
 import invoke from '../invoke';
 
 import {normalizedName, patientFieldsFromEid} from '../../patients';
+
+import removeUndefined from '../../../lib/object/removeUndefined';
 
 import insertFromEid from './insertFromEid';
 
@@ -64,5 +67,32 @@ server(__filename, () => {
 		assert.sameDeepMembers(await Allergies.find().fetchAsync(), []);
 		assert.sameDeepMembers(await Doctors.find().fetchAsync(), []);
 		assert.sameDeepMembers(await Insurances.find().fetchAsync(), []);
+	});
+
+	it('creates an eid entry', async () => {
+		const userId = randomUserId();
+
+		const eid = newEidData();
+
+		await invoke(insertFromEid, {userId}, [eid]);
+
+		const entries = await Eids.find({owner: userId}).fetchAsync();
+
+		assert.strictEqual(entries.length, 1);
+
+		const {_id, createdAt, owner, ...rest} = entries[0]!;
+
+		assert.strictEqual(owner, userId);
+		assert.isAtMost(createdAt.valueOf(), Date.now());
+
+		assert.deepEqual(
+			rest,
+			Object.fromEntries(
+				Object.entries(eid).map(([key, value]) => [
+					key,
+					removeUndefined(value as {}),
+				]),
+			),
+		);
 	});
 });
