05d9df4b83236b60bc4af404dfc6e570209aa326
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-01-25 21:39:03 +0100

    :recycle: refactor(`Document`): Define our own schema.

--- imports/api/Document.ts
+++ imports/api/Document.ts
@@ -1,9 +1,32 @@
-import {type Document as MongoDocument} from 'mongodb';
-
 import schema from '../lib/schema';
 
-type Document = MongoDocument;
+const literalSchema = schema.union([
+	schema.string(),
+	schema.number(),
+	schema.boolean(),
+	schema.undefined(), // TODO: Keep in document, remove from updates.
+	schema.null(), // TODO: Remove from document, add to update.
+	schema.date(),
+]);
 
-export const document = schema.record(schema.string(), schema.any());
+type Literal = schema.infer<typeof literalSchema>;
+
+type DocumentValue = Literal | {[key: string]: DocumentValue} | DocumentValue[];
+
+const documentValue: schema.ZodType<DocumentValue> = schema.lazy(() =>
+	schema.union([
+		literalSchema,
+		schema.record(schema.string(), documentValue),
+		schema.array(documentValue),
+	]),
+);
+
+const documentKey = schema.string();
+
+export const document = schema.record(documentKey, documentValue);
+// TODO: The following does not work:
+// type Document = schema.infer<typeof document>;
+// type Document = Record<string, DocumentValue>;
+type Document = Record<string, any>;
 
 export default Document;
