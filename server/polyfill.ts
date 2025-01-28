041e3721061480352b115e132fc6fa3b14525f78
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-01-28 19:10:29 +0100

    :arrow_up: deps: Upgrade `medidoc` to v1.2.0.

--- server/polyfill.ts
+++ server/polyfill.ts
@@ -8,4 +8,7 @@ import 'core-js/features/string/replace-all';
 import 'core-js/features/array/at';
 
 // eslint-disable-next-line import/no-unassigned-import
+import 'core-js/features/object/has-own';
+
+// eslint-disable-next-line import/no-unassigned-import
 import './polyfill/fetch';
