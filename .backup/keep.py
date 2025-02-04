408c66abdaca291f9edf32bd44e447c18d0f2972
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-02-04 23:18:07 +0100

    :woman_technologist: dx: Generalize backup script to allow testing.

--- .backup/keep.py
+++ .backup/keep.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python3
 
-import fileinput
 from datetime import datetime
 from collections import defaultdict
 from itertools import chain
+from sys import argv, stdin
 
-fmt = '%Y-%m-%d'
+fmt = '%Y-%m-%d_%H:%M:%S'
 cfmt = '%Y-%m-%W-%d'
-today = datetime.today()
-now = today.strftime(cfmt)
+cutoff = datetime.strptime(argv[1], fmt) if len(argv) >= 2 else datetime.today()
+_cutoff = cutoff.strftime(cfmt)
 
 years = defaultdict(list)
 months = defaultdict(list)
 weeks = defaultdict(list)
 days = defaultdict(list)
 
-for line in fileinput.input():
+for line in stdin:
     line = line[:-1]
 
-    day = line[:10]
-
-    key = datetime.strptime(day, fmt).strftime(cfmt)
+    parsed = datetime.strptime(line, fmt)
+    key = parsed.strftime(cfmt)
 
     gaps = [(years, 4), (months, 7), (weeks, 10), (days, 13)]
 
     for d, gap in gaps:
         subkey = key[:gap]
         d[subkey].append(line)
-        if subkey != now[:gap]: break
+        if subkey < _cutoff[:gap]: break
 
 values = chain(years.values(), months.values(), weeks.values(), days.values())
 
