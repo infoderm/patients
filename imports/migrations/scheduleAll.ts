98dd440feba975b616c9a4a671372d8793d39f75
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-02-18 00:07:10 +0100

    :sparkles: feat: Reparse Medidoc reports for which we only display sources.

--- imports/migrations/scheduleAll.ts
+++ imports/migrations/scheduleAll.ts
@@ -2,11 +2,13 @@ import {setDone as setMigrationsStatusDone} from './status';
 
 import v1 from './versions/v1';
 import v2 from './versions/v2';
+import v3 from './versions/v3';
 
 const scheduleAll = async () => {
 	console.time('migrations');
 	await v1();
 	await v2();
+	await v3();
 	console.timeEnd('migrations');
 
 	setMigrationsStatusDone();
