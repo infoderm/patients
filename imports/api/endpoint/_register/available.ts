ab1eda97adbc87d49c7d731c96cc226f8c868680
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-02-24 21:31:59 +0100

    :adhesive_bandage: fix(endpoints): Register `patients/{insert,update}FromEid`.
    This was overlooked in #1100.

--- imports/api/endpoint/_register/available.ts
+++ imports/api/endpoint/_register/available.ts
@@ -68,11 +68,15 @@ import '../patients/find';
 // eslint-disable-next-line import/no-unassigned-import
 import '../patients/insert';
 // eslint-disable-next-line import/no-unassigned-import
+import '../patients/insertFromEid';
+// eslint-disable-next-line import/no-unassigned-import
 import '../patients/merge';
 // eslint-disable-next-line import/no-unassigned-import
 import '../patients/remove';
 // eslint-disable-next-line import/no-unassigned-import
 import '../patients/update';
+// eslint-disable-next-line import/no-unassigned-import
+import '../patients/updateFromEid';
 
 // eslint-disable-next-line import/no-unassigned-import
 import '../permissions/token/generate';
