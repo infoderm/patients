f2229bb1af6301d94bf95cc8a2ac07e6b85897a3
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-02-28 23:03:08 +0100

    :test_tube: test(`<SelectColorSetting>`): Cover resetting to default.

--- imports/ui/settings/SelectColorSetting.tests.tsx
+++ imports/ui/settings/SelectColorSetting.tests.tsx
@@ -11,7 +11,7 @@ import {faker} from '@faker-js/faker';
 import createUserWithPassword from '../../api/user/createUserWithPassword';
 import loginWithPassword from '../../api/user/loginWithPassword';
 import {client, randomPassword, randomUserId} from '../../_test/fixtures';
-import {render} from '../../_test/react';
+import {render, waitFor} from '../../_test/react';
 
 import {
 	TIMEOUT_INPUT_DEBOUNCE,
@@ -20,6 +20,7 @@ import {
 import sleep from '../../lib/async/sleep';
 
 import SelectColorSetting from './SelectColorSetting';
+import {setSetting} from './hooks';
 
 client(__filename, () => {
 	it('should debounce user input', async () => {
@@ -62,4 +63,43 @@ client(__filename, () => {
 
 		assert.strictEqual(button.textContent?.toUpperCase(), validInputs.at(-1));
 	}).timeout(10_000);
+
+	it('allows to reset setting to default', async () => {
+		const {setupUser} = await import('../../../test/app/client/fixtures');
+		const username = randomUserId();
+		const password = randomPassword();
+		await createUserWithPassword(username, password);
+		await loginWithPassword(username, password);
+
+		const {findByRole} = render(
+			<SelectColorSetting
+				aria-label="Color"
+				setting="theme-palette-secondary"
+			/>,
+		);
+
+		const button = await findByRole('button', {
+			name: 'Color',
+		});
+
+		const defaultValue = button.textContent;
+
+		const value = faker.color.rgb();
+		await setSetting('theme-palette-secondary', value);
+
+		await waitFor(() => {
+			assert.strictEqual(button.textContent, value);
+		});
+
+		const reset = await findByRole('button', {
+			name: 'Reset color',
+		});
+
+		const {user} = setupUser();
+		await user.click(reset);
+
+		await waitFor(() => {
+			assert.strictEqual(button.textContent, defaultValue);
+		});
+	});
 });
