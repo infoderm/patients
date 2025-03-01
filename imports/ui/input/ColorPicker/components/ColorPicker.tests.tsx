1bf688267992d310cc5b7e056743a816853d5a91
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-03-01 21:51:22 +0100

    :test_tube: test(`<ColorPicker>`): Cover pressing `{Escape}` in focused input.

--- imports/ui/input/ColorPicker/components/ColorPicker.tests.tsx
+++ imports/ui/input/ColorPicker/components/ColorPicker.tests.tsx
@@ -1,6 +1,7 @@
+import {faker} from '@faker-js/faker';
 import React from 'react';
 
-import {client} from '../../../../_test/fixtures';
+import {client, throws} from '../../../../_test/fixtures';
 import {render, waitForElementToBeRemoved} from '../../../../_test/react';
 
 import ColorPicker from './ColorPicker';
@@ -28,5 +29,38 @@ client(__filename, () => {
 			waitForElementToBeRemoved(hex),
 			user.keyboard('{Escape}'),
 		]);
+	});
+
+	it('should NOT close picker dialog on {Escape} when input is focused', async () => {
+		const {setupUser} = await import('../../../../../test/app/client/fixtures');
+		const {findByRole} = render(
+			<ColorPicker
+				aria-label="test-label"
+				defaultValue="#000"
+				// eslint-disable-next-line @typescript-eslint/no-empty-function
+				onChange={() => {}}
+			/>,
+		);
+
+		const button = await findByRole('button', {name: 'test-label'});
+
+		const {user} = setupUser();
+		await user.click(button);
+
+		const hex = await findByRole('textbox', {name: 'hex'});
+
+		const validInput = faker.color.rgb().toUpperCase();
+
+		await user.clear(hex);
+		await user.paste(validInput);
+
+		await throws(
+			async () =>
+				Promise.all([
+					waitForElementToBeRemoved(hex),
+					user.keyboard('{Escape}'),
+				]),
+			/Timed out in waitForElementToBeRemoved/,
+		);
 	});
 });
