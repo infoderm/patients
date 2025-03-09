83c29da410a716bd5f4635416773fa6302ded2dc
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-03-09 20:35:18 +0100

    :test_tube: test(API): Cover unregistered endpoints.

---
+++ lib/test/fixtures/_regenerator-runtime.app-tests.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/no-unassigned-import
+import 'regenerator-runtime/runtime.js';
---
+++ lib/test/fixtures/enable-api-endpoints.app-tests.ts
@@ -0,0 +1,3 @@
+// DECLARE ALL ENABLED API ENDPOINTS
+// eslint-disable-next-line import/no-unassigned-import
+import '../../../imports/api/endpoint/_register/enabled';
