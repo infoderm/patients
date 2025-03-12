d6e6201a66cf428ec0d8dfd098aabd6511779187
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-03-12 22:43:18 +0100

    :microscope: test: Increase coverage of `thumbify` and its dependencies.

---
+++ imports/api/uploads.tests.ts
@@ -0,0 +1,70 @@
+import {Readable} from 'stream';
+
+import {LRUCache as MemoryLRU} from 'lru-cache';
+
+import {type FileObj} from 'meteor/ostrio:files';
+import {assert} from 'chai';
+
+import {randomPDFUint8Array} from '../_test/pdf';
+import {server} from '../_test/fixtures';
+import streamToBuffer from '../lib/stream/streamToBuffer';
+import {
+	assertEqual as assertEqualImages,
+	whiteRectanglePNG,
+} from '../_test/image';
+import bufferToUint8ArrayView from '../lib/buffer/bufferToUint8ArrayView';
+import {randomPNGBuffer} from '../_test/png';
+
+import {cacheKey, type MetadataType, thumbify} from './uploads';
+
+const width = 200;
+const height = 200;
+
+server(__filename, () => {
+	it('should allow to create PDFs thumbnails', async () => {
+		const cache = new MemoryLRU<string, Uint8Array>({max: 1});
+		const source = () => Readable.from([randomPDFUint8Array()]);
+		const output = await thumbify(
+			{
+				_id: 'test',
+				isImage: false,
+				isPDF: true,
+			} as FileObj<MetadataType>,
+			source,
+			{minWidth: width, minHeight: height},
+			cache,
+		);
+		assert.instanceOf(output, Readable);
+
+		const buffer = await streamToBuffer(output);
+		const expected = await whiteRectanglePNG({width, height});
+
+		await assertEqualImages(buffer, expected);
+	});
+
+	it('should use cached PDFs thumbnails', async () => {
+		const upload = {
+			_id: 'test',
+			isImage: false,
+			isPDF: true,
+		} as FileObj<MetadataType>;
+
+		const source = () => {
+			throw new Error('cache miss');
+		};
+
+		const size = {minWidth: width, minHeight: height};
+
+		const cache = new MemoryLRU<string, Uint8Array>({max: 1});
+		const expected = await randomPNGBuffer();
+		const thumbnail = bufferToUint8ArrayView(expected);
+		cache.set(cacheKey(upload, size), thumbnail);
+
+		const output = await thumbify(upload, source, size, cache);
+		assert.instanceOf(output, Readable);
+
+		const buffer = await streamToBuffer(output);
+
+		assert.deepEqual(buffer, expected);
+	});
+});
