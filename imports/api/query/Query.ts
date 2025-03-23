437cf452de1506b191dfe756dc6416061f65e0f5
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-03-23 17:59:22 +0100

    :construction: progress(`api/query`): Redefine and implement `Query`.

--- imports/api/query/Query.ts
+++ imports/api/query/Query.ts
@@ -1,9 +1,30 @@
-import type Options from './Options';
-import type Selector from './Selector';
+import schema from '../../util/schema';
+
+import sort, {type Sort} from './sort';
+import {projection} from './Projection';
+import type Projection from './Projection';
+import type Filter from './Filter';
+import {filter} from './Filter';
 
 type Query<T> = {
-	selector: Selector<T>;
-	options: Options<T> | null;
+	filter: Filter<T>;
+	projection?: Projection<T>;
+	sort?: Sort<T>;
+	skip?: number;
+	limit?: number;
 };
+
+export const query = <S extends schema.ZodTypeAny>(
+	tSchema: S,
+): schema.ZodType<Query<schema.infer<S>>> =>
+	schema
+		.object({
+			filter: filter(tSchema),
+			projection: projection(tSchema).optional(),
+			sort: sort(tSchema).optional(),
+			skip: schema.number().int().optional(),
+			limit: schema.number().int().optional(),
+		})
+		.strict() as schema.ZodType<Query<schema.infer<S>>>;
 
 export default Query;
