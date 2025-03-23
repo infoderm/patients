878ff3bc5651b0d956b332f2e3dd1e86257ed6cc
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-03-23 12:27:20 +0100

    :woman_technologist: dx(`util/uri`): Improve typing of `myDecodeURIComponent`.

--- imports/util/uri.ts
+++ imports/util/uri.ts
@@ -2,8 +2,9 @@ export const myEncodeURIComponent = (component: string): string => {
 	return encodeURIComponent(component);
 };
 
-export const myDecodeURIComponent = (
-	component: string | undefined,
-): string | undefined => {
+export function myDecodeURIComponent<T extends string | undefined>(
+	component: T,
+): T;
+export function myDecodeURIComponent(component: string | undefined) {
 	return component === undefined ? undefined : decodeURIComponent(component);
-};
+}
