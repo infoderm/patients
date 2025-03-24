6604f58c1aedd1073fcf523797335b1938035eb5
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-03-24 07:34:53 +0100

    :woman_technologist: dx(tests): Correct typing of `newUpload` util.

--- imports/api/_dev/populate/uploads.ts
+++ imports/api/_dev/populate/uploads.ts
@@ -4,8 +4,8 @@ import {randomPNGBuffer, randomPNGDataURI} from '../../../_test/png';
 import {type MetadataType, Uploads} from '../../uploads';
 
 export const newUpload = async (
-	invocation,
-	options?,
+	invocation?: {userId?: string},
+	options?: {name?: string},
 ): Promise<FileRef<MetadataType>> => {
 	const type = 'image/png';
 	const fileName = options?.name ?? 'pic.png';
@@ -17,7 +17,7 @@ export const newUpload = async (
 				{
 					fileName,
 					type,
-					userId: invocation.userId,
+					userId: invocation?.userId,
 				},
 				(writeError, fileRef) => {
 					if (writeError) {
