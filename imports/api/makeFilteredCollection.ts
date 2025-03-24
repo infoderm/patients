6ac1718f56e502a9898d0507d97edb015a138207
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-03-24 04:30:49 +0100

    :recycle: refactor(`makeFilteredCollection`): Extract `make*Publication`.

--- imports/api/makeFilteredCollection.ts
+++ imports/api/makeFilteredCollection.ts
@@ -3,6 +3,7 @@ import {type DependencyList} from 'react';
 import schema from '../util/schema';
 
 import type Collection from './Collection';
+import type Document from './Document';
 
 import defineCollection from './collection/define';
 import define from './publication/define';
@@ -17,43 +18,53 @@ import {type AuthenticatedContext} from './publication/Context';
 import type Query from './query/Query';
 import {userQuery} from './query/UserQuery';
 
+type QueryArg<T> = (
+	ctx: AuthenticatedContext,
+	publicationQuery: UserQuery<T>,
+) => Query<T>;
+
+const makeFilteredCollectionPublication = <T extends Document, U>(
+	collection: Collection<T, U>,
+	query: QueryArg<T>,
+	name: string,
+) => {
+	return async function (publicationQuery: UserQuery<T>) {
+		const {filter, ...rest} = query(this, publicationQuery);
+
+		const options = {
+			...rest,
+			skip: 0,
+			limit: 0,
+		};
+
+		const handle = await observeSetChanges(
+			collection,
+			filter,
+			options,
+			publishCursorObserver(this, name),
+		);
+
+		this.onStop(async (error?: Error) => {
+			await handle.emit('stop', error);
+		});
+		this.ready();
+	};
+};
+
 const makeFilteredCollection = <
 	S extends schema.ZodTypeAny,
 	U = schema.infer<S>,
 >(
 	collection: Collection<schema.infer<S>, U>,
 	tSchema: S,
-	query: (
-		ctx: AuthenticatedContext,
-		publicationQuery: UserQuery<schema.infer<S>>,
-	) => Query<schema.infer<S>>,
+	query: QueryArg<schema.infer<S>>,
 	name: string,
 ) => {
 	const publication = define({
 		name,
 		authentication: AuthenticationLoggedIn,
 		schema: schema.tuple([userQuery(tSchema)]),
-		async handle(publicationQuery: UserQuery<schema.infer<S>>) {
-			const {filter, ...rest} = query(this, publicationQuery);
-
-			const options = {
-				...rest,
-				skip: 0,
-				limit: 0,
-			};
-
-			const handle = await observeSetChanges(
-				collection,
-				filter,
-				options,
-				publishCursorObserver(this, name),
-			);
-
-			this.onStop(async (error?: Error) => {
-				await handle.emit('stop', error);
-			});
-			this.ready();
-		},
+		handle: makeFilteredCollectionPublication(collection, query, name),
 	});
 
 	const Filtered = defineCollection<schema.infer<S>, U>(name);
