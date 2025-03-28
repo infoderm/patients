aee7b4e1d85c1533cb90b4addb8cd1275705a0a8
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-03-28 22:37:30 +0100

    :microscope: test(documents): Increase coverage of deletion and filtering.

---
+++ test/app/client/document/delete.app-tests.ts
@@ -0,0 +1,51 @@
+import {
+	client,
+	randomPassword,
+	randomUserId,
+} from '../../../../imports/_test/fixtures';
+
+import {exampleMedidocReport} from '../../../../imports/api/_dev/populate/documents';
+
+import createUserWithPasswordAndLogin from '../../../../imports/api/user/createUserWithPasswordAndLogin';
+
+import {setupApp, navigateTo, uploadFile} from '../fixtures';
+
+client(__filename, () => {
+	it('should allow to delete documents', async () => {
+		const username = randomUserId();
+		const password = randomPassword();
+		const app = setupApp();
+		await createUserWithPasswordAndLogin(username, password);
+
+		const {findByRole, findByText, findByLabelText, user} = app;
+
+		await navigateTo(app, 'Documents', '/documents');
+
+		await findByRole('heading', {name: 'Nothing to see on page 1.'});
+
+		uploadFile(
+			await findByLabelText('Import Document'),
+			new File([exampleMedidocReport.contents], 'test.txt'),
+		);
+
+		await findByText('PZ7654321Y9');
+
+		await user.click(await findByRole('button', {name: /^Delete document #/}));
+
+		await user.click(await findByRole('button', {name: 'Delete'}));
+
+		await findByRole('heading', {name: 'Nothing to see on page 1.'});
+
+		await user.click(
+			await findByRole('button', {name: 'Show deleted documents'}),
+		);
+
+		await findByText('PZ7654321Y9');
+
+		await user.click(
+			await findByRole('button', {name: 'Hide deleted documents'}),
+		);
+
+		await findByRole('heading', {name: 'Nothing to see on page 1.'});
+	});
+});
