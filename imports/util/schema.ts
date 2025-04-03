421a52069d8b857fcf4251053da5e80ae597dcc5
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-04-03 20:41:34 +0200

    :construction: progress: Make `userFilter` work on intersections.

--- imports/util/schema.ts
+++ imports/util/schema.ts
@@ -1,4 +1,4 @@
-import {z as schema} from 'zod';
+import {z as schema, type ZodTypeAny} from 'zod';
 
 type UnionToIntersectionFn<T> = (
 	T extends unknown ? (k: () => T) => void : never
@@ -10,6 +10,13 @@ type GetUnionLast<T> = UnionToIntersectionFn<T> extends () => infer Last
 	? Last
 	: never;
 
+type UnionToIntersection<T, Intersection extends any = any> = T extends never
+	? Intersection
+	: UnionToIntersection<
+			Exclude<T, GetUnionLast<T>>,
+			GetUnionLast<T> & Intersection
+	  >;
+
 type UnionToTuple<T, Tuple extends unknown[] = []> = [T] extends [never]
 	? Tuple
 	: UnionToTuple<Exclude<T, GetUnionLast<T>>, [GetUnionLast<T>, ...Tuple]>;
@@ -18,9 +25,74 @@ type CastToStringTuple<T> = T extends [string, ...string[]] ? T : never;
 
 export type UnionToTupleString<T> = CastToStringTuple<UnionToTuple<T>>;
 
+export const map = <T extends schema.ZodTypeAny, U extends schema.ZodTypeAny>(
+	fn: (x: schema.ZodTypeAny) => schema.ZodTypeAny,
+	tSchema: T,
+): U => {
+	if (tSchema instanceof schema.ZodReadonly) {
+		return map(fn, tSchema.unwrap()).readonly() as unknown as U;
+	}
+
+	if (tSchema instanceof schema.ZodOptional) {
+		return map(
+			fn,
+			schema.union([tSchema.unwrap(), schema.undefined()]),
+		) as unknown as U;
+	}
+
+	if (tSchema instanceof schema.ZodNullable) {
+		return map(
+			fn,
+			schema.union([tSchema.unwrap(), schema.null()]),
+		) as unknown as U;
+	}
+
+	if (tSchema instanceof schema.ZodBranded) {
+		return map(fn, tSchema.unwrap()).brand() as unknown as U;
+	}
+
+	if (tSchema instanceof schema.ZodUnion) {
+		return schema.union(
+			tSchema.options.map((x: schema.ZodTypeAny) => map(fn, x)),
+		) as unknown as U;
+	}
+
+	if (tSchema instanceof schema.ZodIntersection) {
+		return schema.intersection(
+			map(fn, tSchema._def.left),
+			map(fn, tSchema._def.right),
+		) as unknown as U;
+	}
+
+	return fn(tSchema) as U;
+};
+
+export const toJSON = <T extends schema.ZodTypeAny>(tSchema: T): string => {
+	const x = (tSchema: schema.ZodTypeAny) => {
+		if (tSchema instanceof schema.ZodObject)
+			return {
+				def: tSchema._def,
+				shape: Object.fromEntries(
+					Object.entries(tSchema.shape).map(([key, value]) => [
+						key,
+						map(x, value as any),
+					]),
+				),
+			} as any;
+
+		return tSchema;
+	};
+
+	return JSON.stringify(map(x, tSchema), undefined, 2);
+};
+
+type AtKeyOf<T extends schema.ZodTypeAny> = T extends schema.ZodUnion<infer U>
+	? keyof UnionToIntersection<U>
+	: keyof schema.infer<T>;
+
 export const keyof = <T extends schema.ZodTypeAny>(
 	tSchema: T,
-): schema.ZodEnum<UnionToTupleString<keyof schema.infer<T>>> => {
+): schema.ZodEnum<UnionToTupleString<AtKeyOf<T>>> => {
 	if (tSchema instanceof schema.ZodOptional) {
 		tSchema = tSchema.unwrap();
 	}
@@ -63,13 +135,20 @@ export const keyof = <T extends schema.ZodTypeAny>(
 
 export const at = <
 	T extends schema.ZodTypeAny,
-	K extends schema.ZodType<keyof schema.infer<T>>,
+	K extends schema.ZodType<AtKeyOf<T>>,
 >(
 	tSchema: T,
 	key: K,
 ): schema.ZodType<schema.infer<T>[schema.infer<K>]> => {
 	if (tSchema instanceof schema.ZodOptional) {
-		tSchema = tSchema.unwrap();
+		return schema.union([
+			at(tSchema.unwrap(), key),
+			at(schema.undefined(), schema.never()),
+		]);
+	}
+
+	if (tSchema instanceof schema.ZodAny) {
+		return tSchema;
 	}
 
 	if (tSchema instanceof schema.ZodNever) {
@@ -82,11 +161,23 @@ export const at = <
 
 	if (tSchema instanceof schema.ZodObject) {
 		if (key instanceof schema.ZodLiteral) {
-			return tSchema.shape[key.value] ?? schema.never();
+			return (
+				tSchema.shape[key.value] ??
+				(tSchema._def.catchAll instanceof schema.ZodNever &&
+				tSchema._def.unknownKeys === 'strict'
+					? schema.never()
+					: schema.any())
+			);
 		}
 
 		if (key instanceof schema.ZodString) {
-			return schema.union(Object.values(tSchema.shape) as any);
+			return schema.union(
+				Object.values(tSchema.shape) as [
+					ZodTypeAny,
+					ZodTypeAny,
+					...ZodTypeAny[],
+				],
+			);
 		}
 	} else if (tSchema instanceof schema.ZodRecord) {
 		if (key instanceof schema.ZodLiteral) {
@@ -103,10 +194,10 @@ export const at = <
 	} else if (tSchema instanceof schema.ZodUnion) {
 		return schema.union(tSchema.options.map((item) => at(item, key)));
 	} else if (tSchema instanceof schema.ZodIntersection) {
-		return schema.union([
+		return schema.intersection(
 			at(tSchema._def.left, key),
 			at(tSchema._def.right, key),
-		]);
+		);
 	}
 
 	console.debug({key, tSchema});
@@ -116,6 +207,12 @@ export const at = <
 };
 
 export const partial = <T extends schema.ZodTypeAny>(
+	tSchema: T,
+): schema.ZodType<Partial<schema.infer<T>>> => {
+	return map(_partial, tSchema);
+};
+
+const _partial = <T extends schema.ZodTypeAny>(
 	tSchema: T,
 ): schema.ZodType<Partial<schema.infer<T>>> => {
 	if (tSchema instanceof schema.ZodNever) {
@@ -136,17 +233,6 @@ export const partial = <T extends schema.ZodTypeAny>(
 
 	if (tSchema instanceof schema.ZodRecord) {
 		return tSchema;
-	}
-
-	if (tSchema instanceof schema.ZodUnion) {
-		return schema.union(tSchema.options.map((item) => partial(item)));
-	}
-
-	if (tSchema instanceof schema.ZodIntersection) {
-		return schema.intersection(
-			partial(tSchema._def.left),
-			partial(tSchema._def.right),
-		);
 	}
 
 	console.debug({tSchema});
