8e64cb2549d43af431049bcdabea660b30ef0931
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-04-13 20:15:26 +0200

    :construction: progress: Fix broken tests.

--- imports/ui/allergies/ReactiveAllergyCard.tests.tsx
+++ imports/ui/allergies/ReactiveAllergyCard.tests.tsx
@@ -6,17 +6,35 @@ import {assert} from 'chai';
 
 import {BrowserRouter} from 'react-router-dom';
 
+import {ThemeProvider} from '@mui/material/styles';
+
 import call from '../../api/endpoint/call';
 import {client, randomPassword, randomUserId} from '../../_test/fixtures';
 import createUserWithPasswordAndLogin from '../../api/user/createUserWithPasswordAndLogin';
 import {newPatientFormData} from '../../api/_dev/populate/patients';
 
 import patientsInsert from '../../api/endpoint/patients/insert';
-import {render} from '../../_test/react';
+import {render as _render} from '../../_test/react';
 
 import {type FormattedLine, type NormalizedLine} from '../../api/string';
 
+import useUserTheme from '../useUserTheme';
+
 import ReactiveAllergyCard from './ReactiveAllergyCard';
+
+const RenderWrapper = ({children}: {readonly children: React.ReactNode}) => {
+	const theme = useUserTheme();
+	return (
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>{children}</BrowserRouter>
+		</ThemeProvider>
+	);
+};
+
+const render = (children: React.ReactNode) =>
+	_render(children, {
+		wrapper: RenderWrapper,
+	});
 
 client(__filename, () => {
 	it("should allow to change allergy's color", async () => {
@@ -37,11 +55,7 @@ client(__filename, () => {
 			}),
 		);
 
-		const {findByRole} = render(
-			<BrowserRouter>
-				<ReactiveAllergyCard item={allergy} />
-			</BrowserRouter>,
-		);
+		const {findByRole} = render(<ReactiveAllergyCard item={allergy} />);
 
 		const button = await findByRole('button', {name: 'Color for orange'});
 
