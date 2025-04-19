57ed2cf3f6d87e2750c1a5a98ed90d2d605b6883
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-04-19 08:34:33 +0200

    :woman_technologist: dx(i18n): Add hooks for `DataGrid` localization.

---
+++ imports/i18n/dataGrid.ts
@@ -0,0 +1,77 @@
+import {type GridLocaleText} from '@mui/x-data-grid';
+
+import {get as getSetting} from '../api/settings';
+
+import useLocaleKey from './useLocale';
+import {load, useLoadedValue} from './localesCache';
+
+type LocaleText = Partial<GridLocaleText>;
+
+type DataGridLocalization = {
+	components: {
+		MuiDataGrid: {
+			defaultProps: {
+				localeText: LocaleText;
+			};
+		};
+	};
+};
+
+const dataGridLocalizationLoaders: Readonly<
+	Record<string, () => Promise<DataGridLocalization>>
+> = {
+	'nl-BE': async () =>
+		import('@mui/x-data-grid/locales/nlNL.js').then(({nlNL}) => nlNL),
+	'fr-BE': async () =>
+		import('@mui/x-data-grid/locales/frFR.js').then(({frFR}) => frFR),
+};
+
+const loadDataGridLocalization = async (
+	key: string,
+): Promise<DataGridLocalization | undefined> =>
+	dataGridLocalizationLoaders[key]?.();
+
+const dataGridLocalizationsCache = new Map<
+	string,
+	DataGridLocalization | undefined
+>();
+
+const _getDataGridLocalization = async (
+	key: string,
+): Promise<DataGridLocalization | undefined> => {
+	return load<DataGridLocalization | undefined>(
+		'dataGrid localization',
+		dataGridLocalizationsCache,
+		loadDataGridLocalization,
+		key,
+	);
+};
+
+const _dataGridLocalizationToLocaleText = (
+	localization: DataGridLocalization | undefined,
+) => {
+	return localization?.components.MuiDataGrid.defaultProps.localeText;
+};
+
+export const getLocaleText = async (
+	owner: string,
+): Promise<LocaleText | undefined> => {
+	const key = getSetting(owner, 'lang');
+	const localization = await _getDataGridLocalization(key);
+	return _dataGridLocalizationToLocaleText(localization);
+};
+
+const useDataGridLocalization = (key: string) => {
+	return useLoadedValue<DataGridLocalization | undefined>(
+		'dataGrid localization',
+		dataGridLocalizationsCache,
+		loadDataGridLocalization,
+		key,
+	);
+};
+
+export const useLocaleText = () => {
+	const key = useLocaleKey();
+	const localization = useDataGridLocalization(key);
+	return _dataGridLocalizationToLocaleText(localization);
+};
