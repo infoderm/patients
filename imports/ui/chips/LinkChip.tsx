0681fd15759debd259adc5dff3b517592f443c47
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-04-25 00:20:03 +0200

    :woman_technologist: dx(`<LinkChip>`): Allow refs and improve typing.

--- imports/ui/chips/LinkChip.tsx
+++ imports/ui/chips/LinkChip.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 import {Link} from 'react-router-dom';
-import Chip from '@mui/material/Chip';
 
-const LinkChip = ({to, ...rest}) => <Chip component={Link} to={to} {...rest} />;
+import Chip, {type ChipProps} from '@mui/material/Chip';
+
+const LinkChip = React.forwardRef(
+	({to, ...rest}: ChipProps<typeof Link>, ref) => (
+		<Chip ref={ref} component={Link} to={to} {...rest} />
+	),
+);
 
 export default LinkChip;
