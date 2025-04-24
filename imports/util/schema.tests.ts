b869dba3bb28d00675b3215c1ebca9d79c18d46e
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-04-25 00:20:05 +0200

    :construction: progress: Improve toJSON by drafting simplification util.

--- imports/util/schema.tests.ts
+++ imports/util/schema.tests.ts
@@ -2,7 +2,7 @@ import {assert} from 'chai';
 
 import {isomorphic} from '../_test/fixtures';
 
-import schema, {at, keyof, partial, toJSON} from './schema';
+import schema, {at, keyof, partial, simplify, toJSON} from './schema';
 
 const _asserInvalidInput = <T>(
 	result: schema.SafeParseReturnType<T, T>,
@@ -126,7 +126,7 @@ isomorphic(__filename, () => {
 			),
 		);
 
-		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+		assert.deepEqual(JSON.parse(toJSON(simplify(tSchema))), {
 			_def: {
 				typeName: 'ZodEnum',
 				values: ['count', 'name'],
@@ -144,8 +144,396 @@ isomorphic(__filename, () => {
 	it('`partial` should work on objects', () => {
 		const tSchema = partial(schema.object({count: schema.number()}));
 
+		assert.deepEqual(JSON.parse(toJSON(simplify(tSchema))), {
+			_def: {
+				typeName: 'ZodObject',
+				catchall: {
+					_def: {
+						typeName: 'ZodNever',
+					},
+				},
+				unknownKeys: 'strip',
+			},
+			shape: {
+				count: {
+					_def: {
+						typeName: 'ZodOptional',
+						innerType: {
+							_def: {
+								checks: [],
+								coerce: false,
+								typeName: 'ZodNumber',
+							},
+						},
+					},
+				},
+			},
+		});
+	});
+
+	it('`simplify` branded types', () => {
+		const tSchema = simplify(
+			schema.union([schema.null(), schema.undefined()]).brand(),
+		);
+
 		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
-			def: {
+			_def: {
+				type: {
+					_def: {
+						innerType: {
+							_def: {
+								typeName: 'ZodNull',
+							},
+						},
+						typeName: 'ZodOptional',
+					},
+				},
+				typeName: 'ZodBranded',
+			},
+		});
+	});
+
+	it('`simplify` readonly types', () => {
+		const tSchema = simplify(
+			schema.union([schema.undefined(), schema.null()]).readonly(),
+		);
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				innerType: {
+					_def: {
+						innerType: {
+							_def: {
+								typeName: 'ZodNull',
+							},
+						},
+						typeName: 'ZodOptional',
+					},
+				},
+				typeName: 'ZodReadonly',
+			},
+		});
+	});
+
+	it('`simplify` union with `any`', () => {
+		const tSchema = simplify(schema.union([schema.string(), schema.any()]));
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				typeName: 'ZodAny',
+			},
+			_any: true,
+		});
+	});
+
+	it('`simplify` union with `unknown`', () => {
+		const tSchema = simplify(schema.union([schema.string(), schema.unknown()]));
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				typeName: 'ZodUnknown',
+			},
+			_unknown: true,
+		});
+	});
+
+	it('`simplify` union with `never`', () => {
+		const tSchema = simplify(schema.union([schema.string(), schema.never()]));
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				checks: [],
+				coerce: false,
+				typeName: 'ZodString',
+			},
+		});
+	});
+
+	it('`simplify` unions of `undefined`s and `null`s', () => {
+		const tSchema = simplify(
+			schema
+				.union([
+					schema.string().optional(),
+					schema.null(),
+					schema.undefined(),
+					schema.number().nullable(),
+				])
+				.optional()
+				.nullable()
+				.optional()
+				.nullable(),
+		);
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				innerType: {
+					_def: {
+						innerType: {
+							_def: {
+								options: [
+									{
+										_def: {
+											checks: [],
+											coerce: false,
+											typeName: 'ZodString',
+										},
+									},
+									{
+										_def: {
+											checks: [],
+											coerce: false,
+											typeName: 'ZodNumber',
+										},
+									},
+								],
+								typeName: 'ZodUnion',
+							},
+						},
+						typeName: 'ZodNullable',
+					},
+				},
+				typeName: 'ZodOptional',
+			},
+		});
+	});
+
+	it('`simplify` intersection with `any`', () => {
+		const tSchema = simplify(
+			schema.intersection(schema.string(), schema.any()),
+		);
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				checks: [],
+				coerce: false,
+				typeName: 'ZodString',
+			},
+		});
+	});
+
+	it('`simplify` intersection with `unknown`', () => {
+		const tSchema = simplify(
+			schema.intersection(schema.unknown(), schema.string()),
+		);
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				checks: [],
+				coerce: false,
+				typeName: 'ZodString',
+			},
+		});
+	});
+
+	it('`simplify` intersection with `never`', () => {
+		const tSchema = simplify(
+			schema.intersection(schema.string(), schema.never()),
+		);
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				typeName: 'ZodNever',
+			},
+		});
+	});
+
+	it('`simplify` intersection(any, never)', () => {
+		const tSchema = simplify(schema.intersection(schema.any(), schema.never()));
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				typeName: 'ZodNever',
+			},
+		});
+	});
+
+	it('`simplify` intersection(never, unknown)', () => {
+		const tSchema = simplify(
+			schema.intersection(schema.never(), schema.unknown()),
+		);
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				typeName: 'ZodNever',
+			},
+		});
+	});
+
+	it('`simplify` intersection(string, number)', () => {
+		const tSchema = simplify(
+			schema.intersection(schema.string(), schema.number()),
+		);
+
+		// TODO: Should be `never`.
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				left: {
+					_def: {
+						checks: [],
+						coerce: false,
+						typeName: 'ZodString',
+					},
+				},
+				right: {
+					_def: {
+						checks: [],
+						coerce: false,
+						typeName: 'ZodNumber',
+					},
+				},
+				typeName: 'ZodIntersection',
+			},
+		});
+	});
+
+	it('`simplify` intersection(union(string, number).optional, number)', () => {
+		const tSchema = simplify(
+			schema.intersection(
+				schema.union([schema.string(), schema.number()]).optional(),
+				schema.number(),
+			),
+		);
+
+		// TODO: Should be `number`.
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				left: {
+					_def: {
+						innerType: {
+							_def: {
+								options: [
+									{
+										_def: {
+											checks: [],
+											coerce: false,
+											typeName: 'ZodString',
+										},
+									},
+									{
+										_def: {
+											checks: [],
+											coerce: false,
+											typeName: 'ZodNumber',
+										},
+									},
+								],
+								typeName: 'ZodUnion',
+							},
+						},
+						typeName: 'ZodOptional',
+					},
+				},
+				right: {
+					_def: {
+						checks: [],
+						coerce: false,
+						typeName: 'ZodNumber',
+					},
+				},
+				typeName: 'ZodIntersection',
+			},
+		});
+	});
+
+	it('`simplify` deduplicates intersections (string)', () => {
+		const tSchema = simplify(
+			schema.intersection(schema.string(), schema.string()),
+		);
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				checks: [],
+				coerce: false,
+				typeName: 'ZodString',
+			},
+		});
+	});
+
+	it('`simplify` deduplicates intersections (symbol)', () => {
+		const tSchema = simplify(
+			schema.intersection(schema.symbol(), schema.symbol()),
+		);
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				typeName: 'ZodSymbol',
+			},
+		});
+	});
+
+	it('`simplify` deduplicates intersections (number)', () => {
+		const tSchema = simplify(
+			schema.intersection(schema.number(), schema.number()),
+		);
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				checks: [],
+				coerce: false,
+				typeName: 'ZodNumber',
+			},
+		});
+	});
+
+	it('`simplify` deduplicates intersections (nan)', () => {
+		const tSchema = simplify(schema.intersection(schema.nan(), schema.nan()));
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				typeName: 'ZodNaN',
+			},
+		});
+	});
+
+	it('`simplify` deduplicates intersections (undefined)', () => {
+		const tSchema = simplify(
+			schema.intersection(schema.undefined(), schema.undefined()),
+		);
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				typeName: 'ZodUndefined',
+			},
+		});
+	});
+
+	it('`simplify` deduplicates intersections (null)', () => {
+		const tSchema = simplify(schema.intersection(schema.null(), schema.null()));
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				typeName: 'ZodNull',
+			},
+		});
+	});
+
+	it('`simplify` deduplicates nested intersections', () => {
+		const tSchema = simplify(
+			schema.intersection(
+				schema.string(),
+				schema.intersection(schema.string(), schema.string()),
+			),
+		);
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				checks: [],
+				coerce: false,
+				typeName: 'ZodString',
+			},
+		});
+	});
+
+	it('`simplify` merges object intersections', () => {
+		const tSchema = simplify(
+			schema.intersection(
+				schema.object({count: schema.number()}),
+				schema.object({name: schema.string()}),
+			),
+		);
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
 				catchall: {
 					_def: {
 						typeName: 'ZodNever',
@@ -157,21 +545,132 @@ isomorphic(__filename, () => {
 			shape: {
 				count: {
 					_def: {
-						options: [
-							{
-								_def: {
-									checks: [],
-									coerce: false,
-									typeName: 'ZodNumber',
-								},
+						checks: [],
+						coerce: false,
+						typeName: 'ZodNumber',
+					},
+				},
+				name: {
+					_def: {
+						checks: [],
+						coerce: false,
+						typeName: 'ZodString',
+					},
+				},
+			},
+		});
+	});
+
+	it('`simplify` merges strict object intersections', () => {
+		const tSchema = simplify(
+			schema.intersection(
+				schema.object({count: schema.number()}).strict(),
+				schema.object({name: schema.string()}).strict(),
+			),
+		);
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				catchall: {
+					_def: {
+						typeName: 'ZodNever',
+					},
+				},
+				typeName: 'ZodObject',
+				unknownKeys: 'strict',
+			},
+			shape: {
+				count: {
+					_def: {
+						checks: [],
+						coerce: false,
+						typeName: 'ZodNumber',
+					},
+				},
+				name: {
+					_def: {
+						checks: [],
+						coerce: false,
+						typeName: 'ZodString',
+					},
+				},
+			},
+		});
+	});
+
+	it('`simplify` merges nested object intersections', () => {
+		const tSchema = simplify(
+			schema.intersection(
+				schema.object({id: schema.symbol()}),
+				schema.intersection(
+					schema.object({count: schema.number()}),
+					schema.object({name: schema.string()}),
+				),
+			),
+		);
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				catchall: {
+					_def: {
+						typeName: 'ZodNever',
+					},
+				},
+				typeName: 'ZodObject',
+				unknownKeys: 'strip',
+			},
+			shape: {
+				count: {
+					_def: {
+						checks: [],
+						coerce: false,
+						typeName: 'ZodNumber',
+					},
+				},
+				id: {
+					_def: {
+						typeName: 'ZodSymbol',
+					},
+				},
+				name: {
+					_def: {
+						checks: [],
+						coerce: false,
+						typeName: 'ZodString',
+					},
+				},
+			},
+		});
+	});
+
+	it('`simplify` maps object values', () => {
+		const tSchema = simplify(
+			schema.object({
+				count: schema.union([schema.undefined(), schema.number()]),
+			}),
+		);
+
+		assert.deepEqual(JSON.parse(toJSON(tSchema)), {
+			_def: {
+				catchall: {
+					_def: {
+						typeName: 'ZodNever',
+					},
+				},
+				typeName: 'ZodObject',
+				unknownKeys: 'strip',
+			},
+			shape: {
+				count: {
+					_def: {
+						innerType: {
+							_def: {
+								checks: [],
+								coerce: false,
+								typeName: 'ZodNumber',
 							},
-							{
-								_def: {
-									typeName: 'ZodUndefined',
-								},
-							},
-						],
-						typeName: 'ZodUnion',
+						},
+						typeName: 'ZodOptional',
 					},
 				},
 			},
