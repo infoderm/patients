09d4956c1678c126aa06caa147a00a2ef13cb234
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-04-25 19:04:37 +0200

    :sparkles: feat(`documents`): Allow user filtering and sorting of issues lists.

--- imports/ui/issues/MangledDocuments.tsx
+++ imports/ui/issues/MangledDocuments.tsx
@@ -4,11 +4,11 @@ import {useMangledDocuments} from '../../api/issues';
 
 import paged from '../routes/paged';
 
-import makeDocumentsList from '../documents/makeDocumentsList';
+import makeDocumentsTable from '../documents/makeDocumentsTable';
 import DocumentsListAutoFilterToggleButton from '../documents/DocumentsListAutoFilterToggleButton';
 import useDocumentsListAutoFilter from '../documents/useDocumentsListAutoFilter';
 
-const DocumentsPage = makeDocumentsList(useMangledDocuments);
+const DocumentsPage = makeDocumentsTable(useMangledDocuments);
 const DocumentsPager = paged(DocumentsPage);
 
 type Props = React.JSX.IntrinsicAttributes &
@@ -28,15 +28,6 @@ const MangledDocuments = (props: Props) => {
 					sort={{
 						createdAt: 1,
 					}}
-					LoadingIndicator={(_: {}) => <>Loading...</>}
-					EmptyPage={({page}: {readonly page: number}) =>
-						page === 1 ? (
-							<>All documents have been decoded :)</>
-						) : (
-							// eslint-disable-next-line react/jsx-no-useless-fragment
-							<>{`Nothing to see on page ${page}.`}</>
-						)
-					}
 				/>
 			</div>
 
--- imports/ui/issues/UnlinkedDocuments.tsx
+++ imports/ui/issues/UnlinkedDocuments.tsx
@@ -4,11 +4,11 @@ import {useUnlinkedDocuments} from '../../api/issues';
 
 import paged from '../routes/paged';
 
-import makeDocumentsList from '../documents/makeDocumentsList';
+import makeDocumentsTable from '../documents/makeDocumentsTable';
 import DocumentsListAutoFilterToggleButton from '../documents/DocumentsListAutoFilterToggleButton';
 import useDocumentsListAutoFilter from '../documents/useDocumentsListAutoFilter';
 
-const DocumentsPage = makeDocumentsList(useUnlinkedDocuments);
+const DocumentsPage = makeDocumentsTable(useUnlinkedDocuments);
 const DocumentsPager = paged(DocumentsPage);
 
 type Props = React.JSX.IntrinsicAttributes &
@@ -29,15 +29,6 @@ const UnlinkedDocuments = (props: Props) => {
 						datetime: 1,
 						createdAt: 1,
 					}}
-					LoadingIndicator={(_: {}) => <>Loading...</>}
-					EmptyPage={({page}: {readonly page: number}) =>
-						page === 1 ? (
-							<>All documents have an assigned patient :)</>
-						) : (
-							// eslint-disable-next-line react/jsx-no-useless-fragment
-							<>{`Nothing to see on page ${page}.`}</>
-						)
-					}
 				/>
 			</div>
 
