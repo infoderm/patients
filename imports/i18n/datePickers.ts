74f492738694a6a24c422158256558b1add147fb
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-04-25 19:04:29 +0200

    :recycle: refactor(`i18n`): Extract `i18n/localesCache`, split `i18n/datetime`.

---
+++ imports/i18n/datePickers.ts
@@ -0,0 +1,78 @@
+import {type PickersLocaleText} from '@mui/x-date-pickers';
+
+import {get as getSetting} from '../api/settings';
+
+import useLocaleKey from './useLocale';
+import {load, useLoadedValue} from './localesCache';
+
+type LocaleText = Partial<PickersLocaleText<any>>;
+
+type PickersLocalization = {
+	components: {
+		MuiLocalizationProvider: {
+			defaultProps: {
+				localeText: LocaleText;
+			};
+		};
+	};
+};
+
+const pickersLocalizationLoaders: Readonly<
+	Record<string, () => Promise<PickersLocalization>>
+> = {
+	'nl-BE': async () =>
+		import('@mui/x-date-pickers/locales/nlNL.js').then(({nlNL}) => nlNL),
+	'fr-BE': async () =>
+		import('@mui/x-date-pickers/locales/frFR.js').then(({frFR}) => frFR),
+};
+
+const loadPickersLocalization = async (
+	key: string,
+): Promise<PickersLocalization | undefined> =>
+	pickersLocalizationLoaders[key]?.();
+
+const pickersLocalizationsCache = new Map<
+	string,
+	PickersLocalization | undefined
+>();
+
+const _getPickersLocalization = async (
+	key: string,
+): Promise<PickersLocalization | undefined> => {
+	return load<PickersLocalization | undefined>(
+		'pickers localization',
+		pickersLocalizationsCache,
+		loadPickersLocalization,
+		key,
+	);
+};
+
+const _pickersLocalizationToLocaleText = (
+	localization: PickersLocalization | undefined,
+) => {
+	return localization?.components.MuiLocalizationProvider.defaultProps
+		.localeText;
+};
+
+export const getLocaleText = async (
+	owner: string,
+): Promise<LocaleText | undefined> => {
+	const key = getSetting(owner, 'lang');
+	const localization = await _getPickersLocalization(key);
+	return _pickersLocalizationToLocaleText(localization);
+};
+
+const usePickersLocalization = (key: string) => {
+	return useLoadedValue<PickersLocalization | undefined>(
+		'pickers localization',
+		pickersLocalizationsCache,
+		loadPickersLocalization,
+		key,
+	);
+};
+
+export const useLocaleText = () => {
+	const key = useLocaleKey();
+	const localization = usePickersLocalization(key);
+	return _pickersLocalizationToLocaleText(localization);
+};
