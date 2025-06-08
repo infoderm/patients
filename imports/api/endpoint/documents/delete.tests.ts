d6cae2c8b77461a06b6aff3ff5c599b22327eb6d
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-06-08 18:54:11 +0200

    :woman_technologist: dx(test): Strengthen asserts of `documents/delete`.
    To improve debugging next time we get spurious failures.

--- imports/api/endpoint/documents/delete.tests.ts
+++ imports/api/endpoint/documents/delete.tests.ts
@@ -12,6 +12,7 @@ import documentDelete from './delete';
 server(__filename, () => {
 	it('cannot delete a document when not logged in', async () => {
 		const userId = randomUserId();
+		assert.strictEqual(await Documents.find().countAsync(), 0);
 		const documentId = await newDocument({userId});
 		assert.strictEqual(await Documents.find().countAsync(), 1);
 		await throws(
@@ -23,6 +24,7 @@ server(__filename, () => {
 
 	it('cannot delete a document owned by another user', async () => {
 		const userId = randomUserId();
+		assert.strictEqual(await Documents.find().countAsync(), 0);
 		const documentId = await newDocument({userId: `${userId}x`});
 		assert.strictEqual(await Documents.find().countAsync(), 1);
 		await throws(
@@ -34,6 +36,7 @@ server(__filename, () => {
 
 	it('cannot delete a document that does not exist', async () => {
 		const userId = randomUserId();
+		assert.strictEqual(await Documents.find().countAsync(), 0);
 		return throws(
 			async () => invoke(documentDelete, {userId}, ['x']),
 			/not-found/,
@@ -42,8 +45,11 @@ server(__filename, () => {
 
 	it('can delete a document', async () => {
 		const userId = randomUserId();
+		assert.strictEqual(await Documents.find().countAsync(), 0);
 		const documentAId = await newDocument({userId});
+		assert.strictEqual(await Documents.find().countAsync(), 1);
 		const documentBId = await newDocument({userId});
+		assert.strictEqual(await Documents.find().countAsync(), 2);
 
 		assert.strictEqual(await Documents.find().countAsync(), 2);
 		await invoke(documentDelete, {userId}, [documentAId]);
@@ -60,7 +66,10 @@ server(__filename, () => {
 
 	it('is idempotent', async () => {
 		const userId = randomUserId();
+		assert.strictEqual(await Documents.find().countAsync(), 0);
 		const documentId = await newDocument({userId});
+		assert.strictEqual(await Documents.find().countAsync(), 1);
+
 		assert.strictEqual(await Documents.find({deleted: true}).countAsync(), 0);
 
 		await invoke(documentDelete, {userId}, [documentId]);
