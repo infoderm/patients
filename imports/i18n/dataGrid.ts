755eb1dc47e64cf1ab3863b7b92a63a9994c79b1
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-06-23 17:42:21 +0200

    :wastebasket: trash(`i18n/dataGrid`): Remove unused `getLocaleText`.

--- imports/i18n/dataGrid.ts
+++ imports/i18n/dataGrid.ts
@@ -1,9 +1,7 @@
 import {type GridLocaleText} from '@mui/x-data-grid';
 
-import {get as getSetting} from '../api/settings';
-
 import useLocaleKey from './useLocale';
-import {load, useLoadedValue} from './localesCache';
+import {useLoadedValue} from './localesCache';
 
 type LocaleText = Partial<GridLocaleText>;
 
@@ -36,29 +34,10 @@ const dataGridLocalizationsCache = new Map<
 	DataGridLocalization | undefined
 >();
 
-const _getDataGridLocalization = async (
-	key: string,
-): Promise<DataGridLocalization | undefined> => {
-	return load<DataGridLocalization | undefined>(
-		'dataGrid localization',
-		dataGridLocalizationsCache,
-		loadDataGridLocalization,
-		key,
-	);
-};
-
 const _dataGridLocalizationToLocaleText = (
 	localization: DataGridLocalization | undefined,
 ) => {
 	return localization?.components.MuiDataGrid.defaultProps.localeText;
-};
-
-export const getLocaleText = async (
-	owner: string,
-): Promise<LocaleText | undefined> => {
-	const key = getSetting(owner, 'lang');
-	const localization = await _getDataGridLocalization(key);
-	return _dataGridLocalizationToLocaleText(localization);
 };
 
 const useDataGridLocalization = (key: string) => {
