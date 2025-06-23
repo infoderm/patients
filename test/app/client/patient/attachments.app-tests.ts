9de58999c02571ca43486a4f577ca28007d59061
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-06-23 17:14:28 +0200

    :test_tube: test: Cover detach upload from patient with an app test.

--- test/app/client/patient/attachments.app-tests.ts
+++ test/app/client/patient/attachments.app-tests.ts
@@ -1,3 +1,5 @@
+import {faker} from '@faker-js/faker';
+
 import {
 	client,
 	randomPassword,
@@ -5,6 +7,11 @@ import {
 } from '../../../../imports/_test/fixtures';
 import {randomPNGArrayBuffer} from '../../../../imports/_test/png';
 import createUserWithPasswordAndLogin from '../../../../imports/api/user/createUserWithPasswordAndLogin';
+
+import {newUpload} from '../../../../imports/api/_dev/populate/uploads';
+
+import call from '../../../../imports/api/endpoint/call';
+import attachToPatient from '../../../../imports/api/endpoint/patients/attach';
 
 import {setupApp, navigateTo, createNewPatient, uploadFile} from '../fixtures';
 
@@ -33,5 +40,42 @@ client(__filename, () => {
 		uploadFile(button, file);
 
 		await findByRole('heading', {name: 'Pièces jointes au patient'});
+	});
+
+	it('should allow to detach a file from a patient', async () => {
+		const username = randomUserId();
+		const password = randomPassword();
+		const app = setupApp();
+		await createUserWithPasswordAndLogin(username, password);
+
+		const attachmentName = faker.string.uuid();
+
+		const patientId = await createNewPatient(app, {
+			firstname: 'John',
+			lastname: 'Doe',
+		});
+
+		const {_id: uploadId} = await newUpload(undefined, {name: attachmentName});
+		await call(attachToPatient, patientId!, uploadId);
+
+		await navigateTo(app, 'attachments', `/patient/${patientId}/attachments`);
+
+		const {user, findByRole} = app;
+
+		await findByRole('heading', {name: 'Pièces jointes au patient'});
+
+		await findByRole('img', {name: attachmentName});
+
+		await user.click(
+			await findByRole('button', {name: `open menu for ${attachmentName}`}),
+		);
+
+		await user.click(await findByRole('menuitem', {name: 'Detach'}));
+
+		await user.click(await findByRole('button', {name: 'autofill'}));
+
+		await user.click(await findByRole('button', {name: 'Detach'}));
+
+		await findByRole('heading', {name: 'Nothing to see on page 1.'});
 	});
 });
